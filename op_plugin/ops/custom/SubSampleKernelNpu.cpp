9e8472e75c0d8cd71ed4d0d27e3afb716d992198
li_jinxin <lijinxin6@huawei.com>
2023-04-26 13:59:18 +0800

    Add OP-plugin:Sqrt, SubSample, Take, TanhBackward, Tanh, Tan, Threshold, Triu, Trunc.

---
+++ op_plugin/ops/custom/SubSampleKernelNpu.cpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2023 Huawei Technologies Co., Ltd
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License  (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "op_plugin/ops/OpInterface.h"
+#include "op_plugin/utils/OpAdapter.h"
+
+namespace op_plugin {
+using npu_preparation = at_npu::native::OpPreparation;
+
+at::Tensor npu_sub_sample(
+    const at::Tensor& self,
+    int64_t per_images,
+    double positive_fraction) {
+  at::Tensor result = npu_preparation::ApplyTensor(self);
+  at_npu::native::OpCommand cmd;
+  cmd.Name("SubSample")
+      .Input(self)
+      .Output(result)
+      .Attr("batch_size_per_images", per_images)
+      .Attr("positive_fraction", (float)positive_fraction)
+      .Run();
+  return result;
+}
+} // namespace op_plugin
---
+++ op_plugin/ops/official/TanKernelNpu.cpp
@@ -0,0 +1,61 @@
+// Copyright (c) 2023 Huawei Technologies Co., Ltd
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License  (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "op_plugin/ops/OpInterface.h"
+#include "op_plugin/utils/OpAdapter.h"
+
+namespace op_plugin {
+using npu_preparation = at_npu::native::OpPreparation;
+using npu_utils = at_npu::native::NpuUtils;
+
+namespace {
+at::Tensor& tan_out_nocheck(at::Tensor& result, const at::Tensor& self) {
+  at_npu::native::OpCommand cmd;
+  cmd.Name("Tan")
+      .Input(self)
+      .Output(result)
+      .Run();
+
+  return result;
+}
+} // namespace
+
+at::Tensor& tan_out(const at::Tensor& self, at::Tensor& result) {
+  npu_preparation::CheckOut(
+      {self},
+      result,
+      self);
+
+  if (!npu_utils::check_match(&result)) {
+    at::Tensor contiguous_result = npu_utils::format_contiguous(result);
+    tan_out_nocheck(contiguous_result, self);
+    npu_utils::format_fresh_view(result, contiguous_result);
+  } else {
+    tan_out_nocheck(result, self);
+  }
+
+  return result;
+}
+
+at::Tensor tan(const at::Tensor& self) {
+  at::Tensor result = npu_preparation::ApplyTensor(self);
+  tan_out_nocheck(result, self);
+  return result;
+}
+
+at::Tensor& tan_(at::Tensor& self) {
+  return op_plugin::tan_out(self, self);
+}
+} // namespace op_plugin
