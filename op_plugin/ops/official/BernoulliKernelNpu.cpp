16afb6fb6a2f7eaf180aba51cef740fb16f9323d
戢芳 <jifang5@h-partners.com>
2023-05-16 02:55:37 +0000

    !50 Add op_plugin: bernoulli、bitwise_xor、bmm、clamp. * Add op_plugin: bernoulli、bitwise_xor、bmm、clamp.

---
+++ op_plugin/ops/official/BernoulliKernelNpu.cpp
@@ -0,0 +1,105 @@
+// Copyright (c) 2023 Huawei Technologies Co., Ltd
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License  (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <ATen/NamedTensorUtils.h>
+#include "torch_npu/csrc/aten/NPUGeneratorImpl.h"
+
+#include "op_plugin/ops/OpInterface.h"
+#include "op_plugin/utils/OpAdapter.h"
+
+namespace op_plugin {
+using npu_preparation = at_npu::native::OpPreparation;
+using npu_compile_type = at_npu::native::CompileType;
+using npu_utils = at_npu::native::NpuUtils;
+
+namespace {
+at::Tensor& bernoulli_npu_nocheck(at::Tensor& result, double p, c10::optional<at::Generator> gen) {
+  auto gen_ = at::get_generator_or_default<at_npu::NPUGeneratorImpl>(gen, at_npu::detail::getDefaultNPUGenerator());
+  auto pair = gen_->philox_engine_inputs(10);
+  const int64_t seed = pair.first;
+  const int64_t offset = pair.second;
+
+  at_npu::native::OpCommand cmd;
+  cmd.Name("StatelessBernoulli")
+      .Input(result.sizes(), at::kLong, npu_compile_type::MEMORY_HOST_COMPILE_INDEPENDENT)
+      .Input(at::Scalar(p), at::kFloat)
+      .Input(at::Scalar(seed), at::kLong)
+      .Input(at::Scalar(offset), at::kLong)
+      .Output(result)
+      .Attr("dtype", result.scalar_type())
+      .Run();
+  return result;
+}
+
+at::Tensor& bernoulli_npu_nocheck(at::Tensor& result, const at::Tensor& p, c10::optional<at::Generator> gen) {
+  auto gen_ = at::get_generator_or_default<at_npu::NPUGeneratorImpl>(gen, at_npu::detail::getDefaultNPUGenerator());
+  auto pair = gen_->philox_engine_inputs(10);
+  const int64_t seed = pair.first;
+  const int64_t offset = pair.second;
+
+  at_npu::native::OpCommand cmd;
+  cmd.Name("StatelessBernoulli")
+      .Input(result.sizes(), at::kLong, npu_compile_type::MEMORY_HOST_COMPILE_INDEPENDENT)
+      .Input(p)
+      .Input(at::Scalar(seed), at::kLong)
+      .Input(at::Scalar(offset), at::kLong)
+      .Output(result)
+      .Attr("dtype", result.scalar_type())
+      .Run();
+  return result;
+}
+} // namespace
+
+at::Tensor& bernoulli_(at::Tensor& self, double p, c10::optional<at::Generator> gen) {
+  if (!npu_utils::check_match(&self)) {
+    at::Tensor contiguous_self = npu_utils::format_contiguous(self);
+    bernoulli_npu_nocheck(contiguous_self, p, gen);
+    npu_utils::format_fresh_view(self, contiguous_self);
+  } else {
+    bernoulli_npu_nocheck(self, p, gen);
+  }
+  return self;
+}
+
+at::Tensor& bernoulli_(at::Tensor& self, const at::Tensor& p, c10::optional<at::Generator> gen) {
+  at::Tensor p_ori_format = npu_preparation::CastBackToOriFormat(p);
+  npu_preparation::CheckMemory({self, p}, {self});
+
+  if (!npu_utils::check_match(&self)) {
+    at::Tensor contiguous_self = npu_utils::format_contiguous(self);
+    bernoulli_npu_nocheck(contiguous_self, p_ori_format, gen);
+    npu_utils::format_fresh_view(self, contiguous_self);
+  } else {
+    bernoulli_npu_nocheck(self, p_ori_format, gen);
+  }
+  return self;
+}
+
+at::Tensor bernoulli(const at::Tensor& self, c10::optional<at::Generator> gen) {
+  at::Tensor result = npu_preparation::ApplyTensorWithFormat(self.sizes(), self.options(), ACL_FORMAT_ND);
+  bernoulli_npu_nocheck(result, self, gen);
+  return result;
+}
+
+at::Tensor bernoulli(const at::Tensor& self, double p, c10::optional<at::Generator> gen) {
+  return at::empty_like(self, LEGACY_CONTIGUOUS_MEMORY_FORMAT).bernoulli_(p, gen);
+}
+
+at::Tensor& bernoulli_out(const at::Tensor& self, c10::optional<at::Generator> gen, at::Tensor& result) {
+  result.resize_(self.sizes()).bernoulli_(self, gen);
+  at::namedinference::propagate_names(result, self);
+  return result;
+}
+} // namespace op_plugin
---
+++ op_plugin/ops/official/BitwiseXorKernelNpu.cpp
@@ -0,0 +1,151 @@
+// Copyright (c) 2023 Huawei Technologies Co., Ltd
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License  (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "op_plugin/ops/OpInterface.h"
+#include "op_plugin/utils/OpAdapter.h"
+
+namespace op_plugin {
+using npu_preparation = at_npu::native::OpPreparation;
+using calcu_op_util = at_npu::native::CalcuOpUtil;
+using npu_utils = at_npu::native::NpuUtils;
+
+namespace {
+at::Tensor& bitwise_xor_out_npu_nocheck(
+    at::Tensor& result,
+    const at::Tensor& self,
+    const at::Scalar other) {
+  at_npu::native::OpCommand cmd;
+  cmd.Name("BitwiseXor")
+      .Input(self)
+      .Input(other, self.scalar_type())
+      .Output(result)
+      .Run();
+  return result;
+}
+
+at::Tensor& bitwise_xor_out_npu_nocheck(
+    at::Tensor& result,
+    const at::Tensor& self,
+    const at::Tensor& other) {
+  auto unified_result = npu_preparation::binary_op_check(result, self, other, true);
+  if (other.dim() == 0 && !torch_npu::utils::is_npu(other)) {
+    op_plugin::bitwise_xor_out(self, other.item(), result);
+  } else if (self.dim() == 0 && !torch_npu::utils::is_npu(self)) {
+    op_plugin::bitwise_xor_out(other, self.item(), result);
+  } else {
+    at_npu::native::OpCommand cmd;
+    cmd.Name("BitwiseXor")
+        .Expect(unified_result)
+        .Input(self)
+        .Input(other)
+        .Output(result)
+        .Run();
+  }
+  return result;
+}
+} // namespace
+
+at::Tensor& bitwise_xor_out(
+    const at::Tensor& self,
+    const at::Scalar& other,
+    at::Tensor& result) {
+  npu_preparation::CheckOut(
+      {self},
+      result,
+      self);
+  at::Tensor self_input = (self.dtype() == at::kBool) ? op_plugin::npu_dtype_cast(self, at::kInt) : self;
+  at::Tensor result_cp = (result.dtype() == at::kBool) ? op_plugin::npu_dtype_cast(result, at::kInt) : result;
+
+  if (!npu_utils::check_match(&result_cp)) {
+    at::Tensor contiguous_result = npu_utils::format_contiguous(result_cp);
+    bitwise_xor_out_npu_nocheck(contiguous_result, self_input, other);
+    npu_utils::format_fresh_view(result_cp, contiguous_result);
+  } else {
+    bitwise_xor_out_npu_nocheck(result_cp, self_input, other);
+  }
+  if (self.dtype() == at::kBool) {
+    result_cp = op_plugin::npu_dtype_cast(result_cp, at::kBool);
+    result.copy_(result_cp);
+  }
+  return result;
+}
+
+at::Tensor& bitwise_xor_out(
+    const at::Tensor& self,
+    const at::Tensor& other,
+    at::Tensor& result) {
+  bool is_self_wrapped = calcu_op_util::IsScalarWrappedToTensor(self);
+  at::Tensor output_tensor = is_self_wrapped ? other : self;
+
+  auto output_size = op_infer::broadcast_ops_npu_output_size(self, other);
+  npu_preparation::CheckOut(
+      {self, other},
+      result,
+      output_tensor,
+      output_size);
+  at::Tensor self_input = (self.dtype() == at::kBool) ? op_plugin::npu_dtype_cast(self, at::kInt) : self;
+  at::Tensor other_input = (other.dtype() == at::kBool) ? op_plugin::npu_dtype_cast(other, at::kInt) : other;
+  at::Tensor result_cp = (result.dtype() == at::kBool) ? op_plugin::npu_dtype_cast(result, at::kInt) : result;
+
+  if (!npu_utils::check_match(&result_cp)) {
+    at::Tensor contiguous_result = npu_utils::format_contiguous(result_cp);
+    bitwise_xor_out_npu_nocheck(contiguous_result, self_input, other_input);
+    npu_utils::format_fresh_view(result_cp, contiguous_result);
+  } else {
+    bitwise_xor_out_npu_nocheck(result_cp, self_input, other_input);
+  }
+  if (self.dtype() == at::kBool) {
+    result_cp = op_plugin::npu_dtype_cast(result_cp, at::kBool);
+    result.copy_(result_cp);
+  }
+  return result;
+}
+
+at::Tensor bitwise_xor(const at::Tensor& self, const at::Tensor& other) {
+  bool is_self_wrapped = calcu_op_util::IsScalarWrappedToTensor(self);
+  auto output_size = op_infer::broadcast_ops_npu_output_size(self, other);
+  at::Tensor output_tensor = is_self_wrapped ? other : self;
+
+  at::Tensor self_input  = (self.dtype() == at::kBool) ? op_plugin::npu_dtype_cast(self, at::kInt) : self;
+  at::Tensor other_input = (other.dtype() == at::kBool) ? op_plugin::npu_dtype_cast(other, at::kInt) : other;
+  at::Tensor result = output_tensor.dtype() == at::kBool ?
+      npu_preparation::ApplyTensor(output_size, output_tensor.options().dtype(at::kInt), output_tensor) :
+      npu_preparation::ApplyTensor(output_tensor, output_size);
+
+  bitwise_xor_out_npu_nocheck(result, self_input, other_input);
+  if (output_tensor.dtype() == at::kBool) {
+    result = op_plugin::npu_dtype_cast(result, at::kBool);
+  }
+  return result;
+}
+
+at::Tensor bitwise_xor(const at::Tensor& self, const at::Scalar& other) {
+  at::Tensor self_input = (self.dtype() == at::kBool) ? op_plugin::npu_dtype_cast(self, at::kInt) : self;
+  at::Tensor result = npu_preparation::ApplyTensor(self_input);
+  bitwise_xor_out_npu_nocheck(result, self_input, other);
+  if (self.dtype() == at::kBool) {
+    result = op_plugin::npu_dtype_cast(result, at::kBool);
+  }
+  return result;
+}
+
+at::Tensor& bitwise_xor_(at::Tensor& self, const at::Tensor& other) {
+  return op_plugin::bitwise_xor_out(self, other, self);
+}
+
+at::Tensor& bitwise_xor_(at::Tensor& self, const at::Scalar& other) {
+  return op_plugin::bitwise_xor_out(self, other, self);
+}
+} // namespace op_plugin
