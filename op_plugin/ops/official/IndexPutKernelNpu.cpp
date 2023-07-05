982add5cac7c750dcb9c9f26764f7cf6e2ded70b
chlme16 <td.chenhongliang@huawei.com>
2023-07-05 10:35:17 +0800

    Fix bug of indexput

--- op_plugin/ops/official/IndexPutKernelNpu.cpp
+++ op_plugin/ops/official/IndexPutKernelNpu.cpp
@@ -15,8 +15,6 @@
 
 #include <ATen/native/IndexingUtils.h>
 #include <ATen/native/TypeProperties.h>
-#include "torch_npu/csrc/framework/graph/util/GraphModeGuard.h"
-#include "torch_npu/csrc/framework/graph/construct/GraphConstructor.h"
 #include <third_party/acl/inc/op_proto/experiment_ops.h>
 
 #include "op_plugin/ops/OpInterface.h"
@@ -24,18 +22,23 @@
 #include "op_plugin/utils/AdvancedIndex.h"
 
 namespace op_plugin {
-using DyNumAndIndex = std::vector<std::pair<uint32_t, uint32_t>>;
 using npu_preparation = at_npu::native::OpPreparation;
 using npu_utils = at_npu::native::NpuUtils;
+using npu_compile_type = at_npu::native::CompileType;
 
 namespace {
 template <typename ge_op_type>
 at_npu::native::DynamicInputRegFunc indexput_func =
-    [](DyNumAndIndex num_and_index, std::string op_name) -> ge::OperatorPtr {
+    [](std::vector<std::pair<uint32_t, uint32_t>> num_and_index, std::string op_name) -> ge::OperatorPtr {
   auto ge_op = std::make_shared<ge_op_type>(op_name.c_str());
   ge_op->create_dynamic_input_byindex_indices(num_and_index.front().first, num_and_index.front().second);
   return ge_op;
 };
+const std::string x_str = "x";
+const std::string value_str = "value";
+const std::string indexed_sizes_str = "indexed_sizes";
+const std::string indexed_strides_str = "indexed_strides";
+const std::string aicore_str = "AiCore";
 
 bool is_aicpu_valid(const at::Tensor& self,
     const std::vector<at::Tensor>& all_defined_indices,
@@ -59,12 +62,6 @@ bool is_aicpu_valid(const at::Tensor& self,
     return true;
   }
 
-  // first return false, after trans to int, and then check by aicpu or aicore
-  for (int32_t i = 0; i < all_defined_indices.size(); i++) {
-    if (all_defined_indices[i].scalar_type() == at::kBool) {
-      return false;
-    }
-  }
   // indices may need broadcast, in this case, indexput is implemented by aicpu
   for (int32_t i = 1; i < all_defined_indices.size(); i++) {
     if (all_defined_indices[0].dim() != all_defined_indices[i].dim()) {
@@ -109,20 +106,19 @@ at::Tensor& index_put_aicore_nocheck(
       all_defined_indices[0].sizes()[0] != value.sizes()[0]) {
     temp_value_broadcast = op_plugin::npu_broadcast(temp_value, all_defined_indices[0].sizes());
   }
-  auto masks_tensors = at::tensor(masks, self.options().dtype(at::kLong));
-  auto expand_masks_tensors = at::tensor(expand_masks, self.options().dtype(at::kLong));
+
   at_npu::native::OpCommand cmd;
   cmd.Name("IndexPutV2")
-      .Input(temp_self, (string)"x")
-      .Input(temp_value_broadcast, (string)"value")
-      .Input(masks_tensors, (string)"indexed_sizes")
-      .Input(expand_masks_tensors, (string)"indexed_strides");
+      .Input(temp_self, x_str)
+      .Input(temp_value_broadcast, value_str)
+      .Input(masks, at::kLong, npu_compile_type::MEMORY_HOST_COMPILE_INDEPENDENT, "", indexed_sizes_str)
+      .Input(expand_masks, at::kLong, npu_compile_type::MEMORY_HOST_COMPILE_INDEPENDENT, "", indexed_strides_str);
   for (int i = 0; i < all_defined_indices.size(); i++) {
     string input_name = "indices" +std::to_string(i);
     cmd.Input(all_defined_indices[i], input_name);
   }
   cmd.DynamicInputReg(indexput_func<ge::op::IndexPutV2>, {{all_defined_indices.size(), 4}})
-      .Output(temp_self, (string)"x")
+      .Output(temp_self, x_str)
       .Attr("accumulate", accumulate)
       .Run();
   if (self.scalar_type() == at::ScalarType::Half) {
@@ -173,20 +169,20 @@ at::Tensor& index_put_aicpu_nocheck(
     temp_value = op_plugin::npu_dtype_cast(value, at::ScalarType::Float);
     result = op_plugin::npu_dtype_cast(result, at::ScalarType::Float);
   }
-  auto masks_tensors = at::tensor(masks, self.options().dtype(at::kLong));
+
   at_npu::native::OpCommand cmd;
   cmd.Name("IndexPutV2")
-      .Input(temp_self, (string)"x")
-      .Input(temp_value, (string)"value")
-      .Input(masks_tensors, (string)"indexed_sizes")
-      .Input(masks_tensors, (string)"indexed_strides");
+      .Input(temp_self, x_str)
+      .Input(temp_value, value_str)
+      .Input(masks, at::kLong, npu_compile_type::MEMORY_HOST_COMPILE_INDEPENDENT, "", indexed_sizes_str)
+      .Input(masks, at::kLong, npu_compile_type::MEMORY_HOST_COMPILE_INDEPENDENT, "", indexed_strides_str);
   for (int i = 0; i < all_defined_indices.size(); i++) {
     string input_name = "indices" + std::to_string(i);
     cmd.Input(all_defined_indices[i], input_name);
   }
   cmd.DynamicInputReg(indexput_func<ge::op::IndexPutV2>, {{all_defined_indices.size(), 4}})
-      .Output(result, (string)"x")
-      .Attr("_exclude_engines", (string)"AiCore")
+      .Output(result, x_str)
+      .Attr("_exclude_engines", aicore_str)
       .Attr("accumulate", accumulate)
       .Run();
 
@@ -216,70 +212,23 @@ at::Tensor& index_put_aicpu(
 
 at::Tensor& index_put_aicore(
     at::Tensor& self,
-    const c10::List<c10::optional<at::Tensor>>& indices,
-    std::vector<at::Tensor> all_defined_indices,
+    std::vector<at::Tensor> indices_expand,
+    at::SmallVector<int64_t, N> masks,
+    at::SmallVector<int64_t, N> bool_masks,
     const at::Tensor& value,
     bool accumulate) {
-  std::vector<at::Tensor> indices_expand;
-  at::SmallVector<int64_t, N> indices_expand_mask;
-  at::Tensor value_broadcast;
-  auto input_shape = op_infer::array_to_small_vector(self.sizes());
-  c10::List<c10::optional<at::Tensor>> shaped_indices;
-
-  // transfer input and index to 1 dim tensor if index is bool type, and the shape of input and index is the same
-  if (all_defined_indices.size() == 1) {
-    auto indices_shape = op_infer::array_to_small_vector(all_defined_indices[0].sizes());
-    if (all_defined_indices[0].scalar_type() == at::kBool && input_shape == indices_shape) {
-      self = self.reshape(-1);
-      shaped_indices.push_back(all_defined_indices[0].reshape(-1));
-      indices_expand = op_plugin::AdvanceIndex::npu_expand_tensors(self, shaped_indices);
-      indices_expand_mask = npu_expand_tensors_mask(self, shaped_indices);
-    }
-  }
-
-  if (shaped_indices.size() == 0) {
-    indices_expand = op_plugin::AdvanceIndex::npu_expand_tensors(self, indices);
-    indices_expand_mask = npu_expand_tensors_mask(self, indices);
-  }
-
   // value broadcast
   auto index_output_size = op_infer::index_npu_output_size(self, indices_expand);
   auto value_shape = op_infer::array_to_small_vector(value.sizes());
-  value_broadcast = (index_output_size != value_shape) ?
+  at::Tensor value_broadcast = (index_output_size != value_shape) ?
       op_plugin::npu_broadcast(value, index_output_size) : value;
 
-  // re-caculate mask
-  at::SmallVector<int64_t, N> masks;
-  for (c10::optional<at::Tensor> index_opt : indices_expand) {
-    if (index_opt.has_value()) {
-      const auto& index = *index_opt;
-      if (index.defined()) {
-        masks.emplace_back(1);
-      } else {
-        masks.emplace_back(0);
-      }
-    } else {
-      masks.emplace_back(0);
-    }
-  }
-
-  // after expand, check it through aicpu or aicore
-  bool aicpu_true = is_aicpu_valid(self, indices_expand, masks);
-  if (aicpu_true) {
-    index_put_aicpu(self, self, indices_expand, masks, value, accumulate);
-    self.copy_(self);
-  } else {
-    if (!npu_utils::check_match(&self)) {
+  if (!npu_utils::check_match(&self)) {
     at::Tensor contiguous_self = npu_utils::format_contiguous(self);
-    index_put_aicore_nocheck(contiguous_self, indices_expand, masks,
-        indices_expand_mask, value_broadcast, accumulate);
-    npu_utils::format_fresh_view(self, contiguous_self);
-    } else {
-      index_put_aicore_nocheck(self, indices_expand, masks, indices_expand_mask, value_broadcast, accumulate);
-    }
-  }
-  if (shaped_indices.size() != 0) {
-    self = self.reshape(input_shape);
+    index_put_aicore_nocheck(contiguous_self, indices_expand, masks, bool_masks, value_broadcast, accumulate);
+    self.copy_(contiguous_self);
+  } else {
+    index_put_aicore_nocheck(self, indices_expand, masks, bool_masks, value_broadcast, accumulate);
   }
   return self;
 }
@@ -310,11 +259,20 @@ at::Tensor& _index_put_impl_(
   if (self.device().type() == at::kCPU) {
     return at::native::_index_put_impl_(self, indices, value, accumulate, unsafe);
   }
-
   at::native::checkIndexTensorTypes(indices);
   at::SmallVector<int64_t, N> masks;
   std::vector<at::Tensor> all_defined_indices;
-  for (c10::optional<at::Tensor> index_opt : indices) {
+  std::vector<at::Tensor> indices_expand;
+  c10::List<c10::optional<at::Tensor>> indices_expand_list;
+  indices_expand = op_plugin::AdvanceIndex::npu_expand_tensors(self, indices);
+  for (at::Tensor index_opt : indices_expand) {
+    indices_expand_list.push_back(index_opt);
+  }
+  auto info = op_plugin::AdvanceIndex::make_info(self, indices_expand_list);
+  TORCH_CHECK(op_plugin::AdvanceIndex::is_expandable_to(value.sizes(), info.src.sizes()),
+      "shape mismatch: value tensor of shape ", value.sizes(),
+      " cannot be broadcast to indexing result of shape ", info.src.sizes());
+  for (c10::optional<at::Tensor> index_opt : indices_expand) {
     if (index_opt.has_value()) {
       const auto& index = *index_opt;
       if (index.defined()) {
@@ -327,7 +285,6 @@ at::Tensor& _index_put_impl_(
       masks.emplace_back(0);
     }
   }
-
   for (auto &all_defined_indice : all_defined_indices) {
     if (all_defined_indice.device() != self.device()) {
       all_defined_indice = all_defined_indice.to(self.device());
@@ -337,17 +294,14 @@ at::Tensor& _index_put_impl_(
   npu_preparation::CastBackToOriFormat(self);
   at::Tensor value_copy = value;
   at::Tensor self_copy = self;
-  // if the dtype of self is half, both self and value will be cast to float later.
-  if (value.scalar_type() != self.scalar_type() && self.scalar_type() != at::ScalarType::Half) {
-    value_copy = op_plugin::npu_dtype_cast(value, self.scalar_type());
-  }
   npu_preparation::CastBackToOriFormat(value_copy);
 
   bool aicpu_true = is_aicpu_valid(self, all_defined_indices, masks);
   if (aicpu_true) {
     index_put_aicpu(self_copy, self_copy, all_defined_indices, masks, value_copy, accumulate);
   } else {
-    index_put_aicore(self_copy, indices, all_defined_indices, value_copy, accumulate);
+    auto bool_mask = npu_expand_tensors_mask(self, indices);
+    index_put_aicore(self_copy, indices_expand, masks, bool_mask, value_copy, accumulate);
   }
   self.copy_(self_copy);
   return self;
