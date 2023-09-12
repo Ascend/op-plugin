6d67e74791c735e3bfee88f7b40df44ae2166e36
leiwang <leiwang5@huawei.com>
2023-09-12 22:02:02 +0800

    add the PTA2.1 for AtenIR zeros_like

--- op_plugin/ops/base_ops/opapi/ZerosLikeKernelNpuOpApi.cpp
+++ op_plugin/ops/base_ops/opapi/ZerosLikeKernelNpuOpApi.cpp
@@ -27,4 +27,30 @@ at::Tensor& zero_(at::Tensor& self) {
   return self;
 }
 
+at::Tensor zeros_like(
+    const at::Tensor &self,
+    c10::optional<c10::ScalarType> dtype_opt,
+    c10::optional<c10::Layout> layout_opt,
+    c10::optional<c10::Device> device_opt,
+    c10::optional<bool> pin_memory_opt,
+    c10::optional<c10::MemoryFormat> optional_memory_format)
+{
+  DO_COMPATIBILITY(aclnnInplaceZero, acl_op::zeros_like(self, dtype_opt, layout_opt, device_opt,
+                                                        pin_memory_opt, optional_memory_format));
+  auto device = device_opt.has_value() ? device_opt.value() : self.device();
+  if (!torch_npu::utils::is_npu(device)) {
+    auto result = at::empty_like(self, dtype_opt, layout_opt, device_opt, pin_memory_opt, optional_memory_format);
+    return result.fill_(0);
+  }
+  
+  auto other_options = c10::TensorOptions().dtype(dtype_opt)
+                                           .device(device_opt)
+                                           .layout(layout_opt)
+                                           .pinned_memory(pin_memory_opt);
+  auto options = self.options().merge_in(other_options);
+  at::Tensor result = npu_preparation::apply_tensor(self, options);
+  EXEC_NPU_CMD(aclnnInplaceZero, result);
+  return result;
+}
+
 }  // namespace op_api
