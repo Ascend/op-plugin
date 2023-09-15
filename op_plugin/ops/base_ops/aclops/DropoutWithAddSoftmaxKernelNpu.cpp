cf36859364ce02fd822993ec4039688d7aebf52d
赖长铃 <laichangling@huawei.com>
2023-09-15 04:08:20 +0000

    Revert "!393 Replace DropoutgenmaskV3 operator to support deterministic computation"
    This reverts commit 2a6a505ff68fc1d3857713915e2fb31ce2204b2b.

--- op_plugin/ops/base_ops/aclops/DropoutWithAddSoftmaxKernelNpu.cpp
+++ op_plugin/ops/base_ops/aclops/DropoutWithAddSoftmaxKernelNpu.cpp
@@ -27,26 +27,20 @@ namespace {
 at::Tensor dropout_genmask(const at::Tensor& self, at::Scalar prob) {
   uint32_t length = (self.numel() + 128 - 1) / 128 * 128;
   at::Tensor mask = npu_preparation::apply_tensor_with_format(
-      {length / 8},
+      {length},
       self.options().dtype(at::kByte),
       ACL_FORMAT_ND);
   at::IntArrayRef self_shape = self.sizes();
 
-  const auto gen = at_npu::detail::getDefaultNPUGenerator();
-  auto pair = at::check_generator<at_npu::NPUGeneratorImpl>(gen)->philox_engine_inputs(10);
-  const int64_t seed = pair.first;
-  const int64_t seed1 = 0;
-  const int64_t offset = pair.second;
-  at::SmallVector<int64_t, N> offsetList = {0, offset};
-
+  int64_t seed = 2;
+  int64_t seed2 = 0;
   at_npu::native::OpCommand cmd;
-  cmd.Name("StatelessDropOutGenMask")
+  cmd.Name("DropOutGenMaskV3")
       .Input(self_shape)
       .Input(prob, self.scalar_type(), npu_compile_type::MEMORY_HOST_COMPILE_DEPENDENT)
-      .Input(at::Scalar(seed), at::ScalarType::Int)
-      .Input(at::Scalar(seed1), at::ScalarType::Int)
-      .Input(offsetList, at::kLong, npu_compile_type::MEMORY_HOST_COMPILE_DEPENDENT)
       .Output(mask)
+      .Attr("seed", seed)
+      .Attr("seed2", seed2)
       .Run();
   return mask;
 }
