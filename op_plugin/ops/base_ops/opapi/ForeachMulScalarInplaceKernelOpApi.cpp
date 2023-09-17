aa3a430b7ca15fa8c0fd0d3816cd0685b507e187
wszh-01 <393493772@qq.com>
2023-09-17 21:12:14 +0800

    torch._foreach_mul_算子实现
    修改op_plugin/config下v1r11，v2r0，v2r1对应目录下的op_plugin_function.yaml文件，添加foreach_mul算子配置；
    
    添加ops/base_ops/opapi目录下算子实现.cpp文件

---
+++ op_plugin/ops/base_ops/opapi/ForeachMulScalarInplaceKernelOpApi.cpp
@@ -0,0 +1,21 @@
+#include <ATen/native/ForeachUtils.h>
+#include "op_plugin/OpApiInterface.h"
+#include "op_plugin/utils/op_api_common.h"
+
+namespace op_api {
+using npu_preparation = at_npu::native::OpPreparation;
+
+void _foreach_mul_(const at::TensorList self, const at::Scalar& scalar) {
+    if (self.empty()) {
+        return;
+    }
+    auto scalar_type = self[0].scalar_type();
+    bool is_support = true;
+    if (scalar_type != at::ScalarType::Half && scalar_type != at::ScalarType::Float && scalar_type != at::ScalarType::Int) {
+        is_support = false;
+        TORCH_CHECK(is_support, "input must be half, float or int32");
+    }
+    at::Tensor scalar_tensor = at_npu::native::CalcuOpUtil::CopyScalarToDevice(scalar, self[0].scalar_type());
+    EXEC_NPU_CMD(aclnnForeachMulScalarInplace, self, scalar_tensor);
+}
+}
