54f9685659558ed13b5ce809566dba4fd7ac967b
lifu <lifu12@h-partners.com>
2023-09-21 02:08:23 +0000

    !457 【hostapi】modify searchsorted aclnn name Merge pull request !457 from lifu/scalar_to_s

--- op_plugin/ops/base_ops/opapi/SearchsortedKernelNpuOpApi.cpp
+++ op_plugin/ops/base_ops/opapi/SearchsortedKernelNpuOpApi.cpp
@@ -64,11 +64,11 @@ at::Tensor searchsorted(
     bool right,
     const c10::optional<c10::string_view> side_opt,
     const c10::optional<at::Tensor>& sorter_opt) {
-  DO_COMPATIBILITY(aclnnSearchSortedScalar,
+  DO_COMPATIBILITY(aclnnSearchSorteds,
                    acl_op::searchsorted(sorted_sequence, self, out_int32, right, side_opt, sorter_opt));
   at::ScalarType scalar_type = out_int32 ? at::kInt : at::kLong;
   at::Tensor result = npu_preparation::apply_tensor_without_format({}, sorted_sequence.options().dtype(scalar_type));
-  EXEC_NPU_CMD(aclnnSearchSortedScalar, sorted_sequence, self, out_int32, right, sorter_opt, result);
+  EXEC_NPU_CMD(aclnnSearchSorteds, sorted_sequence, self, out_int32, right, sorter_opt, result);
   return result;
 }
 } // namespace op_api
