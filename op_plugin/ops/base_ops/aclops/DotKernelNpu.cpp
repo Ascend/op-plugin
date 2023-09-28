3430124fa90c7d15c9578b1beceb9a7ecb96c61a
朱维琛 <zhuweichen2@huawei.com>
2023-09-28 07:20:24 +0000

    !608 Add dtype interception for Dot Merge pull request !608 from 朱维琛/dot_fix

--- op_plugin/ops/base_ops/aclops/DotKernelNpu.cpp
+++ op_plugin/ops/base_ops/aclops/DotKernelNpu.cpp
@@ -23,40 +23,46 @@ using npu_utils = at_npu::native::NpuUtils;
 
 namespace {
 at::Tensor& dot_out_npu_nocheck(at::Tensor& result, const at::Tensor& self, const at::Tensor& tensor) {
-  at_npu::native::OpCommand cmd;
-  cmd.Name("Dot")
-      .Input(self)
-      .Input(tensor)
-      .Output(result)
-      .Run();
+    at_npu::native::OpCommand cmd;
+    cmd.Name("Dot")
+        .Input(self)
+        .Input(tensor)
+        .Output(result)
+        .Run();
 
-  return result;
+    return result;
 }
 } // namespace
 
 at::Tensor& dot_out(const at::Tensor& self, const at::Tensor& tensor, at::Tensor& result) {
-  c10::SmallVector<int64_t, N> output_size = {};
-  npu_preparation::CheckOut(
-      {self, tensor},
-      result,
-      self,
-      output_size);
+    auto self_dtype = self.scalar_type();
+    TORCH_CHECK(self_dtype != at::kInt && self_dtype != at::kByte && self_dtype != at::kChar,
+                "'dot_npu' not implemented for 'Int'");
+    c10::SmallVector<int64_t, N> output_size = {};
+    npu_preparation::CheckOut(
+        {self, tensor},
+        result,
+        self,
+        output_size);
 
-  if (!npu_utils::check_match(&result)) {
-    at::Tensor contiguous_result = npu_utils::format_contiguous(result);
-    dot_out_npu_nocheck(contiguous_result, self, tensor);
-    npu_utils::format_fresh_view(result, contiguous_result);
-  } else {
-    dot_out_npu_nocheck(result, self, tensor);
-  }
+    if (!npu_utils::check_match(&result)) {
+        at::Tensor contiguous_result = npu_utils::format_contiguous(result);
+        dot_out_npu_nocheck(contiguous_result, self, tensor);
+        npu_utils::format_fresh_view(result, contiguous_result);
+    } else {
+        dot_out_npu_nocheck(result, self, tensor);
+    }
 
-  return result;
+    return result;
 }
 
 at::Tensor dot(const at::Tensor& self, const at::Tensor& tensor) {
-  c10::SmallVector<int64_t, N> output_size = {};
-  at::Tensor result = npu_preparation::apply_tensor(self, output_size);
-  dot_out_npu_nocheck(result, self, tensor);
-  return result;
+    auto self_dtype = self.scalar_type();
+    TORCH_CHECK(self_dtype != at::kInt && self_dtype != at::kByte && self_dtype != at::kChar,
+                "'dot_npu' not implemented for 'Int'");
+    c10::SmallVector<int64_t, N> output_size = {};
+    at::Tensor result = npu_preparation::apply_tensor(self, output_size);
+    dot_out_npu_nocheck(result, self, tensor);
+    return result;
 }
 } // acl_op
