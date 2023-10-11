0386ab3d64356c5f7d7f61768e45d309b4c4d9e6
zhaolun <zhaolun3@hisilicon.com>
2023-10-11 13:25:53 +0000

    !741 [matmul]convert addmm to matmul with bias Merge pull request !741 from zhaolun/master

--- op_plugin/ops/base_ops/aclops/AddmmKernelNpu.cpp
+++ op_plugin/ops/base_ops/aclops/AddmmKernelNpu.cpp
@@ -16,10 +16,92 @@
 
 #include "op_plugin/AclOpsInterface.h"
 #include "op_plugin/utils/OpAdapter.h"
+#include "torch_npu/csrc/framework/utils/InternalFormatOpAdapter.h"
+#include "torch_npu/csrc/framework/utils/UtilForOpAdapter.h"
 
 namespace acl_op {
+using format_helper = at_npu::native::FormatHelper;
 using npu_preparation = at_npu::native::OpPreparation;
 using npu_utils = at_npu::native::NpuUtils;
+
+bool is_transpose_last_two_dims_flex(const at::Tensor &tensor)
+{
+    if (tensor.dim() != 2) {
+        return false;
+    }
+
+    int64_t dim1 = tensor.dim() - 1;
+    int64_t dim2 = tensor.dim() - 2;
+    if (tensor.stride(dim2) == 1 && tensor.stride(dim1) == tensor.size(dim2)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Pick out strict-transpose tensors from flex-transpose tensors.
+bool is_transpose_last_two_dims_strict(const at::Tensor &tensor, bool is_transpose_flex)
+{
+    auto base_sizes = torch_npu::NPUBridge::GetNpuStorageImpl(tensor)->get_npu_desc().base_sizes_;
+    if (is_transpose_flex && base_sizes.size() == tensor.dim() && tensor.size(-1) == base_sizes[tensor.dim() - 2] &&
+        tensor.size(-2) == base_sizes[tensor.dim() - 1]) {
+        return true;
+    }
+    return false;
+}
+
+// Refresh storage desc of view-transpose tensor.
+void set_transposed_npu_desc(at::Tensor &tensor)
+{
+    at::Tensor temp_transpose_Tensor = tensor.transpose(-1, -2);
+    at_npu::native::StorageDescHelper::SetDesc(tensor, temp_transpose_Tensor.sizes(), temp_transpose_Tensor.strides());
+}
+
+void mm_set_format_contiguous(at::Tensor &tensor, bool &is_tensor_trans_flex, bool &is_tensor_trans_strict)
+{
+    if (is_tensor_trans_flex) {
+        if (!is_tensor_trans_strict) {
+            // Matmul cannot directly deal with view+transposed tensor with NZ format,
+            // so Transdata is necessary
+            tensor = npu_preparation::CastBackToOriFormat(tensor);
+            // Storage desc of view-transpose tensors should be refreshed to be
+            // matched.
+            set_transposed_npu_desc(tensor);
+        }
+    } else {
+        tensor = npu_utils::format_contiguous_add_copy_optimize(tensor);
+    }
+}
+
+bool mm_check_split_k(const at::Tensor &self, const at::Tensor &mat2, bool is_support_nd_out)
+{
+    if (!is_support_nd_out || !(self.dtype() == at::ScalarType::Half && mat2.dtype() == at::ScalarType::Half) ||
+        !(format_helper::GetFormat(self) == ACL_FORMAT_ND && format_helper::GetFormat(mat2) == ACL_FORMAT_ND)) {
+        return false;
+    }
+    // split_k rule, maybe modified afterwards
+    const static int64_t kSplitKTimes = 8;
+    return self.size(1) >= kSplitKTimes * std::max(self.size(0), mat2.size(1));
+}
+
+bool is_transpose_both_inner_axis(const at::Tensor &self, const at::Tensor &mat2)
+{
+    const static int64_t kInnerAxisMaxLimit = 65535;
+    int64_t self_inner_axis = self.size(self.dim() - 1);
+    int64_t self_outer_axis = self.size(self.dim() - 2);
+    int64_t mat2_inner_axis = mat2.size(mat2.dim() - 1);
+    int64_t mat2_outer_axis = mat2.size(mat2.dim() - 2);
+    if (op_plugin::utils::is_transpose_last_two_dims(self)) {
+        self_inner_axis = self.size(self.dim() - 2);
+        self_outer_axis = self.size(self.dim() - 1);
+    }
+    if (op_plugin::utils::is_transpose_last_two_dims(mat2)) {
+        mat2_inner_axis = mat2.size(mat2.dim() - 2);
+        mat2_outer_axis = mat2.size(mat2.dim() - 1);
+    }
+    return self_inner_axis > kInnerAxisMaxLimit && self_outer_axis <= kInnerAxisMaxLimit &&
+           mat2_inner_axis > kInnerAxisMaxLimit && mat2_outer_axis <= kInnerAxisMaxLimit;
+}
 
 at::Tensor& addmm_out(
     const at::Tensor& self,
@@ -28,12 +110,35 @@ at::Tensor& addmm_out(
     const at::Scalar& beta,
     const at::Scalar& alpha,
     at::Tensor& result) {
-  at::Tensor mul_result = at::mul(mat1, alpha);
-  at::Tensor mm_result = at::mm(mul_result, mat2);
+    static const bool is_support_nd_out = c10_npu::GetSocVersion() >= c10_npu::SocVersion::Ascend910B1;
+    // k-cut is conflict with bias in mm
+    bool need_to_convert_bias = !mm_check_split_k(mat1, mat2, is_support_nd_out) && self.dim() == 1;
+    if (beta.toFloat() == 1.0 && alpha.toFloat() == 1.0 && need_to_convert_bias) {
+      bool is_mat1_t_flex = is_transpose_last_two_dims_flex(mat1);
+      bool is_mat2_t_flex = is_transpose_last_two_dims_flex(mat2);
+      bool is_mat1_t_strict = is_transpose_last_two_dims_strict(mat1, is_mat1_t_flex);
+      bool is_mat2_t_strict = is_transpose_last_two_dims_strict(mat2, is_mat2_t_flex);
+      at::Tensor contiguous_mat1 = mat1;
+      at::Tensor contiguous_mat2 = mat2;
+      mm_set_format_contiguous(contiguous_mat1, is_mat1_t_flex, is_mat1_t_strict);
+      mm_set_format_contiguous(contiguous_mat2, is_mat2_t_flex, is_mat2_t_strict);
+      at_npu::native::OpCommand cmd;
+      cmd.Name("MatMul")
+          .InputWithoutContiguous(contiguous_mat1)
+          .InputWithoutContiguous(contiguous_mat2)
+          .Input(self)
+          .Output(result)
+          .Attr("transpose_x1", is_mat1_t_flex)
+          .Attr("transpose_x2", is_mat2_t_flex)
+          .Run();
+      return result;
+    }
+    at::Tensor mul_result = at::mul(mat1, alpha);
+    at::Tensor mm_result = at::mm(mul_result, mat2);
 
-  // matmul*alpha+self*beta
-  at::add_out(result, mm_result, self, beta);
-  return result;
+    // matmul*alpha+self*beta
+    at::add_out(result, mm_result, self, beta);
+    return result;
 }
 
 at::Tensor addmm(
@@ -42,11 +147,11 @@ at::Tensor addmm(
     const at::Tensor& mat2,
     const at::Scalar& beta,
     const at::Scalar& alpha) {
-  auto output_size = op_infer::addmm_npu_output_size(self, mat1, mat2, beta, alpha);
-  at::Tensor result = npu_preparation::apply_tensor(output_size, self.options(), self);
+    auto output_size = op_infer::addmm_npu_output_size(self, mat1, mat2, beta, alpha);
+    at::Tensor result = npu_preparation::apply_tensor(output_size, self.options(), self);
 
-  acl_op::addmm_out(self, mat1, mat2, beta, alpha, result);
-  return result;
+    acl_op::addmm_out(self, mat1, mat2, beta, alpha, result);
+    return result;
 }
 
 at::Tensor& addmm_(
@@ -55,14 +160,14 @@ at::Tensor& addmm_(
     const at::Tensor& mat2,
     const at::Scalar& beta,
     const at::Scalar& alpha) {
-  npu_preparation::CheckMemory({self, mat1, mat2}, {self});
-  if (!npu_utils::check_match(&self)) {
-    at::Tensor contiguous_self = npu_utils::format_contiguous(self);
-    acl_op::addmm_out(contiguous_self, mat1, mat2, beta, alpha, contiguous_self);
-    npu_utils::format_fresh_view(self, contiguous_self);
-  } else {
-    acl_op::addmm_out(self, mat1, mat2, beta, alpha, self);
-  }
-  return self;
+    npu_preparation::CheckMemory({self, mat1, mat2}, {self});
+    if (!npu_utils::check_match(&self)) {
+      at::Tensor contiguous_self = npu_utils::format_contiguous(self);
+      acl_op::addmm_out(contiguous_self, mat1, mat2, beta, alpha, contiguous_self);
+      npu_utils::format_fresh_view(self, contiguous_self);
+    } else {
+      acl_op::addmm_out(self, mat1, mat2, beta, alpha, self);
+    }
+    return self;
 }
 } // namespace acl_op
