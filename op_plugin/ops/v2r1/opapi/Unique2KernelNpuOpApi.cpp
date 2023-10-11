b2471c0bdd3e9979470461fb72eec7af7a133720
张向龙3 <zhangxianglong3@huawei.com>
2023-10-11 08:00:36 +0000

    !730 [HostAPI]修改unique2在1.11版本上float16精度问题 Merge pull request !730 from 张向龙3/1011

--- op_plugin/ops/v2r1/opapi/Unique2KernelNpuOpApi.cpp
+++ op_plugin/ops/v2r1/opapi/Unique2KernelNpuOpApi.cpp
@@ -21,17 +21,21 @@
 namespace op_api {
 using npu_preparation = at_npu::native::OpPreparation;
 
-std::tuple<at::Tensor, at::Tensor> _unique(
+std::tuple<at::Tensor, at::Tensor, at::Tensor> _unique2(
     const at::Tensor& self,
     bool sorted,
-    bool return_inverse) {
-  DO_COMPATIBILITY(aclnnUnique, acl_op::_unique(self, sorted, return_inverse));
+    bool return_inverse,
+    bool return_counts) {
+  DO_COMPATIBILITY(aclnnUnique2, acl_op::_unique2(self, sorted, return_inverse, return_counts));
   at::Tensor y = npu_preparation::apply_tensor_without_format(self, self.numel());
-  at::Tensor y_inverse = return_inverse
+  at::Tensor y_inverse = (return_inverse || return_counts)
                             ? npu_preparation::apply_tensor_without_format(self.sizes(),
                                                                            self.options().dtype(at::kLong))
                             : npu_preparation::apply_tensor_without_format({0}, self.options().dtype(at::kLong));
-
+  at::Tensor y_counts = return_counts ? npu_preparation::apply_tensor_without_format(self.numel(),
+                                                                                     self.options().dtype(at::kLong))
+                                      : npu_preparation::apply_tensor_without_format({0},
+                                                                                     self.options().dtype(at::kLong));
   static auto opApiFuncAddr = []() {
     auto ret = GetOpApiFuncAddr("aclGetViewShape");
     TORCH_CHECK(ret != nullptr);
@@ -39,17 +43,20 @@ std::tuple<at::Tensor, at::Tensor> _unique(
   }();
   using aclGetViewShapeFunc = int (*)(const aclTensor* tensor, int64_t** view_dims, uint64_t* view_dims_num);
   auto aclGetViewShape = reinterpret_cast<aclGetViewShapeFunc>(opApiFuncAddr);
-  sorted = true;
-  auto npuAclParams = EXEC_NPU_CMD_SYNC(aclnnUnique, self, sorted, return_inverse, y, y_inverse);
+  auto npuAclParams = EXEC_NPU_CMD_SYNC(aclnnUnique2, self, sorted, return_inverse, return_counts, y, y_inverse,
+                                        y_counts);
   int64_t* view_dims = nullptr;
   uint64_t view_dim_num = 0;
-  auto ret = aclGetViewShape(npuAclParams.Get<3>(), &view_dims, &view_dim_num);
+  auto ret = aclGetViewShape(npuAclParams.Get<4>(), &view_dims, &view_dim_num);
   TORCH_CHECK(ret == 0, "aclGetViewShape failed.");
   c10::SmallVector<int64_t, SIZE> output_size(view_dims, view_dims + view_dim_num);
   y.resize_(output_size);
+  if (return_counts) {
+    y_counts.resize_(output_size);
+  }
   delete view_dims;
   view_dims = nullptr;
-  return std::tuple<at::Tensor, at::Tensor>(y, y_inverse);
+  return std::tuple<at::Tensor, at::Tensor, at::Tensor>(y, y_inverse, y_counts);
 }
 
 } // namespace op_api
