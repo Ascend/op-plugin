b00754ae0510438fc38d1ab112b990190449bd8d
zhaolei156 <zhaolei156@huawei.com>
2023-10-19 06:13:26 +0000

    !790 Fix codecheck Merge pull request !790 from zhaolei156/clean

--- op_plugin/ops/base_ops/aclops/MvKernelNpu.cpp
+++ op_plugin/ops/base_ops/aclops/MvKernelNpu.cpp
@@ -24,50 +24,51 @@ using npu_preparation = at_npu::native::OpPreparation;
 using npu_utils = at_npu::native::NpuUtils;
 
 namespace {
-at::Tensor& mv_out_npu_nocheck(at::Tensor& result, const at::Tensor& self, const at::Tensor& vec) {
-  bool is_self_t = op_plugin::utils::is_transpose_last_two_dims(self);
-  at::Tensor contiguous_self = is_self_t ? self : npu_utils::format_contiguous(self);
-  at::Tensor vec_t = at::unsqueeze(vec, 1);
+at::Tensor &mv_out_npu_nocheck(at::Tensor &result, const at::Tensor &self, const at::Tensor &vec)
+{
+    bool is_self_t = op_plugin::utils::is_transpose_last_two_dims(self);
+    at::Tensor contiguous_self = is_self_t ? self : npu_utils::format_contiguous(self);
+    at::Tensor vec_t = at::unsqueeze(vec, 1);
 
-  at_npu::native::OpCommand cmd;
-  cmd.Name("MatMul")
-      .InputWithoutContiguous(contiguous_self)
-      .Input(vec_t)
-      .Attr("transpose_x1", is_self_t)
-      .Attr("transpose_x2", false)
-      .Output(result)
-      .Run();
+    at_npu::native::OpCommand cmd;
+    cmd.Name("MatMul")
+        .InputWithoutContiguous(contiguous_self)
+        .Input(vec_t)
+        .Attr("transpose_x1", is_self_t)
+        .Attr("transpose_x2", false)
+        .Output(result)
+        .Run();
 
-  at_npu::native::npu_fast_reshape_(result);
-  return result;
+    at_npu::native::npu_fast_reshape_(result);
+    return result;
 }
 } // namespace
 
-at::Tensor& mv_out(const at::Tensor& self, const at::Tensor& vec, at::Tensor& result) {
-  TORCH_CHECK(self.dim() >= 1, "mv(): input tensor must has at least 1 dimension, but got ", self.dim(), " dimensions");
-  npu_preparation::CheckOut(
-      {self},
-      result,
-      self,
-      {self.size(0)});
+at::Tensor &mv_out(const at::Tensor &self, const at::Tensor &vec, at::Tensor &result)
+{
+    TORCH_CHECK(self.dim() >= 1, "mv(): input tensor must has at least 1 dimension, but got ", self.dim(),
+                " dimensions");
+    npu_preparation::CheckOut({self}, result, self, {self.size(0)});
 
-  result.unsqueeze_(1);
-  if (!npu_utils::check_match(&result)) {
-    at::Tensor contiguous_result = npu_utils::format_contiguous(result);
-    mv_out_npu_nocheck(contiguous_result, self, vec);
-    npu_utils::format_fresh_view(result, contiguous_result);
-  } else {
-    mv_out_npu_nocheck(result, self, vec);
-  }
-  result.squeeze_(1);
-  return result;
+    result.unsqueeze_(1);
+    if (!npu_utils::check_match(&result)) {
+        at::Tensor contiguous_result = npu_utils::format_contiguous(result);
+        mv_out_npu_nocheck(contiguous_result, self, vec);
+        npu_utils::format_fresh_view(result, contiguous_result);
+    } else {
+        mv_out_npu_nocheck(result, self, vec);
+    }
+    result.squeeze_(1);
+    return result;
 }
 
-at::Tensor mv(const at::Tensor& self, const at::Tensor& vec) {
-  TORCH_CHECK(self.dim() >= 1, "mv(): input tensor must has at least 1 dimension, but got ", self.dim(), " dimensions");
-  at::Tensor result = npu_preparation::apply_tensor(self, {self.size(0), 1});
-  mv_out_npu_nocheck(result, self, vec);
-  result.squeeze_(1);
-  return result;
+at::Tensor mv(const at::Tensor &self, const at::Tensor &vec)
+{
+    TORCH_CHECK(self.dim() >= 1, "mv(): input tensor must has at least 1 dimension, but got ", self.dim(),
+                " dimensions");
+    at::Tensor result = npu_preparation::apply_tensor(self, {self.size(0), 1});
+    mv_out_npu_nocheck(result, self, vec);
+    result.squeeze_(1);
+    return result;
 }
 } // namespace acl_op
