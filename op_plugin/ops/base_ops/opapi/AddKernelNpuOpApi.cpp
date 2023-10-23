165374b1f45e16c1feb3da4ae2615acf6634653f
张向龙3 <zhangxianglong3@huawei.com>
2023-10-23 11:20:30 +0000

    !795 Fix some CodeCheck questions Merge pull request !795 from 张向龙3/1019_codecheck

--- op_plugin/ops/base_ops/opapi/AddKernelNpuOpApi.cpp
+++ op_plugin/ops/base_ops/opapi/AddKernelNpuOpApi.cpp
@@ -19,117 +19,134 @@
 #include "op_plugin/utils/op_api_common.h"
 
 namespace op_api {
-using npu_preparation = at_npu::native::OpPreparation;
+    using npu_preparation = at_npu::native::OpPreparation;
 
-inline void alpha_check_npu(const at::ScalarType dtype, at::Scalar alpha) {
-  TORCH_CHECK(!alpha.isBoolean() || dtype == at::ScalarType::Bool,
-              "Boolean alpha only supported for Boolean results.");
-  TORCH_CHECK(isFloatingType(dtype) || alpha.isIntegral(true),
-              "For integral input tensors, argument alpha must not be a floating point number.");
-}
+    inline void alpha_check_npu(const at::ScalarType dtype, at::Scalar alpha)
+    {
+      TORCH_CHECK(!alpha.isBoolean() || dtype == at::ScalarType::Bool,
+                  "Boolean alpha only supported for Boolean results.");
+      TORCH_CHECK(isFloatingType(dtype) || alpha.isIntegral(true),
+                  "For integral input tensors, argument alpha must not be a floating point number.");
+    }
 
-static at::Tensor& add_out_npu_nocheck(
-    const at::Tensor& self,
-    const at::Tensor& other,
-    const at::Scalar& alpha,
-    at::Tensor& result) {
-  // executing the NPU operator
-  if (other.dim() == 0 && !torch_npu::utils::is_npu(other)) {
-    c10::Scalar others = other.item();
-    EXEC_NPU_CMD(aclnnAdds, self, others, alpha, result);
-  } else {
-    EXEC_NPU_CMD(aclnnAdd, self, other, alpha, result);
-  }
-  return result;
-}
+    static at::Tensor &add_out_npu_nocheck(
+        const at::Tensor &self,
+        const at::Tensor &other,
+        const at::Scalar &alpha,
+        at::Tensor &result)
+    {
+      // executing the NPU operator
+      if (other.dim() == 0 && !torch_npu::utils::is_npu(other))
+      {
+        c10::Scalar others = other.item();
+        EXEC_NPU_CMD(aclnnAdds, self, others, alpha, result);
+      }
+      else
+      {
+        EXEC_NPU_CMD(aclnnAdd, self, other, alpha, result);
+      }
+      return result;
+    }
 
-static at::Tensor& inplace_add_out_npu_no_check(
-    at::Tensor& self,
-    const at::Tensor& other,
-    const at::Scalar& alpha) {
-  // check if other scalar tensor
-  if (other.dim() == 0 && !torch_npu::utils::is_npu(other)) {
-    c10::Scalar other_scalar = other.item();
-    EXEC_NPU_CMD(aclnnInplaceAdds, self, other_scalar, alpha);
-  } else {
-    EXEC_NPU_CMD(aclnnInplaceAdd, self, other, alpha);
-  }
-  return self;
-}
+    static at::Tensor &inplace_add_out_npu_no_check(
+        at::Tensor &self,
+        const at::Tensor &other,
+        const at::Scalar &alpha)
+    {
+      // check if other scalar tensor
+      if (other.dim() == 0 && !torch_npu::utils::is_npu(other))
+      {
+        c10::Scalar other_scalar = other.item();
+        EXEC_NPU_CMD(aclnnInplaceAdds, self, other_scalar, alpha);
+      }
+      else
+      {
+        EXEC_NPU_CMD(aclnnInplaceAdd, self, other, alpha);
+      }
+      return self;
+    }
 
-static at::Tensor self_tensor_to_device(const at::Tensor& tensor, const at::ScalarType result_type) {
-  if (npu_preparation::is_scalar_wrapped_to_tensor(tensor)) {
-    at::Scalar scalar = tensor.item();
-    return npu_preparation::copy_scalar_to_device(scalar, result_type);
-  }
-  return tensor;
-}
+    static at::Tensor self_tensor_to_device(const at::Tensor &tensor, const at::ScalarType result_type)
+    {
+      if (npu_preparation::is_scalar_wrapped_to_tensor(tensor))
+      {
+        at::Scalar scalar = tensor.item();
+        return npu_preparation::copy_scalar_to_device(scalar, result_type);
+      }
+      return tensor;
+    }
 
-static at::Tensor add_dest_output(const at::Tensor& self, const at::Tensor& other) {
-  bool isSelfWrapped = npu_preparation::is_scalar_wrapped_to_tensor(self);
-  return isSelfWrapped ? other : self;
-}
+    static at::Tensor add_dest_output(const at::Tensor &self, const at::Tensor &other)
+    {
+      bool isSelfWrapped = npu_preparation::is_scalar_wrapped_to_tensor(self);
+      return isSelfWrapped ? other : self;
+    }
 
-at::Tensor add(const at::Tensor& self, const at::Tensor& other, const at::Scalar& alpha) {
-  DO_COMPATIBILITY(aclnnAdd, acl_op::add(self, other, alpha));
-  DO_COMPATIBILITY(aclnnAdds, acl_op::add(self, other, alpha));
-  alpha_check_npu(self.scalar_type(), alpha);
-  // calculate the output size
-  at::Tensor output_tensor = add_dest_output(self, other);
-  auto output_size = op_infer::broadcast_ops_npu_output_size(self, other);
-  at::ScalarType result_type = at::native::result_type(self, other);
-  at::Tensor self_cp = self_tensor_to_device(self, result_type);
-  // construct the output tensor of the NPU
-  at::Tensor result =
-      npu_preparation::apply_tensor_without_format(output_size, output_tensor.options().dtype(result_type));
-  // calculate the output result of the NPU
-  add_out_npu_nocheck(self_cp, other, alpha, result);
-  return result;
-}
+    at::Tensor add(const at::Tensor &self, const at::Tensor &other, const at::Scalar &alpha)
+    {
+      DO_COMPATIBILITY(aclnnAdd, acl_op::add(self, other, alpha));
+      DO_COMPATIBILITY(aclnnAdds, acl_op::add(self, other, alpha));
+      alpha_check_npu(self.scalar_type(), alpha);
+      // calculate the output size
+      at::Tensor output_tensor = add_dest_output(self, other);
+      auto output_size = op_infer::broadcast_ops_npu_output_size(self, other);
+      at::ScalarType result_type = at::native::result_type(self, other);
+      at::Tensor self_cp = self_tensor_to_device(self, result_type);
+      // construct the output tensor of the NPU
+      at::Tensor result =
+          npu_preparation::apply_tensor_without_format(output_size, output_tensor.options().dtype(result_type));
+      // calculate the output result of the NPU
+      add_out_npu_nocheck(self_cp, other, alpha, result);
+      return result;
+    }
 
-at::Tensor add(const at::Tensor& self, const at::Scalar& other, const at::Scalar& alpha) {
-  DO_COMPATIBILITY(aclnnAdds, acl_op::add(self, other, alpha));
-  alpha_check_npu(self.scalar_type(), alpha);
-  // calculate the output size
-  auto output_size = op_infer::input_same_output_size(self);
-  at::ScalarType result_type = at::native::result_type(self, other);
-  // construct the output tensor of the NPU
-  at::Tensor result = npu_preparation::apply_tensor_without_format(output_size, self.options().dtype(result_type));
-  // calculate the output result of the NPU
-  EXEC_NPU_CMD(aclnnAdds, self, other, alpha, result);
-  return result;
-}
+    at::Tensor add(const at::Tensor &self, const at::Scalar &other, const at::Scalar &alpha)
+    {
+      DO_COMPATIBILITY(aclnnAdds, acl_op::add(self, other, alpha));
+      alpha_check_npu(self.scalar_type(), alpha);
+      // calculate the output size
+      auto output_size = op_infer::input_same_output_size(self);
+      at::ScalarType result_type = at::native::result_type(self, other);
+      // construct the output tensor of the NPU
+      at::Tensor result = npu_preparation::apply_tensor_without_format(output_size, self.options().dtype(result_type));
+      // calculate the output result of the NPU
+      EXEC_NPU_CMD(aclnnAdds, self, other, alpha, result);
+      return result;
+    }
 
-at::Tensor& add_out(
-    const at::Tensor& self,
-    const at::Tensor& other,
-    const at::Scalar& alpha,
-    at::Tensor& result) {
-  DO_COMPATIBILITY(aclnnAdd, acl_op::add_out(self, other, alpha, result));
-  DO_COMPATIBILITY(aclnnAdds, acl_op::add_out(self, other, alpha, result));
-  bool isSelfWrapped = npu_preparation::is_scalar_wrapped_to_tensor(self);
-  at::Tensor output_tensor = isSelfWrapped ? other : self;
-  auto output_size = op_infer::broadcast_ops_npu_output_size(self, other);
-  at::ScalarType result_type = at::native::result_type(self, other);
-  at::Tensor self_cp = self_tensor_to_device(self, result_type);
+    at::Tensor &add_out(
+        const at::Tensor &self,
+        const at::Tensor &other,
+        const at::Scalar &alpha,
+        at::Tensor &result)
+    {
+      DO_COMPATIBILITY(aclnnAdd, acl_op::add_out(self, other, alpha, result));
+      DO_COMPATIBILITY(aclnnAdds, acl_op::add_out(self, other, alpha, result));
+      bool isSelfWrapped = npu_preparation::is_scalar_wrapped_to_tensor(self);
+      at::Tensor output_tensor = isSelfWrapped ? other : self;
+      auto output_size = op_infer::broadcast_ops_npu_output_size(self, other);
+      at::ScalarType result_type = at::native::result_type(self, other);
+      at::Tensor self_cp = self_tensor_to_device(self, result_type);
 
-  npu_preparation::check_tensor({self}, result, result, output_size);
-  npu_preparation::check_memory({self, other}, {result});
-  add_out_npu_nocheck(self_cp, other, alpha, result);
-  return result;
-}
+      npu_preparation::check_tensor({self}, result, result, output_size);
+      npu_preparation::check_memory({self, other}, {result});
+      add_out_npu_nocheck(self_cp, other, alpha, result);
+      return result;
+    }
 
-at::Tensor& add_(at::Tensor& self, const at::Tensor& other, const at::Scalar& alpha) {
-  DO_COMPATIBILITY(aclnnInplaceAdd, acl_op::add_(self, other, alpha));
-  DO_COMPATIBILITY(aclnnInplaceAdds, acl_op::add_(self, other, alpha));
-  npu_preparation::check_memory({self, other}, {self});
-  inplace_add_out_npu_no_check(self, other, alpha);
-  return self;
-}
+    at::Tensor &add_(at::Tensor &self, const at::Tensor &other, const at::Scalar &alpha)
+    {
+      DO_COMPATIBILITY(aclnnInplaceAdd, acl_op::add_(self, other, alpha));
+      DO_COMPATIBILITY(aclnnInplaceAdds, acl_op::add_(self, other, alpha));
+      npu_preparation::check_memory({self, other}, {self});
+      inplace_add_out_npu_no_check(self, other, alpha);
+      return self;
+    }
 
-at::Tensor& add_(at::Tensor& self, const at::Scalar& other, const at::Scalar& alpha) {
-  DO_COMPATIBILITY(aclnnInplaceAdds, acl_op::add_(self, other, alpha));
-  EXEC_NPU_CMD(aclnnInplaceAdds, self, other, alpha);
-  return self;
-}
+    at::Tensor &add_(at::Tensor &self, const at::Scalar &other, const at::Scalar &alpha)
+    {
+      DO_COMPATIBILITY(aclnnInplaceAdds, acl_op::add_(self, other, alpha));
+      EXEC_NPU_CMD(aclnnInplaceAdds, self, other, alpha);
+      return self;
+    }
 } // namespace op_api
--- op_plugin/utils/custom_functions/opapi/SumKernelNpuOpApi.cpp
+++ op_plugin/utils/custom_functions/opapi/SumKernelNpuOpApi.cpp
@@ -19,45 +19,49 @@
 #include "op_plugin/utils/op_api_common.h"
 
 namespace op_api {
-using npu_preparation = at_npu::native::OpPreparation;
+    using npu_preparation = at_npu::native::OpPreparation;
 
-at::Tensor& sum_out_common_nocheck(const at::Tensor &self,
-                                   at::IntArrayRef dim,
-                                   bool keepdim,
-                                   c10::optional<c10::ScalarType> dtype,
-                                   at::Tensor &result) {
-  DO_COMPATIBILITY(aclnnReduceSum, acl_op::sum_out(self, dim, keepdim, dtype, result));
-  auto output_size = op_infer::sum_npu_output_size(self, dim, keepdim);
-  auto res_type = dtype.has_value() ? dtype.value() : result.scalar_type();
-  npu_preparation::check_tensor({self}, result, res_type, output_size);
+    at::Tensor &sum_out_common_nocheck(const at::Tensor &self,
+                                       at::IntArrayRef dim,
+                                       bool keepdim,
+                                       c10::optional<c10::ScalarType> dtype,
+                                       at::Tensor &result)
+    {
+      DO_COMPATIBILITY(aclnnReduceSum, acl_op::sum_out(self, dim, keepdim, dtype, result));
+      auto output_size = op_infer::sum_npu_output_size(self, dim, keepdim);
+      auto res_type = dtype.has_value() ? dtype.value() : result.scalar_type();
+      npu_preparation::check_tensor({self}, result, res_type, output_size);
 
-  EXEC_NPU_CMD(aclnnReduceSum, self, dim, keepdim, res_type, result);
-  return result;
-}
-
-at::Tensor sum_common_nocheck(const at::Tensor &self,
-                              at::IntArrayRef dim,
-                              bool keepdim,
-                              c10::optional<c10::ScalarType> dtype) {
-  DO_COMPATIBILITY(aclnnReduceSum, acl_op::sum(self, dim, keepdim, dtype));
-  auto output_size = op_infer::reduce_ops_npu_output_size(self, dim, keepdim);
-  auto self_size = self.sizes();
-  auto out_type = self.scalar_type();
-
-  if (dtype.has_value()) {
-    out_type = dtype.value();
-  } else if (isIntegralType(out_type, true)) {
-    out_type = at::kLong;
-  }
-
-  for (uint64_t i = 0; i < self_size.size(); i++) {
-    if (self_size[i] == 0) {
-      return at::zeros(output_size, self.options().dtype(out_type));
+      EXEC_NPU_CMD(aclnnReduceSum, self, dim, keepdim, res_type, result);
+      return result;
     }
-  }
 
-  at::Tensor result = npu_preparation::apply_tensor_without_format(output_size, self.options().dtype(out_type));
-  EXEC_NPU_CMD(aclnnReduceSum, self, dim, keepdim, out_type, result);
-  return result;
-}
+    at::Tensor sum_common_nocheck(const at::Tensor &self,
+                                  at::IntArrayRef dim,
+                                  bool keepdim,
+                                  c10::optional<c10::ScalarType> dtype)
+    {
+      DO_COMPATIBILITY(aclnnReduceSum, acl_op::sum(self, dim, keepdim, dtype));
+      auto output_size = op_infer::reduce_ops_npu_output_size(self, dim, keepdim);
+      auto self_size = self.sizes();
+      auto out_type = self.scalar_type();
+
+      if (dtype.has_value()) {
+        out_type = dtype.value();
+      } else if (isIntegralType(out_type, true)) {
+        out_type = at::kLong;
+      }
+
+      for (uint64_t i = 0; i < self_size.size(); i++)
+      {
+        if (self_size[i] == 0)
+        {
+          return at::zeros(output_size, self.options().dtype(out_type));
+        }
+      }
+
+      at::Tensor result = npu_preparation::apply_tensor_without_format(output_size, self.options().dtype(out_type));
+      EXEC_NPU_CMD(aclnnReduceSum, self, dim, keepdim, out_type, result);
+      return result;
+    }
 } // namespace op_api
