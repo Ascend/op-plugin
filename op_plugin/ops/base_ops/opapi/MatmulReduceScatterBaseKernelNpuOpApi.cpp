75b78a9f1000285932eddc2dc164e64b83fe5183
白亮 <bailiang13@huawei.com>
2023-11-24 03:21:08 +0000

    !994 merge from master：add api all_gather_base_mm and mm_reduce_scatter_base Merge pull request !994 from 白亮/5.0.0

---
+++ op_plugin/ops/base_ops/opapi/MatmulReduceScatterBaseKernelNpuOpApi.cpp
@@ -0,0 +1,43 @@
+// Copyright (c) 2023 Huawei Technologies Co., Ltd
+// Copyright (c) 2019, Facebook CORPORATION.
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "op_plugin/OpApiInterface.h"
+#include "op_plugin/utils/op_api_common.h"
+
+namespace op_api {
+
+at::Tensor npu_mm_reduce_scatter_base(const at::Tensor &self, const at::Tensor &x2, c10::string_view hcom,
+                                      int64_t world_size, c10::string_view reduce_op,
+                                      const c10::optional<at::Tensor> &bias, int64_t comm_turn)
+{
+    TORCH_CHECK(world_size > 0, "world_size should be greater than 0, but the actual value is ", world_size);
+    TORCH_CHECK(self.dim() == 2 && x2.dim() == 2, "Both inputs of mm are required to be 2D, but the actual inputs are ",
+                self.dim(), "D and ", x2.dim(), "D");
+    TORCH_CHECK(self.size(1) == x2.size(0),
+                "The K-axis in the two inputs of Matmul must be equal, but in reality, the K-axis of x1 is ",
+                self.size(1), " and the K-axis of x2 is ", x2.size(0));
+    TORCH_CHECK(self.size(0) % world_size == 0, "The M-axis in input of Matmul should be be divisible by world_size");
+    auto output_size = {self.size(0) / world_size, x2.size(1)};
+    auto result = at_npu::native::OpPreparation::apply_tensor_without_format(output_size, self.options());
+    char *reduce_op_ptr = const_cast<char *>(reduce_op.data());
+    char *hcom_ptr = const_cast<char *>(hcom.data());
+    const at::Tensor &bias_real = bias.value_or(at::Tensor());
+    int64_t stream_mode = ACL_STOP_ON_FAILURE;
+    EXEC_NPU_CMD(aclnnMatmulReduceScatter, self, x2, bias_real, hcom_ptr, reduce_op_ptr, comm_turn, stream_mode,
+                 result);
+    return result;
+}
+}  // namespace op_api
---
+++ test/test_custom_ops/test_all_gather_base_mm.py
@@ -0,0 +1,100 @@
+import os
+
+import numpy as np
+import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
+import torch_npu
+
+from torch_npu.testing.testcase import TestCase, run_tests
+from torch_npu.testing.common_utils import create_common_tensor
+from torch_npu.testing.common_distributed import skipIfUnsupportMultiNPU
+
+
+class TestAllGatherBaseMm(TestCase):
+
+    @classmethod
+    def _init_dist_hccl(cls, rank, world_size):
+        os.environ['MASTER_ADDR'] = '127.0.0.1'
+        os.environ['MASTER_PORT'] = '50000'
+        os.environ['HCCL_WHITELIST_DISABLE'] = '1'
+        torch_npu.npu.set_device(rank)
+        dist.init_process_group(backend='hccl', world_size=world_size, rank=rank)
+        return dist
+
+    @classmethod
+    def _test_npu_all_gather_base_mm(cls, rank, input_list):
+        x1, x2, world_size, init_pg, c2p = input_list
+        pg = init_pg(rank, world_size)
+        group = pg.distributed_c10d._get_default_group()
+        if torch.__version__ > '2.0':
+            hcom_name = group._get_backend(torch.device('npu')).get_hccl_comm_name(rank)
+        else:
+            hcom_name = group.get_hccl_comm_name(rank)
+
+        x1 = x1.npu()
+        x2 = x2.npu()
+        out, gather_out = torch_npu.npu_all_gather_base_mm(x1,
+                                                           x2,
+                                                           hcom_name,
+                                                           world_size,
+                                                           bias=None,
+                                                           gather_index=0,
+                                                           gather_output=True,
+                                                           comm_turn=0)
+
+        c2p.put((rank, out.cpu(), gather_out.cpu()))
+        pg.barrier()
+
+    def _test_multiprocess(self, f, init_pg, input_list):
+        expt_out_list, expt_gather, x1, x2, world_size = input_list
+        ctx = mp.get_context('spawn')
+        c2p = ctx.Queue(world_size)
+        ps = []
+
+        for i in range(world_size):
+            p = ctx.Process(
+                target=f,
+                args=(i, [x1, x2, world_size, init_pg, c2p]))
+            p.start()
+            ps.append(p)
+
+        for _ in range(world_size):
+            rank, output, gather_output = c2p.get()
+            self.assertEqual(output, expt_out_list[rank],
+                             ("rank {} Expect receive tensor {} but got {}.").format(rank, expt_out_list[rank], output))
+            self.assertEqual(gather_output, expt_gather,
+                             ("rank {} Expect receive tensor {} but got {}.").format(rank, expt_gather, gather_output))
+
+        for p in ps:
+            p.join()
+
+    def _construct_excepted_result(self, x1_list, x2_list, world_size):
+        gather_out = torch.cat(x1_list)
+        out_list = []
+        out_dype = gather_out.dtype
+        for i in range(world_size):
+            out_list.append(torch.matmul(gather_out.to(torch.float), x2_list[i].to(torch.float)).to(out_dype))
+        return out_list, gather_out
+
+    @skipIfUnsupportMultiNPU(8)
+    def test_npu_all_gather_base_mm(self):
+        world_size = 8
+        dtype = np.float16
+        data_format = -1
+        x1_shape = [dtype, data_format, [16, 512]]
+        x2_shape = [dtype, data_format, [512, 256]]
+        x1_list = []
+        x2_list = []
+        for _ in range(world_size):
+            x1, _ = create_common_tensor(x1_shape, -1, 1)
+            x2, _ = create_common_tensor(x2_shape, -1, 1)
+            x1_list.append(x1)
+            x2_list.append(x2)
+        expt_out_list, expt_gather = self._construct_excepted_result(x1_list, x2_list, world_size)
+        self._test_multiprocess(TestAllGatherBaseMm._test_npu_all_gather_base_mm,
+                                TestAllGatherBaseMm._init_dist_hccl, [expt_out_list, expt_gather, x1, x2, world_size])
+
+
+if __name__ == '__main__':
+    run_tests()
