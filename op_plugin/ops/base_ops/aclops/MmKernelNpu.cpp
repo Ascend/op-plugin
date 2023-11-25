5b13b7244c72e62188129cf4940b84d0461f3c9b
唐超 <tangchao47@hisilicon.com>
2023-11-25 03:48:53 +0000

    !972 Transpose inner Axis in BF16 Merge pull request !972 from 唐超/bf16_innerAxis

--- op_plugin/ops/base_ops/aclops/MmKernelNpu.cpp
+++ op_plugin/ops/base_ops/aclops/MmKernelNpu.cpp
@@ -148,7 +148,8 @@ bool is_transpose_inner_axis(const at::Tensor &self)
     const static int64_t kInnerAxisMinBytes = 256;
     const static int64_t kInnerAxisMaxLimit = 65535;
     if (c10_npu::GetSocVersion() < c10_npu::SocVersion::Ascend910B1 || self.dim() < 2 ||
-        (self.scalar_type() != at::ScalarType::Half && self.scalar_type() != at::ScalarType::Float)) {
+        (self.scalar_type() != at::ScalarType::Half && self.scalar_type() != at::ScalarType::Float &&
+         self.scalar_type() != at::ScalarType::BFloat16)) {
         return false;
     }
     int64_t data_type = static_cast<int64_t>(elementSize(self.scalar_type()));
