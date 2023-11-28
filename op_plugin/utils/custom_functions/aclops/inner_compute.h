0b078e8c2bdd99a95087f57445c6445d15e1368b
戢芳 <jifang5@h-partners.com>
2023-11-28 02:14:10 +0000

    !1011 Add scatter_ and revert scatter_out. Merge pull request !1011 from 戢芳/scatter

--- op_plugin/utils/custom_functions/aclops/inner_compute.h
+++ op_plugin/utils/custom_functions/aclops/inner_compute.h
@@ -44,8 +44,6 @@ at::Tensor& mean_out_common_nocheck(const at::Tensor& self, at::IntArrayRef dim,
                                     c10::optional<c10::ScalarType> dtype, at::Tensor& result);
 at::Tensor mean_common_nocheck(const at::Tensor& self, at::IntArrayRef dim, bool keepdim,
                                c10::optional<c10::ScalarType> dtype);
-at::Tensor& scatter_npu_common_nocheck(at::Tensor& self, int64_t dim, const at::Tensor& index, const at::Tensor& src);
-at::Tensor& scatter_npu_src_impl(at::Tensor& self, int64_t dim, const at::Tensor& index_ex, const at::Tensor& src_ex);
 ::std::tuple<at::Tensor, at::Tensor> prelu_backward_commom_nocheck(at::Tensor& grad_input,
                                                                    at::Tensor& grad_weight,
                                                                    const at::Tensor& grad_output,
