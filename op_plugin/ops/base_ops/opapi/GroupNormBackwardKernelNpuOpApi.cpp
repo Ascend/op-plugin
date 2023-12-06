765a2794eb6b9753d6764bdbd791f8abfb7c3956
weinachuan <weinachuan@huawei.com>
2023-12-06 13:41:46 +0000

    !1078 回退GroupNormGrad的适配 Merge pull request !1078 from weinachuan/master

--- op_plugin/ops/base_ops/opapi/GroupNormBackwardKernelNpuOpApi.cpp
+++ op_plugin/ops/base_ops/opapi/GroupNormBackwardKernelNpuOpApi.cpp
@@ -16,7 +16,6 @@
 #include "op_plugin/AclOpsInterface.h"
 #include "op_plugin/OpApiInterface.h"
 #include "op_plugin/utils/op_api_common.h"
-#include "op_plugin/utils/OpAdapter.h"
 
 namespace op_api {
 using npu_preparation = at_npu::native::OpPreparation;
@@ -36,40 +35,23 @@ std::tuple<at::Tensor, at::Tensor, at::Tensor> native_group_norm_backward(
     DO_COMPATIBILITY(aclnnGroupNormBackward,
                      acl_op::native_group_norm_backward(dY, X, mean, rstd, gamma_opt, N, C, HxW, group,
                                                         grad_input_mask));
+
     at::Tensor grad_x;
     at::Tensor grad_gamma;
     at::Tensor grad_beta;
     if (grad_input_mask[0]) {
-        grad_x = npu_preparation::apply_tensor_without_format(X.sizes(), X.options().dtype(at::kFloat));
+        grad_x = npu_preparation::apply_tensor_without_format(dY);
     }
     if (grad_input_mask[1]) {
-        grad_gamma = npu_preparation::apply_tensor_without_format({C}, X.options().dtype(at::kFloat));
-        grad_gamma.zero_();
+        grad_gamma = npu_preparation::apply_tensor_without_format(X, {C});
     }
     if (grad_input_mask[2]) {
-        grad_beta = npu_preparation::apply_tensor_without_format({C}, X.options().dtype(at::kFloat));
-        grad_beta.zero_();
+        grad_beta = npu_preparation::apply_tensor_without_format(X, {C});
     }
-    EXEC_NPU_CMD(aclnnGroupNormGrad, dY, mean, rstd, X, gamma_opt,
-                 group, "NCHW", grad_input_mask[0], grad_input_mask[1], grad_input_mask[2],
+
+    EXEC_NPU_CMD(aclnnGroupNormBackward, dY, X, mean, rstd, gamma_opt, N, C, HxW, group, grad_input_mask,
                  grad_x, grad_gamma, grad_beta);
-    auto result_dtype = dY.scalar_type();
-    at::Tensor grad_x_cp;
-    at::Tensor grad_gamma_cp;
-    at::Tensor grad_beta_cp;
-    if (grad_input_mask[0]) {
-        grad_x_cp = grad_x.scalar_type() == result_dtype ? grad_x :
-            at_npu::native::custom_ops::npu_dtype_cast(grad_x, result_dtype);
-    }
-    if (grad_input_mask[1]) {
-        grad_gamma_cp = grad_x.scalar_type() == result_dtype ? grad_gamma :
-            at_npu::native::custom_ops::npu_dtype_cast(grad_gamma, result_dtype);
-    }
-    if (grad_input_mask[2]) {
-        grad_beta_cp = grad_x.scalar_type() == result_dtype ? grad_beta :
-            at_npu::native::custom_ops::npu_dtype_cast(grad_beta, result_dtype);
-    }
-    return std::make_tuple(grad_x_cp, grad_gamma_cp, grad_beta_cp);
+    return std::make_tuple(grad_x, grad_gamma, grad_beta);
 }
 
 } // namespace op_api
