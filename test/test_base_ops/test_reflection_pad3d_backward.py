f74f5ea527eb828fde22746bca24c04fdd089a41
李璐璞 <lilupu1@huawei.com>
2023-12-11 12:06:29 +0000

    !1062 reflection_pad3d_backward/replication_pad3d_backward 新增op api接口 Merge pull request !1062 from 李璐璞/add_xxx_pad3d_grad_op_api

---
+++ test/test_base_ops/test_reflection_pad3d_backward.py
@@ -0,0 +1,40 @@
+import numpy as np
+import torch
+import torch_npu
+from torch_npu.testing.testcase import TestCase, run_tests
+from torch_npu.testing.common_utils import create_common_tensor
+
+torch.npu.set_compile_mode(jit_compile=False)
+
+
+class TestReflectionPad3dBackward(TestCase):
+
+    def reflection_pad3d_backward(self, grad_out, self_tensor, padding):
+        padding_layer = torch.nn.ReflectionPad3d(padding)
+
+        self_tensor.requires_grad = True
+        output = padding_layer(self_tensor)
+        output.backward(grad_out)
+
+        grad_result = self_tensor.grad
+        return grad_result
+
+    def test_reflection_pad3d_backward(self):
+        dtype = np.float32
+        data_format = -1
+        input_shape = [dtype, data_format, [1, 1, 4, 4, 4]]
+        grad_shape = [dtype, data_format, [1, 1, 8, 8, 8]]
+        padding = [2, 2, 2, 2, 2, 2]
+        grad_out_tensor = create_common_tensor(grad_shape, -1, 1)[0]
+        self_tensor = create_common_tensor(input_shape, -1, 1)[0]
+
+        self_tensor_npu = self_tensor.clone().npu()
+
+        golden = self.reflection_pad3d_backward(grad_out_tensor, self_tensor, padding)
+        output = self.reflection_pad3d_backward(grad_out_tensor.npu(), self_tensor_npu, padding)
+
+        self.assertRtolEqual(golden, output)
+
+
+if __name__ == "__main__":
+    run_tests()
