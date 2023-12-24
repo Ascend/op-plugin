35b163bf81e372d3d839c1cffa16afba2ed44eb0
阮晨杰 <ruanchenjie@hisilicon.com>
2023-12-24 03:17:39 +0000

    !1094 【WeightQuantBatchMatmulV2】 torch 定义ATENIR及API Merge pull request !1094 from 阮晨杰/master

---
+++ op_plugin/ops/v2r0/opapi/WeightQuantBatchMatmulV2KernelNpuOpApi.cpp
@@ -0,0 +1,61 @@
+// Copyright (c) 2023 Huawei Technologies Co., Ltd
+// Copyright (c) 2019, Facebook CORPORATION.
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "op_plugin/OpApiInterface.h"
+#include "op_plugin/utils/op_api_common.h"
+
+namespace op_api {
+using npu_preparation = at_npu::native::OpPreparation;
+
+at::Tensor npu_weight_quant_batchmatmul(const at::Tensor &x, const at::Tensor &weight,
+                                        const at::Tensor &antiquant_scale,
+                                        const c10::optional<at::Tensor> &antiquant_offset,
+                                        const c10::optional<at::Tensor> &quant_scale,
+                                        const c10::optional<at::Tensor> &quant_offset,
+                                        const c10::optional<at::Tensor> &bias)
+{
+    auto x_dim_num = x.dim();
+    auto weight_dim_num = weight.dim();
+    TORCH_CHECK(x_dim_num == 2, "x shape dims should be 2, but it is ", x_dim_num);
+    TORCH_CHECK(weight_dim_num == 2, "weight shape dims should be 2, but it is ", weight_dim_num);
+
+    auto x_k_dim = x.size(1);
+    auto weight_k_dim = weight.size(0);
+    TORCH_CHECK(x_k_dim == weight_k_dim, "The k of x and weight should be equal. but x_k_dim is ", x_k_dim,
+                ", weight_k_dim is ", weight_k_dim);
+
+    const at::Tensor &antiquant_offset_real = antiquant_offset.value_or(at::Tensor());
+    const at::Tensor &quant_scale_real = quant_scale.value_or(at::Tensor());
+    const at::Tensor &quant_offset_real = quant_offset.value_or(at::Tensor());
+    const at::Tensor &bias_real = bias.value_or(at::Tensor());
+
+    TORCH_CHECK((quant_scale.has_value() || !quant_offset.has_value()),
+                "Quantization parameters are incorrectly set, quant_offset cannot exist in isolation from quant_scale");
+
+    c10::TensorOptions options =
+        quant_scale.has_value() ? x.options().dtype(at::kChar) : x.options().dtype(x.scalar_type());
+
+    auto output_size = op_infer::array_to_small_vector(x.sizes());
+    output_size[0] = x.size(0);
+    output_size[1] = weight.size(1);
+    at::Tensor result = npu_preparation::apply_tensor_without_format(output_size, options);
+
+    EXEC_NPU_CMD(aclnnWeightQuantBatchMatmulV2, x, weight, antiquant_scale, antiquant_offset_real, quant_scale_real,
+                 quant_offset_real, bias_real, result);
+
+    return result;
+}
+}  // namespace op_api
---
+++ test/test_custom_ops/test_npu_weight_quant_batchmatmul.py
@@ -0,0 +1,44 @@
+import math
+import unittest
+import numpy as np
+import torch
+
+import torch_npu
+from torch_npu.testing.testcase import TestCase, run_tests
+from torch_npu.testing.common_utils import create_common_tensor
+
+DEVICE_NAME = torch_npu.npu.get_device_name(0)[:10]
+
+
+class TestNPUWeightQuantBatchMatmul(TestCase):
+
+    def supported_op_exec(self, x, weight, antiquant_scale, antiquant_offset):
+        res = torch.matmul(x, (weight + antiquant_offset) * antiquant_scale)
+        return res
+
+    def custom_op_exec(self, x, weight, antiquant_scale, antiquant_offset):
+        return torch_npu.npu_weight_quant_batchmatmul(x, weight, antiquant_scale, antiquant_offset)
+
+    @unittest.skipIf(DEVICE_NAME != 'Ascend910B',
+        "OP `weight_quant_batchmatmul` is only supported on 910B, skip this ut for this device type!")
+    def test_npu_npu_weight_quant_batchmatmul(self, device="npu"):
+        torch.mannal_seed(0)
+        x = torch.randn((96, 11264), dtype=torch.bfloat16).npu()
+        weight = torch.randn((11264, 1164), dtype=torch.int8).npu()
+        weight_t = torch.transpose(weight, 0, 1)
+        antiquant_scale = torch.randn((1, 1164), dtype=torch.bfloat16).npu()
+        antiquant_offset = torch.randn((1, 1164), dtype=torch.bfloat16).npu()
+
+        x_clone = x.clone()
+        weight_t_clone = weight_t.clone()
+        antiquant_scale_clone = antiquant_scale.clone()
+        antiquant_offset_clone = antiquant_offset.clone()
+
+        supported_output = self.supported_op_exec(x, weight_t, antiquant_scale, antiquant_offset)
+        custom_output = self.custom_op_exec(x_clone, weight_t_clone, antiquant_scale_clone, antiquant_offset_clone)
+
+        self.assertRtolEqual(supported_output, custom_output, 0.001)
+
+
+if __name__ == "__main__":
+    run_tests()
