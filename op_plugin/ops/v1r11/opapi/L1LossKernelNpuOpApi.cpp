6386354e9bca533364ace7febcd6571703eb3ba1
吴金 <wujin46@huawei.com>
2024-01-04 09:24:51 +0000

    !1153 pta1.11: L1Loss supports int64 input Merge pull request !1153 from 吴金/master

--- op_plugin/ops/v1r11/opapi/L1LossKernelNpuOpApi.cpp
+++ op_plugin/ops/v1r11/opapi/L1LossKernelNpuOpApi.cpp
@@ -24,44 +24,47 @@ using npu_preparation = at_npu::native::OpPreparation;
 at::Tensor& l1_loss_out(const at::Tensor& self,
                         const at::Tensor& target,
                         int64_t reduction,
-                        at::Tensor& result) {
-  DO_COMPATIBILITY(aclnnL1Loss, acl_op::l1_loss_out(self, target, reduction, result));
-  // check if result on NPU
-  TORCH_CHECK(torch_npu::utils::is_npu(result), "result with device ", result.device(),
-              " doesn't match the desired device NPU");
-  // 1. When reduction = 'none', shape of result must be the same as self.
-  // 2. When reduction != 'none', result must be a 0-dimensional tensor.
-  at::IntArrayRef output_size;
-  if (reduction == at::Reduction::None) {
-    auto output_size_vec = op_infer::broadcast_ops_npu_output_size(self, target);
-    output_size = output_size_vec;
-  }
-  // Shape of result must be the same as self, dtype has no limitation.
-  if (result.sizes() != output_size) {
-    result.resize_(output_size);
-  }
-  // dispatch hostAPI
-  EXEC_NPU_CMD(aclnnL1Loss, self, target, reduction, result);
-  return result;
+                        at::Tensor& result)
+{
+    DO_COMPATIBILITY(aclnnL1Loss, acl_op::l1_loss_out(self, target, reduction, result));
+    // check if result on NPU
+    TORCH_CHECK(torch_npu::utils::is_npu(result), "result with device ", result.device(),
+                " doesn't match the desired device NPU");
+    // 1. When reduction = 'none', shape of result must be the same as self.
+    // 2. When reduction != 'none', result must be a 0-dimensional tensor.
+    at::IntArrayRef output_size;
+    if (reduction == at::Reduction::None) {
+        auto output_size_vec = op_infer::broadcast_ops_npu_output_size(self, target);
+        output_size = output_size_vec;
+    }
+    // Shape of result must be the same as self, dtype has no limitation.
+    if (result.sizes() != output_size) {
+        result.resize_(output_size);
+    }
+    // dispatch hostAPI
+    EXEC_NPU_CMD(aclnnL1Loss, self, target, reduction, result);
+    return result;
 }
 
 at::Tensor l1_loss(const at::Tensor& self,
                    const at::Tensor& target,
-                   int64_t reduction) {
-  DO_COMPATIBILITY(aclnnL1Loss, acl_op::l1_loss(self, target, reduction));
-  // construct the output tensor of NPU
-  // 1. If reduction='none', the output size should be the same size as self.
-  // 2. Otherwise pass {} to apply_tensor.
-  // 3. Dtype of output should be the same dtype as self.
-  at::IntArrayRef output_size;
-  if (reduction == at::Reduction::None) {
-    auto output_size_vec = op_infer::broadcast_ops_npu_output_size(self, target);
-    output_size = output_size_vec;
-  }
-  at::Tensor result = npu_preparation::apply_tensor_without_format(self, output_size);
-  // dispatch hostAPI
-  EXEC_NPU_CMD(aclnnL1Loss, self, target, reduction, result);
-  return result;
+                   int64_t reduction)
+{
+    DO_COMPATIBILITY(aclnnL1Loss, acl_op::l1_loss(self, target, reduction));
+    // construct the output tensor of NPU
+    // 1. If reduction='none', the output size should be the same size as self.
+    // 2. Otherwise pass {} to apply_tensor.
+    // 3. Dtype of output should be the same dtype as self.
+    at::IntArrayRef output_size;
+    if (reduction == at::Reduction::None) {
+        auto output_size_vec = op_infer::broadcast_ops_npu_output_size(self, target);
+        output_size = output_size_vec;
+    }
+    auto promote = at::native::result_type(target, self);
+    at::Tensor result = npu_preparation::apply_tensor_without_format(output_size, self.options().dtype(promote));
+    // dispatch hostAPI
+    EXEC_NPU_CMD(aclnnL1Loss, self, target, reduction, result);
+    return result;
 }
 
 } // namespace op_api
