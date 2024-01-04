46a101b4a09da2844c88c93ad57ab28dd4aeee99
刘嘉巍 <843972097@qq.com>
2024-01-04 11:16:22 +0000

    !1157 [Fix] Use is_contiguous to optimize performance. Merge pull request !1157 from 刘嘉巍/master-dev

--- op_plugin/ops/base_ops/aclops/BernoulliKernelNpu.cpp
+++ op_plugin/ops/base_ops/aclops/BernoulliKernelNpu.cpp
@@ -22,86 +22,86 @@
 #include "op_plugin/utils/OpAdapter.h"
 
 namespace acl_op {
-using npu_preparation = at_npu::native::OpPreparation;
-using npu_compile_type = at_npu::native::CompileType;
-using npu_utils = at_npu::native::NpuUtils;
+    using npu_preparation = at_npu::native::OpPreparation;
+    using npu_compile_type = at_npu::native::CompileType;
+    using npu_utils = at_npu::native::NpuUtils;
 
 namespace {
-at::Tensor& bernoulli_npu_nocheck(at::Tensor& result, double p, c10::optional<at::Generator> gen) {
-  auto gen_ = at::get_generator_or_default<at_npu::NPUGeneratorImpl>(gen, at_npu::detail::getDefaultNPUGenerator());
-  auto pair = gen_->philox_engine_inputs(10);
-  const int64_t seed = static_cast<int64_t>(pair.first);
-  const int64_t offset = static_cast<int64_t>(pair.second);
+    at::Tensor &bernoulli_npu_nocheck(at::Tensor &result, double p, c10::optional<at::Generator> gen) {
+        auto gen_ = at::get_generator_or_default<at_npu::NPUGeneratorImpl>(gen, at_npu::detail::getDefaultNPUGenerator());
+        auto pair = gen_->philox_engine_inputs(10);
+        const int64_t seed = static_cast<int64_t>(pair.first);
+        const int64_t offset = static_cast<int64_t>(pair.second);
 
-  at_npu::native::OpCommand cmd;
-  cmd.Name("StatelessBernoulli")
-      .Input(result.sizes(), at::kLong, npu_compile_type::MEMORY_HOST_COMPILE_INDEPENDENT)
-      .Input(at::Scalar(p), at::kFloat)
-      .Input(at::Scalar(seed), at::kLong)
-      .Input(at::Scalar(offset), at::kLong)
-      .Output(result)
-      .Attr("dtype", result.scalar_type())
-      .Run();
-  return result;
-}
+        at_npu::native::OpCommand cmd;
+        cmd.Name("StatelessBernoulli")
+            .Input(result.sizes(), at::kLong, npu_compile_type::MEMORY_HOST_COMPILE_INDEPENDENT)
+            .Input(at::Scalar(p), at::kFloat)
+            .Input(at::Scalar(seed), at::kLong)
+            .Input(at::Scalar(offset), at::kLong)
+            .Output(result)
+            .Attr("dtype", result.scalar_type())
+            .Run();
+        return result;
+    }
 
-at::Tensor& bernoulli_npu_nocheck(at::Tensor& result, const at::Tensor& p, c10::optional<at::Generator> gen) {
-  auto gen_ = at::get_generator_or_default<at_npu::NPUGeneratorImpl>(gen, at_npu::detail::getDefaultNPUGenerator());
-  auto pair = gen_->philox_engine_inputs(10);
-  const int64_t seed = static_cast<int64_t>(pair.first);
-  const int64_t offset = static_cast<int64_t>(pair.second);
+    at::Tensor &bernoulli_npu_nocheck(at::Tensor &result, const at::Tensor &p, c10::optional<at::Generator> gen) {
+        auto gen_ = at::get_generator_or_default<at_npu::NPUGeneratorImpl>(gen, at_npu::detail::getDefaultNPUGenerator());
+        auto pair = gen_->philox_engine_inputs(10);
+        const int64_t seed = static_cast<int64_t>(pair.first);
+        const int64_t offset = static_cast<int64_t>(pair.second);
 
-  at_npu::native::OpCommand cmd;
-  cmd.Name("StatelessBernoulli")
-      .Input(result.sizes(), at::kLong, npu_compile_type::MEMORY_HOST_COMPILE_INDEPENDENT)
-      .Input(p)
-      .Input(at::Scalar(seed), at::kLong)
-      .Input(at::Scalar(offset), at::kLong)
-      .Output(result)
-      .Attr("dtype", result.scalar_type())
-      .Run();
-  return result;
-}
+        at_npu::native::OpCommand cmd;
+        cmd.Name("StatelessBernoulli")
+            .Input(result.sizes(), at::kLong, npu_compile_type::MEMORY_HOST_COMPILE_INDEPENDENT)
+            .Input(p)
+            .Input(at::Scalar(seed), at::kLong)
+            .Input(at::Scalar(offset), at::kLong)
+            .Output(result)
+            .Attr("dtype", result.scalar_type())
+            .Run();
+        return result;
+    }
 } // namespace
 
-at::Tensor& bernoulli_(at::Tensor& self, double p, c10::optional<at::Generator> gen) {
-  if (!npu_utils::check_match(&self)) {
-    at::Tensor contiguous_self = npu_utils::format_contiguous(self);
-    bernoulli_npu_nocheck(contiguous_self, p, gen);
-    npu_utils::format_fresh_view(self, contiguous_self);
-  } else {
-    bernoulli_npu_nocheck(self, p, gen);
-  }
-  return self;
+at::Tensor &bernoulli_(at::Tensor &self, double p, c10::optional<at::Generator> gen) {
+    if (!self.is_contiguous()) {
+        at::Tensor contiguous_self = npu_utils::format_contiguous(self);
+        bernoulli_npu_nocheck(contiguous_self, p, gen);
+        npu_utils::format_fresh_view(self, contiguous_self);
+    } else {
+        bernoulli_npu_nocheck(self, p, gen);
+    }
+    return self;
 }
 
-at::Tensor& bernoulli_(at::Tensor& self, const at::Tensor& p, c10::optional<at::Generator> gen) {
-  at::Tensor p_ori_format = npu_preparation::CastBackToOriFormat(p);
-  npu_preparation::CheckMemory({self, p}, {self});
+at::Tensor &bernoulli_(at::Tensor &self, const at::Tensor &p, c10::optional<at::Generator> gen) {
+    at::Tensor p_ori_format = npu_preparation::CastBackToOriFormat(p);
+    npu_preparation::CheckMemory({self, p}, {self});
 
-  if (!npu_utils::check_match(&self)) {
-    at::Tensor contiguous_self = npu_utils::format_contiguous(self);
-    bernoulli_npu_nocheck(contiguous_self, p_ori_format, gen);
-    npu_utils::format_fresh_view(self, contiguous_self);
-  } else {
-    bernoulli_npu_nocheck(self, p_ori_format, gen);
-  }
-  return self;
+    if (!self.is_contiguous()) {
+        at::Tensor contiguous_self = npu_utils::format_contiguous(self);
+        bernoulli_npu_nocheck(contiguous_self, p_ori_format, gen);
+        npu_utils::format_fresh_view(self, contiguous_self);
+    } else {
+        bernoulli_npu_nocheck(self, p_ori_format, gen);
+    }
+    return self;
 }
 
-at::Tensor bernoulli(const at::Tensor& self, c10::optional<at::Generator> gen) {
-  at::Tensor result = npu_preparation::apply_tensor_with_format(self.sizes(), self.options(), ACL_FORMAT_ND);
-  bernoulli_npu_nocheck(result, self, gen);
-  return result;
+at::Tensor bernoulli(const at::Tensor &self, c10::optional<at::Generator> gen) {
+    at::Tensor result = npu_preparation::apply_tensor_with_format(self.sizes(), self.options(), ACL_FORMAT_ND);
+    bernoulli_npu_nocheck(result, self, gen);
+    return result;
 }
 
-at::Tensor bernoulli(const at::Tensor& self, double p, c10::optional<at::Generator> gen) {
-  return at::empty_like(self, LEGACY_CONTIGUOUS_MEMORY_FORMAT).bernoulli_(p, gen);
+at::Tensor bernoulli(const at::Tensor &self, double p, c10::optional<at::Generator> gen) {
+    return at::empty_like(self, LEGACY_CONTIGUOUS_MEMORY_FORMAT).bernoulli_(p, gen);
 }
 
-at::Tensor& bernoulli_out(const at::Tensor& self, c10::optional<at::Generator> gen, at::Tensor& result) {
-  result.resize_(self.sizes()).bernoulli_(self, gen);
-  at::namedinference::propagate_names(result, self);
-  return result;
+at::Tensor &bernoulli_out(const at::Tensor &self, c10::optional<at::Generator> gen, at::Tensor &result) {
+    result.resize_(self.sizes()).bernoulli_(self, gen);
+    at::namedinference::propagate_names(result, self);
+    return result;
 }
 } // namespace acl_op
--- op_plugin/ops/base_ops/aclops/BmmKernelNpu.cpp
+++ op_plugin/ops/base_ops/aclops/BmmKernelNpu.cpp
@@ -21,53 +21,53 @@
 #include "torch_npu/csrc/framework/utils/UtilForOpAdapter.h"
 
 namespace acl_op {
-using npu_format_helper = at_npu::native::FormatHelper;
-using npu_preparation = at_npu::native::OpPreparation;
-using npu_utils = at_npu::native::NpuUtils;
+    using npu_format_helper = at_npu::native::FormatHelper;
+    using npu_preparation = at_npu::native::OpPreparation;
+    using npu_utils = at_npu::native::NpuUtils;
 
 namespace {
-at::Tensor& bmm_out_nocheck(at::Tensor& result, const at::Tensor& self, const at::Tensor& mat2) {
-  bool is_self_t = op_plugin::utils::is_transpose_last_two_dims(self);
-  bool is_mat2_t = op_plugin::utils::is_transpose_last_two_dims(mat2);
-  at::Tensor contiguous_self = is_self_t ? self : npu_utils::format_contiguous_add_copy_optimize(self);
-  at::Tensor contiguous_mat2 = is_mat2_t ? mat2 : npu_utils::format_contiguous_add_copy_optimize(mat2);
+    at::Tensor &bmm_out_nocheck(at::Tensor &result, const at::Tensor &self, const at::Tensor &mat2) {
+        bool is_self_t = op_plugin::utils::is_transpose_last_two_dims(self);
+        bool is_mat2_t = op_plugin::utils::is_transpose_last_two_dims(mat2);
+        at::Tensor contiguous_self = is_self_t ? self : npu_utils::format_contiguous_add_copy_optimize(self);
+        at::Tensor contiguous_mat2 = is_mat2_t ? mat2 : npu_utils::format_contiguous_add_copy_optimize(mat2);
 
-  at_npu::native::OpCommand cmd;
-  cmd.Name("BatchMatMul")
-      .InputWithoutContiguous(contiguous_self)
-      .InputWithoutContiguous(contiguous_mat2)
-      .Output(result)
-      .Attr("adj_x1", is_self_t)
-      .Attr("adj_x2", is_mat2_t)
-      .Run();
-  return result;
-}
+        at_npu::native::OpCommand cmd;
+        cmd.Name("BatchMatMul")
+            .InputWithoutContiguous(contiguous_self)
+            .InputWithoutContiguous(contiguous_mat2)
+            .Output(result)
+            .Attr("adj_x1", is_self_t)
+            .Attr("adj_x2", is_mat2_t)
+            .Run();
+        return result;
+    }
 } // namespace
 
-at::Tensor& bmm_out(const at::Tensor& self, const at::Tensor& mat2, at::Tensor& result) {
-  TORCH_CHECK(self.device() == mat2.device(),
-      "Expected all tensors to be on the same device, but found at least two devices, ",
-      (torch_npu::utils::is_npu(self) ? "npu" : "cpu"),
-      " and ",
-      (torch_npu::utils::is_npu(mat2) ? "npu! " : "cpu! "));
-  auto output_size = {self.size(0), self.size(1), mat2.size(2)};
-  npu_preparation::CheckOut(
-      {self, mat2},
-      result,
-      self,
-      output_size);
+at::Tensor &bmm_out(const at::Tensor &self, const at::Tensor &mat2, at::Tensor &result) {
+    TORCH_CHECK(self.device() == mat2.device(),
+                "Expected all tensors to be on the same device, but found at least two devices, ",
+                (torch_npu::utils::is_npu(self) ? "npu" : "cpu"),
+                " and ",
+                (torch_npu::utils::is_npu(mat2) ? "npu! " : "cpu! "));
+    auto output_size = {self.size(0), self.size(1), mat2.size(2)};
+    npu_preparation::CheckOut(
+        {self, mat2},
+        result,
+        self,
+        output_size);
 
-  if (!npu_utils::check_match(&result)) {
-    at::Tensor contiguous_result = npu_utils::format_contiguous(result);
-    bmm_out_nocheck(contiguous_result, self, mat2);
-    npu_utils::format_fresh_view(result, contiguous_result);
-  } else {
-    bmm_out_nocheck(result, self, mat2);
-  }
-  return result;
+    if (!result.is_contiguous()) {
+        at::Tensor contiguous_result = npu_utils::format_contiguous(result);
+        bmm_out_nocheck(contiguous_result, self, mat2);
+        npu_utils::format_fresh_view(result, contiguous_result);
+    } else {
+        bmm_out_nocheck(result, self, mat2);
+    }
+    return result;
 }
 
-at::Tensor bmm(const at::Tensor& self, const at::Tensor& mat2) {
+at::Tensor bmm(const at::Tensor &self, const at::Tensor &mat2) {
     TORCH_CHECK(self.device() == mat2.device(),
                 "Expected all tensors to be on the same device, but found at least two devices, ",
                 (torch_npu::utils::is_npu(self) ? "npu" : "cpu"),
@@ -77,35 +77,34 @@ at::Tensor bmm(const at::Tensor& self, const at::Tensor& mat2) {
     TORCH_CHECK(mat2.dim() >= 3, "bmm expect mat2 at least 3D tensors, but got: ", mat2.dim());
     auto output_size = {self.size(0), self.size(1), mat2.size(2)};
 
-  at::Tensor result;
-  bool need_nd_out = false;
-  // Check if the mm output is specified as NCHW.
-  // It will be deleted after the overall strategy of the NLP model is formulated.
-  if ((self.scalar_type() == at::kHalf)) {
-    // check is 16-algined with high-performance
-    auto is_aligin = [&]() {
-      return (!(static_cast<uint64_t>(self.size(1)) & 0xF)) && (!(static_cast<uint64_t>(self.size(2)) & 0xF)) &&
-             (!(static_cast<uint64_t>(mat2.size(1)) & 0xF)) && (!(static_cast<uint64_t>(mat2.size(2)) & 0xF));
-    };
-    static auto mm_bmm_nd = !at_npu::native::env::CheckMmBmmNDDisable();
-    static bool is_support_nd_out = c10_npu::GetSocVersion() >= c10_npu::SocVersion::Ascend910B1;
-    // There is a data trampling problem in non-aligned scenes. For the time being, only aligned scenes are supported.
-    if (npu_format_helper::IsBaseFormatType(self) && npu_format_helper::IsBaseFormatType(mat2) &&
-        mm_bmm_nd && ((is_support_nd_out && op_plugin::utils::is_nd_to_nz_on_fly(self, mat2)) ||
-        (!is_support_nd_out && is_aligin()))) {
-      result = npu_preparation::apply_tensor_with_format(output_size, self.options(), ACL_FORMAT_ND);
+    at::Tensor result;
+    bool need_nd_out = false;
+    // Check if the mm output is specified as NCHW.
+    // It will be deleted after the overall strategy of the NLP model is formulated.
+    if ((self.scalar_type() == at::kHalf)) {
+        // check is 16-algined with high-performance
+        auto is_aligin = [&]() {
+            return (!(static_cast<uint64_t>(self.size(1)) & 0xF)) && (!(static_cast<uint64_t>(self.size(2)) & 0xF)) &&
+                    (!(static_cast<uint64_t>(mat2.size(1)) & 0xF)) && (!(static_cast<uint64_t>(mat2.size(2)) & 0xF));
+        };
+        static auto mm_bmm_nd = !at_npu::native::env::CheckMmBmmNDDisable();
+        static bool is_support_nd_out = c10_npu::GetSocVersion() >= c10_npu::SocVersion::Ascend910B1;
+        // There is a data trampling problem in non-aligned scenes. For the time being, only aligned scenes are supported.
+        if (npu_format_helper::IsBaseFormatType(self) && npu_format_helper::IsBaseFormatType(mat2) &&
+            mm_bmm_nd && ((is_support_nd_out && op_plugin::utils::is_nd_to_nz_on_fly(self, mat2)) || (!is_support_nd_out && is_aligin()))) {
+            result = npu_preparation::apply_tensor_with_format(output_size, self.options(), ACL_FORMAT_ND);
+        } else {
+            result = npu_preparation::apply_tensor_with_format(output_size, self.options(), ACL_FORMAT_FRACTAL_NZ, true);
+            need_nd_out = mm_bmm_nd;
+        }
     } else {
-      result = npu_preparation::apply_tensor_with_format(output_size, self.options(), ACL_FORMAT_FRACTAL_NZ, true);
-      need_nd_out = mm_bmm_nd;
+        result = npu_preparation::apply_tensor_with_format(output_size, self.options(), ACL_FORMAT_ND);
     }
-  } else {
-    result = npu_preparation::apply_tensor_with_format(output_size, self.options(), ACL_FORMAT_ND);
-  }
 
-  bmm_out_nocheck(result, self, mat2);
-  if (need_nd_out) {
-    result = at_npu::native::custom_ops::npu_format_cast(result, ACL_FORMAT_ND);
-  }
-  return result;
+    bmm_out_nocheck(result, self, mat2);
+    if (need_nd_out) {
+        result = at_npu::native::custom_ops::npu_format_cast(result, ACL_FORMAT_ND);
+    }
+    return result;
 }
 } // namespace acl_op
