2a5a011ad7f9d8fe19f8c2378999432c1bab1205
王嘉诚 <12731429+wang-pierre-jiacheng@user.noreply.gitee.com>
2024-01-12 06:59:37 +0000

    !1132 【PTA】support weight quant add Merge pull request !1132 from 王嘉诚/master

--- op_plugin/ops/base_ops/opapi/MatmulAllReduceBaseKernelNpuOpApi.cpp
+++ op_plugin/ops/base_ops/opapi/MatmulAllReduceBaseKernelNpuOpApi.cpp
@@ -18,26 +18,7 @@
 #include "op_plugin/utils/op_api_common.h"
 
 namespace op_api {
-
-static c10::SmallVector<int64_t, op_infer::SIZE> get_output_size(const at::Tensor &x1, const at::Tensor &x2)
-{
-    bool is_x2_t = op_plugin::utils::is_transpose_last_two_dims(x2);
-    if (!is_x2_t) {
-        TORCH_CHECK(x1.size(x1.dim() - 1) == x2.size(0), "K of x1 and x2 should be same, but they are x1_k: ",
-                    x1.size(x1.dim() - 1), ", x2_k: ", x2.size(0));
-    } else {
-        TORCH_CHECK(x1.size(x1.dim() - 1) == x2.size(1), "K of x1 and x2 should be same, but they are x1_k: ",
-                    x1.size(x1.dim() - 1), ", x2_k: ", x2.size(1));
-    }
-    // size of last dim of output should be the same as size of last dim of x2
-    auto output_size = op_infer::array_to_small_vector(x1.sizes());
-    if (!is_x2_t) {
-        output_size[x1.dim() - 1] = x2.size(1);
-    } else {
-        output_size[x1.dim() - 1] = x2.size(0);
-    }
-    return output_size;
-}
+using npu_preparation = at_npu::native::OpPreparation;
 
 at::Tensor npu_mm_all_reduce_base(const at::Tensor &x1, const at::Tensor &x2, c10::string_view hcom,
                                   c10::string_view reduce_op, const c10::optional<at::Tensor> &bias,
@@ -47,21 +28,39 @@ at::Tensor npu_mm_all_reduce_base(const at::Tensor &x1, const at::Tensor &x2, c1
 {
     TORCH_CHECK(x2.dim() == 2, "x2 needs to be 2D, but got: ", x2.dim(), "D");
     // size of last dim of output should be the same as size of last dim of x2
-    auto output_size = get_output_size(x1, x2);
+    TORCH_CHECK(x1.size(x1.dim() - 1) == x2.size(0), "K of x1 and x2 should be same, but they are x1_k: ",
+                x1.size(x1.dim() - 1), ", x2_k: ", x2.size(0));
+    // size of last dim of output should be the same as size of last dim of x2
+    auto output_size = op_infer::array_to_small_vector(x1.sizes());
+    output_size[x1.dim() - 1] = x2.size(1);
     auto result = at_npu::native::OpPreparation::apply_tensor_without_format(output_size, x1.options());
     char *reduce_op_ptr = const_cast<char *>(reduce_op.data());
     char *hcom_ptr = const_cast<char *>(hcom.data());
     const at::Tensor &bias_real = bias.value_or(at::Tensor());
     const at::Tensor &x3_real = x3.value_or(at::Tensor());
     int64_t stream_mode = ACL_STOP_ON_FAILURE;
+    // atomic matmul with add
+    if (x3.has_value()) {
+        TORCH_CHECK(x3_real.sizes().equals(output_size), "x3 with shape ", x3_real.sizes(),
+                    " doesn't match the output shape ", output_size);
+        TORCH_CHECK(x3_real.scalar_type() == result.scalar_type(), "x3 with dtype ", x3_real.scalar_type(),
+                    " doesn't match the output dtype ", result.scalar_type());
+    }
+
     if (!isIntegralType(x2.scalar_type())) {
         EXEC_NPU_CMD(aclnnMatmulAllReduce, x1, x2, bias_real, hcom_ptr, reduce_op_ptr, comm_turn, stream_mode, result);
     } else {
         TORCH_CHECK(x2.scalar_type() == at::kChar, "x2 must be an int8 tensor for quant.");
         const at::Tensor &antiquant_scale_real = antiquant_scale.value_or(at::Tensor());
         const at::Tensor &antiquant_offset_real = antiquant_offset.value_or(at::Tensor());
-        EXEC_NPU_CMD(aclnnWeightQuantMatmulAllReduce, x1, x2, bias_real, antiquant_scale_real, antiquant_offset_real,
-                     x3_real, hcom_ptr, reduce_op_ptr, comm_turn, stream_mode, result);
+        if (x3.has_value()) {
+            EXEC_NPU_CMD(aclnnWeightQuantMatmulAllReduce, x1, x2, bias_real, antiquant_scale_real, antiquant_offset_real,
+                         x3_real, hcom_ptr, reduce_op_ptr, comm_turn, stream_mode, x3_real);
+            return x3_real;
+        } else {
+            EXEC_NPU_CMD(aclnnWeightQuantMatmulAllReduce, x1, x2, bias_real, antiquant_scale_real, antiquant_offset_real,
+                         x3_real, hcom_ptr, reduce_op_ptr, comm_turn, stream_mode, result);
+        }
     }
 
     return result;
---
+++ test/test_custom_ops/test_weight_quant_mm_all_reduce_base.py
@@ -0,0 +1,115 @@
+import os
+import unittest
+
+import numpy as np
+import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
+import torch_npu
+
+from torch_npu.testing.testcase import TestCase, run_tests
+from torch_npu.testing.common_utils import create_common_tensor
+from torch_npu.testing.common_distributed import skipIfUnsupportMultiNPU
+
+DEVICE_NAME = torch_npu.npu.get_device_name(0)[:10]
+
+
+class TestMmAllReduceBase(TestCase):
+
+    @classmethod
+    def _init_dist_hccl(cls, rank, world_size):
+        os.environ['MASTER_ADDR'] = '127.0.0.1'
+        os.environ['MASTER_PORT'] = '50000'
+        os.environ['HCCL_WHITELIST_DISABLE'] = '1'
+        torch_npu.npu.set_device(rank)
+        dist.init_process_group(backend='hccl', world_size=world_size, rank=rank)
+        return dist
+
+    @classmethod
+    def _test_npu_mm_all_reduce_base(cls, rank, input_list):
+        x1, x2, scale, offset, world_size, init_pg, c2p = input_list
+        pg = init_pg(rank, world_size)
+        group = pg.distributed_c10d._get_default_group()
+        if torch.__version__ > '2.0.1':
+            hcom_name = group._get_backend(torch.device('npu')).get_hccl_comm_name(rank)
+        else:
+            hcom_name = group.get_hccl_comm_name(rank)
+
+        x1 = x1.npu()
+        x2 = x2.npu()
+        scale = scale.npu()
+        offset = offset.npu()
+        out = torch_npu.npu_mm_all_reduce_base(x1, x2, hcom_name, reduce_op='sum', antiquant_scale=scale,
+                                               antiquant_offset=offset, bias=None, comm_turn=0)
+
+        c2p.put((rank, out.cpu()))
+        pg.barrier()
+
+    def _test_multiprocess(self, f, init_pg, input_list):
+        expt_out_list, x1_list, x2_list, scale, offset, world_size = input_list
+        ctx = mp.get_context('spawn')
+        c2p = ctx.Queue(world_size)
+        ps = []
+
+        for i in range(world_size):
+            p = ctx.Process(
+                target=f,
+                args=(i, [x1_list[i], x2_list[i], scale, offset, world_size, init_pg, c2p]))
+            p.start()
+            ps.append(p)
+
+        for _ in range(world_size):
+            rank, output = c2p.get()
+            self.assertEqual(output, expt_out_list[rank],
+                             ("rank {} Expect receive tensor {} but got {}.").format(rank, expt_out_list, output))
+
+        for p in ps:
+            p.join()
+
+    def _construct_excepted_result(self, x1_list, x2_list, scale, offset, world_size):
+        out = None
+        out_list = []
+        out_dtype = np.float16
+        for i in range(world_size):
+            x1 = x1_list[i]
+            x2 = x2_list[i]
+            weight = torch.add(x2, offset)
+            dequant = torch.mul(weight, scale)
+            out_single = torch.matmul(x1, dequant)
+            if out is None:
+                out = out_single
+            else:
+                out = torch.add(out, out_single)
+        for i in range(world_size):
+            out_list.append(out.to(x1_list[0].dtype))
+        return out_list
+
+    @skipIfUnsupportMultiNPU(8)
+    @unittest.skipIf(DEVICE_NAME != 'Ascend910B',
+        "OP `MatmulAllReduce` is only supported on 910B, skip this ut for this device type!")
+    def test_npu_mm_all_reduce_base(self):
+        world_size = 8
+        dtype = np.float16
+        dtype_quant = np.int8
+        data_format = -1
+        x1_shape = [dtype, data_format, [1, 256]]
+        x2_shape = [dtype_quant, data_format, [256, 256]]
+        scale_shape = [dtype, data_format, [256]]
+        offset_shape = [dtype, data_format, [256]]
+        x1_list = []
+        x2_list = []
+        scale, _ = create_common_tensor(scale_shape, 0.0010, 0.0110) # 量化场景scale的取值范围
+        offset, _ = create_common_tensor(offset_shape, -1, 1)
+        for _ in range(world_size):
+            x1, _ = create_common_tensor(x1_shape, -1, 1)
+            x2, _ = create_common_tensor(x2_shape, -1, 1)
+            x1_list.append(x1)
+            x2_list.append(x2)
+        expt_out_list = self._construct_excepted_result(x1_list, x2_list, scale, offset, world_size)
+        self._test_multiprocess(TestMmAllReduceBase._test_npu_mm_all_reduce_base,
+                                TestMmAllReduceBase._init_dist_hccl,
+                                [expt_out_list, x1_list, x2_list, scale, offset, world_size])
+
+
+if __name__ == '__main__':
+    run_tests()
