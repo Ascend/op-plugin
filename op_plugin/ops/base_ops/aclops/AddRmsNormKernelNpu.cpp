63c0867542720ab21bba3e905be99db04fff820b
TYWZ22259 <wangyuqiao1@huawei.com>
2024-01-19 09:04:53 +0000

    !1171 Add AddRmsNorm PTA support Merge pull request !1171 from TYWZ22259/npu_add_rms_norm

---
+++ op_plugin/ops/base_ops/aclops/AddRmsNormKernelNpu.cpp
@@ -0,0 +1,61 @@
+// Copyright (c) 2024 Huawei Technologies Co., Ltd
+// Copyright (c) 2019, Facebook CORPORATION.
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License  (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "op_plugin/AclOpsInterface.h"
+#include "op_plugin/utils/OpAdapter.h"
+
+namespace acl_op {
+using npu_preparation = at_npu::native::OpPreparation;
+
+namespace {
+c10::SmallVector<int64_t, SIZE> get_rstd_shape(const at::Tensor &self, const at::Tensor &gamma)
+{
+    c10::SmallVector<int64_t, SIZE> ret;
+    auto rstd_dim = self.dim() - gamma.dim();
+    for (uint64_t i = 0; i < self.dim(); i++) {
+        if (i < rstd_dim) {
+            ret.emplace_back(self.size(i));
+        } else {
+            ret.emplace_back(1);
+        }
+    }
+    return ret;
+}
+
+} // namespace
+
+std::tuple<at::Tensor, at::Tensor, at::Tensor> npu_add_rms_norm(const at::Tensor &x1, const at::Tensor &x2, const at::Tensor &gamma, double epsilon)
+{
+    TORCH_CHECK(x1.dim() >= gamma.dim(), "The gamma shape should not be bigger than self shape.");
+    at::Tensor y = npu_preparation::apply_tensor(x1.sizes(), x1.options().dtype(gamma.dtype()), x1);
+    auto rstd_shape = get_rstd_shape(x1, gamma);
+    at::Tensor rstd = npu_preparation::apply_tensor(rstd_shape, x1.options().dtype(at::kFloat), x1);
+    at::Tensor x = npu_preparation::apply_tensor(x1.sizes(), x1.options().dtype(gamma.dtype()), x1);
+
+    at_npu::native::OpCommand cmd;
+    cmd.Name("AddRmsNorm")
+        .Input(x1, "x1")
+        .Input(x2, "x2")
+        .Input(gamma, "gamma")
+        .Output(y, "y")
+        .Output(rstd, "rstd")
+        .Output(x, "x")
+        .Attr("epsilon", static_cast<float>(epsilon))
+        .Run();
+
+    return std::make_tuple(y, rstd, x);
+}
+} // namespace acl_op
---
+++ test/test_custom_ops/test_npu_add_rms_norm.py
@@ -0,0 +1,70 @@
+import unittest
+
+import numpy as np
+import torch
+import torch_npu
+
+from torch_npu.testing.testcase import TestCase, run_tests
+from torch_npu.testing.common_utils import get_npu_device
+
+DEVICE_NAME = torch_npu.npu.get_device_name(0)[:10]
+
+
+class TestNPUAddRmsNorm(TestCase):
+
+    def supported_op_exec(self, x1, x2, gamma, epsilon=1e-6):
+        ori_dtype = x1.dtype
+        x = x1 + x2
+        if ori_dtype == np.float16:
+            x = x.astype(np.float32)
+        variance = np.mean(np.power(x, 2), axis=-1, keepdims=True)
+        std = np.sqrt(variance + epsilon)
+        rstd = np.divide(1, std)
+        result_mid = x * rstd
+        result = result_mid * gamma
+        if ori_dtype == np.float16:
+            x = x.astype(np.float16)
+            result = result.astype(np.float16)
+        return result, rstd, x
+
+    def custom_op_exec(self, x1, x2, gamma, epsilon=1e-6):
+        y, rstd, x = torch_npu.npu_add_rms_norm(x1, x2, gamma, epsilon)
+        return y.cpu().numpy(), rstd.cpu().numpy(), x.cpu().numpy()
+
+    @unittest.skipIf(DEVICE_NAME != 'Ascend910B',
+        "OP `AddRmsNorm` is only supported on , skip this ut for this device type!")
+    def test_add_rms_norm_fp32(self, device="npu"):
+        if device is None:
+            device = get_npu_device()
+        cpu_input0 = np.random.uniform(0, 100, [1024, 12288]).astype(np.float32)
+        cpu_input1 = np.random.uniform(0, 100, [1024, 12288]).astype(np.float32)
+        cpu_input2 = np.random.uniform(0, 100, [12288]).astype(np.float32)
+        npu_input0 = torch.from_numpy(cpu_input0).npu()
+        npu_input1 = torch.from_numpy(cpu_input1).npu()
+        npu_input2 = torch.from_numpy(cpu_input2).npu()
+
+        supported_output0, supported_output1, supported_output2 = self.supported_op_exec(cpu_input0, cpu_input1, cpu_input2)
+        custom_output0, custom_output1, custom_output2 = self.custom_op_exec(npu_input0, npu_input1, npu_input2)
+        self.assertRtolEqual(supported_output0, custom_output0, 0.0001)
+        self.assertRtolEqual(supported_output1, custom_output1, 0.0001)
+        self.assertRtolEqual(supported_output2, custom_output2, 0.0001)
+
+    @unittest.skipIf(DEVICE_NAME != 'Ascend910B',
+        "OP `AddRmsNorm` is only supported on , skip this ut for this device type!")
+    def test_add_rms_norm_fp16(self):
+        cpu_input0 = np.random.uniform(0, 100, [1024, 12288]).astype(np.float16)
+        cpu_input1 = np.random.uniform(0, 100, [1024, 12288]).astype(np.float16)
+        cpu_input2 = np.random.uniform(0, 100, [12288]).astype(np.float16)
+        npu_input0 = torch.from_numpy(cpu_input0).npu()
+        npu_input1 = torch.from_numpy(cpu_input1).npu()
+        npu_input2 = torch.from_numpy(cpu_input2).npu()
+
+        supported_output0, supported_output1, supported_output2 = self.supported_op_exec(cpu_input0, cpu_input1, cpu_input2)
+        custom_output0, custom_output1, custom_output2 = self.custom_op_exec(npu_input0, npu_input1, npu_input2)
+        self.assertRtolEqual(supported_output0, custom_output0, 0.001)
+        self.assertRtolEqual(supported_output1, custom_output1, 0.001)
+        self.assertRtolEqual(supported_output2, custom_output2, 0.001)
+
+
+if __name__ == "__main__":
+    run_tests()
