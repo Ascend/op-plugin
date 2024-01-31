1cfe68c1a95e6610e4b5995d891a4542b5376b59
罗骏 <luojun46@huawei.com>
2024-01-31 09:37:20 +0000

    !1197 AddLayerNormGrad Add Optional Input Merge pull request !1197 from 罗骏/add_optional_input

--- op_plugin/ops/base_ops/aclops/AddLayerNormBackwardKernelNpu.cpp
+++ op_plugin/ops/base_ops/aclops/AddLayerNormBackwardKernelNpu.cpp
@@ -20,29 +20,38 @@
 namespace acl_op {
 using npu_preparation = at_npu::native::OpPreparation;
 
-std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor>npu_add_layer_norm_backward(const at::Tensor &dy,
+std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor>npu_add_layer_norm_backward(const c10::optional<at::Tensor> &dy_opt,
                                                                                       const at::Tensor &x1,
                                                                                       const at::Tensor &x2,
                                                                                       const at::Tensor &rstd,
                                                                                       const at::Tensor &mean,
-                                                                                      const at::Tensor &gamma)
+                                                                                      const at::Tensor &gamma,
+                                                                                      const c10::optional<at::Tensor> &dsum_opt)
 {
     at::SmallVector<int64_t, SIZE> shape;
-    shape.emplace_back(gamma.size(0));
+    for (uint64_t index = 0; index < gamma.dim(); index++) {
+        shape.emplace_back(gamma.size(index));
+    }
 
-    at::Tensor dx = npu_preparation::apply_tensor(dy);
+    const at::Tensor &dy = c10::value_or_else(dy_opt, [] { return at::Tensor(); });
+    const at::Tensor &dsum = c10::value_or_else(dsum_opt, [] { return at::Tensor(); });
+    auto d_y = dy.defined() ? dy : at::zeros(x1.sizes(), x1.options());
+    auto d_sum = dsum.defined() ? dsum : at::zeros(x1.sizes(), x1.options());
+
+    at::Tensor dx = npu_preparation::apply_tensor(d_y);
     at::Tensor dgamma = npu_preparation::apply_tensor(shape, x1.options().dtype(at::kFloat), x1);
     at::Tensor dbeta = npu_preparation::apply_tensor(shape, x1.options().dtype(at::kFloat), x1);
 
     at_npu::native::OpCommand cmd;
 
     cmd.Name("AddLayerNormGrad")
-        .Input(dy, "dy")
+        .Input(d_y, "dy")
         .Input(x1, "x1")
         .Input(x2, "x2")
         .Input(rstd, "rstd")
         .Input(mean, "mean")
         .Input(gamma, "gamma")
+        .Input(d_sum, "dsum")
         .Output(dx, "dx")
         .Output(dgamma, "dgamma")
         .Output(dbeta, "dbeta")
