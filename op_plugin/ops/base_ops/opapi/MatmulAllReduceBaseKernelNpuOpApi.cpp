bf727b7c17e3c14e11d4d448489913ee58730e9c
王嘉诚 <12731429+wang-pierre-jiacheng@user.noreply.gitee.com>
2024-02-29 12:32:38 +0000

    !1312 【op-plugin】support A8W8 BF16 Merge pull request !1312 from 王嘉诚/master

--- op_plugin/ops/base_ops/opapi/MatmulAllReduceBaseKernelNpuOpApi.cpp
+++ op_plugin/ops/base_ops/opapi/MatmulAllReduceBaseKernelNpuOpApi.cpp
@@ -19,20 +19,34 @@
 namespace op_api {
 using npu_preparation = at_npu::native::OpPreparation;
 
+at::ScalarType get_output_dtype(const at::Tensor &x1, const c10::optional<at::Tensor> &dequant_scale)
+{
+    auto output_dtype = x1.scalar_type() == at::kChar ? at::ScalarType::Half : x1.scalar_type();
+    if (dequant_scale.has_value()) {
+        const at::Tensor &dequant = dequant_scale.value();
+        if (dequant.scalar_type() == at::kBFloat16) {
+            output_dtype = at::kBFloat16;
+        }
+    }
+    return output_dtype;
+}
+
 void check_params(const at::Tensor &x1, const at::Tensor &x2,
                   const c10::optional<at::Tensor> &antiquant_scale,
                   const c10::optional<at::Tensor> &antiquant_offset,
                   const c10::optional<at::Tensor> &x3,
                   const c10::optional<at::Tensor> &dequant_scale)
 {
-    // 对x1和x2的shape进行校验，满足matmul计算规则
+    // check shape: shape of x1:[s,m,k], shape of x2:[k,n], k_x1 == k_x2
     TORCH_CHECK(x2.dim() == 2, "x2 needs to be 2D, but got: ", x2.dim(), "D");
     TORCH_CHECK(x1.size(x1.dim() - 1) == x2.size(0), "K of x1 and x2 should be same, but they are x1_k: ",
                 x1.size(x1.dim() - 1), ", x2_k: ", x2.size(0));
 
-    // 由于aclnn接口不接收冗余传入的参数，aclnn内部无法提示冗余参数，需要在此进行校验。
-    // 对于MC2非量化场景，aclnn接口不对antiquant_scale、antiquant_offset、dequant_scale进行处理，在此校验住，不要传入冗余参数。
-    // A8W8; antiquantScale NULL antiquantOffset NULL
+    // check parameters.
+    // aclnn apis for MC2 share one torch_npu api, therefore, each aclnn api only accepts parameters
+    // that will be used. Any unused parameter will be seen as illegal. The job must be done here in
+    // torch_npu api.
+    // A8W8: antiquantScale and antiquantOffset should be None.
     if (isIntegralType(x1.scalar_type()) && isIntegralType(x2.scalar_type())) {
         TORCH_CHECK(x1.scalar_type() == at::kChar, "x1 must be an int8 tensor for quant.");
         TORCH_CHECK(x2.scalar_type() == at::kChar, "x2 must be an int8 tensor for quant.");
@@ -40,15 +54,13 @@ void check_params(const at::Tensor &x1, const at::Tensor &x2,
                     "when both dtype of x1 and dtype of x2 are equal to int8, "
                     "antiquantScale, antiquantOffset should both be null");
     }
-    // 对于MC2伪量化场景A16W8，aclnn接口不对dequant_scale进行处理，在此校验住，不要传入冗余参数。
-    // A16W8; dequantScale NULL
+    // A16W8: dequantScale should be None.
     if (!isIntegralType(x1.scalar_type()) && isIntegralType(x2.scalar_type())) {
         TORCH_CHECK(x2.scalar_type() == at::kChar, "x2 must be an int8 tensor for weight quant.");
         TORCH_CHECK((!dequant_scale.has_value()),
                     "when only dtype of x2 is equal to int8, dequantScale should be null");
     }
-    // 对于MC2全量化场景A8W8，aclnn接口不对antiquant_scale、antiquant_offset进行处理，在此校验住，不要传入冗余参数。
-    // MC2 without quant; antiquantScale NULL antiquantOffset NULL dequantScale NULL
+    // MC2 without quantization. antiquantScale and antiquantOffset and dequantScale should be None.
     if (!isIntegralType(x1.scalar_type()) && !isIntegralType(x2.scalar_type())) {
         TORCH_CHECK((!antiquant_scale.has_value() && !antiquant_offset.has_value() && !dequant_scale.has_value()),
                     "when neither dtype of x1 or dtype of x2 is equal to int8, "
@@ -59,9 +71,9 @@ void check_params(const at::Tensor &x1, const at::Tensor &x2,
     if (x3.has_value()) {
         auto output_size = op_infer::array_to_small_vector(x1.sizes());
         output_size[x1.dim() - 1] = x2.size(1);
-        // a8w8: dtype of output should be half.
-        auto output_dtype = x1.scalar_type() == at::kChar ? at::ScalarType::Half : x1.scalar_type();
-        const at::Tensor &x3_real = x3.value_or(at::Tensor());
+        // A8W8: dtype of output should be half or bfloat16.
+        auto output_dtype = get_output_dtype(x1, dequant_scale);
+        const at::Tensor &x3_real = x3.value();
         TORCH_CHECK(x3_real.sizes().equals(output_size), "x3 with shape ", x3_real.sizes(),
                     " doesn't match the output shape ", output_size);
         TORCH_CHECK(x3_real.scalar_type() == output_dtype, "x3 with dtype ", x3_real.scalar_type(),
@@ -81,7 +93,7 @@ at::Tensor npu_mm_all_reduce_base(const at::Tensor &x1, const at::Tensor &x2, c1
     auto output_size = op_infer::array_to_small_vector(x1.sizes());
     output_size[x1.dim() - 1] = x2.size(1);
     // a8w8: dtype of output should be half.
-    auto output_dtype = x1.scalar_type() == at::kChar ? at::ScalarType::Half : x1.scalar_type();
+    auto output_dtype = get_output_dtype(x1, dequant_scale);
     auto result = at_npu::native::OpPreparation::apply_tensor_without_format(output_size,
                                                                              x1.options().dtype(output_dtype));
     char *reduce_op_ptr = const_cast<char *>(reduce_op.data());
@@ -90,7 +102,7 @@ at::Tensor npu_mm_all_reduce_base(const at::Tensor &x1, const at::Tensor &x2, c1
     const at::Tensor &x3_real = x3.value_or(at::Tensor());
     int64_t stream_mode = ACL_STOP_ON_FAILURE;
     // a8w8: x1\x2 kChar; a16w8: x2 kChar;
-    if (!isIntegralType(x2.scalar_type())) {
+    if (!isIntegralType(x1.scalar_type()) && !isIntegralType(x2.scalar_type())) {
         if (x3.has_value()) {
             EXEC_NPU_CMD(aclnnMatmulAllReduceV2, x1, x2, bias_real, x3_real, hcom_ptr, reduce_op_ptr, comm_turn,
                          stream_mode, x3_real);
@@ -99,11 +111,13 @@ at::Tensor npu_mm_all_reduce_base(const at::Tensor &x1, const at::Tensor &x2, c1
             EXEC_NPU_CMD(aclnnMatmulAllReduce, x1, x2, bias_real, hcom_ptr, reduce_op_ptr, comm_turn, stream_mode,
                          result);
         }
-    } else if (isIntegralType(x1.scalar_type())) {
+    }
+    if (isIntegralType(x1.scalar_type()) && isIntegralType(x2.scalar_type())) {
         const at::Tensor &dequant_scale_real = dequant_scale.value_or(at::Tensor());
         EXEC_NPU_CMD(aclnnQuantMatmulAllReduce, x1, x2, bias_real, x3_real, dequant_scale_real, hcom_ptr,
                      reduce_op_ptr, comm_turn, stream_mode, result);
-    } else {
+    }
+    if (!isIntegralType(x1.scalar_type()) && isIntegralType(x2.scalar_type())) {
         const at::Tensor &antiquant_scale_real = antiquant_scale.value_or(at::Tensor());
         const at::Tensor &antiquant_offset_real = antiquant_offset.value_or(at::Tensor());
         if (x3.has_value()) {
