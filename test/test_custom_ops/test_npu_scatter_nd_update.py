4b89ae71977c9c06bd3656911cda115a1477291c
董鸿儒 <donghongru@h-partners.com>
2024-03-04 03:20:35 +0000

    !1215 add scatterndupdate Merge pull request !1215 from 董鸿儒/cherry-pick-1700466999

---
+++ test/test_custom_ops/test_npu_scatter_nd_update.py
@@ -0,0 +1,39 @@
+import math
+import unittest
+import numpy as np
+import torch
+
+import torch_npu
+from torch_npu.testing.testcase import TestCase, run_tests
+from torch_npu.testing.common_utils import SupportedDevices
+
+torch.npu.set_compile_mode(jit_compile=False)
+torch.npu.config.allow_internal_format = False
+
+
+class TestNpuScatterNdUpdate(TestCase):
+
+    def supported_op_exec(self, var, indices_tensor, updates):
+        for i in range(len(var)):
+            if i < len(indices_tensor):
+                var[indices_tensor[i][0]][indices_tensor[i][1]] = updates[i]
+
+        return var
+
+    def custom_op_exec(self, var, indices_tensor, updates):
+        return torch_npu.npu_scatter_nd_update(var, indices_tensor, updates)
+
+    @SupportedDevices(['Ascend910B'])
+    def test_npu_scatter_nd_update(self, device="npu"):
+        var = torch.zeros([3, 2], dtype=torch.float16).npu()
+        indices = np.array([[0, 0], [1, 1]])
+        indices_tensor = torch.from_numpy(indices).to(device)
+        updates = np.array([10, 20])
+
+        supported_output = self.supported_op_exec(var, indices_tensor, updates)
+        custom_output = self.custom_op_exec(var, indices_tensor, updates)
+        self.assertEqual(supported_output, custom_output)
+
+
+if __name__ == "__main__":
+    run_tests()
