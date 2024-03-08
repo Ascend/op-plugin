639d1c7130a509ef2220060ffd3a146237578915
朱维琛 <zhuweichen2@huawei.com>
2024-03-08 04:06:26 +0000

    !1355 Fix index calling aclop nonzero Merge pull request !1355 from 朱维琛/nonzerov2

--- op_plugin/ops/v1r11/opapi/IndexKernelNpuOpApi.cpp
+++ op_plugin/ops/v1r11/opapi/IndexKernelNpuOpApi.cpp
@@ -46,7 +46,7 @@ at::Tensor index(const at::Tensor& self, const torch::List<c10::optional<at::Ten
     return at::native::index(self, orig);
   }
   bool needCast = op_plugin::AdvanceIndex::checkIndexTensorTypes(orig);
-  auto indices = op_plugin::AdvanceIndex::npu_expand_tensors(self, orig, needCast);
+  auto indices = op_plugin::AdvanceIndex::npu_expand_tensors(self, orig, needCast, true);
   return index_high_dims_op_api(self, indices);
 }
 
--- op_plugin/ops/v2r1/opapi/IndexKernelNpuOpApi.cpp
+++ op_plugin/ops/v2r1/opapi/IndexKernelNpuOpApi.cpp
@@ -43,7 +43,7 @@ at::Tensor index_high_dims_op_api(const at::Tensor& self, std::vector<at::Tensor
 at::Tensor index(const at::Tensor& self, const torch::List<c10::optional<at::Tensor>>& orig) {
   DO_COMPATIBILITY(aclnnIndex, acl_op::index(self, orig));
   bool needCast = op_plugin::AdvanceIndex::checkIndexTensorTypes(orig);
-  auto indices = op_plugin::AdvanceIndex::npu_expand_tensors(self, orig, needCast);
+  auto indices = op_plugin::AdvanceIndex::npu_expand_tensors(self, orig, needCast, true);
   return index_high_dims_op_api(self, indices);
 }
 
