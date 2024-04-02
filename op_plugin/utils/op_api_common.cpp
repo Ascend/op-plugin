9b148a34188b7a59f0f6b0de9ea071811f20903e
宋恺 <songkai16@huawei.com>
2024-04-02 08:41:15 +0000

    !1441 change libopapi sub so name Merge pull request !1441 from 宋恺/master

--- op_plugin/utils/op_api_common.cpp
+++ op_plugin/utils/op_api_common.cpp
@@ -312,12 +312,9 @@ void *GetOpApiFuncAddrFromFeatureLib(const char *api_name)
 {
     GET_OP_API_FUNC_FROM_FEATURE_LIB(ops_infer_handler, "libaclnn_ops_infer.so");
     GET_OP_API_FUNC_FROM_FEATURE_LIB(ops_train_handler, "libaclnn_ops_train.so");
-    GET_OP_API_FUNC_FROM_FEATURE_LIB(adv_infer_handler, "libaclnn_adv_infer.so");
-    GET_OP_API_FUNC_FROM_FEATURE_LIB(adv_train_handler, "libaclnn_adv_train.so");
-    GET_OP_API_FUNC_FROM_FEATURE_LIB(dvpp_handler, "libacl_dvpp_op.so");
-    GET_OP_API_FUNC_FROM_FEATURE_LIB(sparse_handler, "libaclsparse.so");
-    GET_OP_API_FUNC_FROM_FEATURE_LIB(optim_handler, "libacloptim.so");
-    GET_OP_API_FUNC_FROM_FEATURE_LIB(fft_handler, "libaclfft.so");
-    GET_OP_API_FUNC_FROM_FEATURE_LIB(rand_handler, "libaclrand.so");
+    GET_OP_API_FUNC_FROM_FEATURE_LIB(math_handler, "libaclnn_math.so");
+    GET_OP_API_FUNC_FROM_FEATURE_LIB(sparse_handler, "libaclnn_sparse.so");
+    GET_OP_API_FUNC_FROM_FEATURE_LIB(fft_handler, "libaclnn_fft.so");
+    GET_OP_API_FUNC_FROM_FEATURE_LIB(rand_handler, "libaclnn_rand.so");
     return nullptr;
 }
