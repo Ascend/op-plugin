fb6261d80ee73627363ab143628b0767377ee1c4
张逸凡 <zhangyifan115@huawei.com>
2024-04-12 03:22:34 +0000

    !1486 fix coalesce sparse bf16 bug Merge pull request !1486 from 张逸凡/master

--- op_plugin/ops/v2r1/opapi/sparse/SparseCoalesceKernelOpApi.cpp
+++ op_plugin/ops/v2r1/opapi/sparse/SparseCoalesceKernelOpApi.cpp
@@ -53,7 +53,7 @@ SparseTensor _coalesce_sparse(const SparseTensor& self)
         indices.options().dtype(at::kInt));
     at::Tensor indices_t = at_npu::native::NpuUtils::format_contiguous(indices.transpose(0, 1)).to(at::kInt);
     at::Tensor unique_indices = std::get<1>(unique_indices_info).to(at::kInt);
-    if (values.scalar_type() == at::kHalf) {
+    if (values.scalar_type() == at::kHalf || (values.scalar_type() == at::kBFloat16)) {
         at::Tensor values_f = values.to(at::kFloat);
         at::Tensor new_values_f = at::zeros(new_values_size, values.options()).to(at::kFloat);
         EXEC_NPU_CMD(
@@ -65,9 +65,15 @@ SparseTensor _coalesce_sparse(const SparseTensor& self)
             new_indices_t,
             new_values_f);
         at::Tensor new_indices = new_indices_t.transpose(0, 1).to(at::kLong);
-        at::Tensor new_values = new_values_f.to(at::kHalf);
-        SparseTensor dst = ::at::native::_sparse_coo_tensor_unsafe(new_indices, new_values, self.sizes())._coalesced_(true);
-        return dst;
+        if (values.scalar_type() == at::kHalf) {
+            at::Tensor new_values = new_values_f.to(at::kHalf);
+            SparseTensor dst = ::at::native::_sparse_coo_tensor_unsafe(new_indices, new_values, self.sizes())._coalesced_(true);
+            return dst;
+        } else {
+            at::Tensor new_values = new_values_f.to(at::kBFloat16);
+            SparseTensor dst = ::at::native::_sparse_coo_tensor_unsafe(new_indices, new_values, self.sizes())._coalesced_(true);
+            return dst;
+        }
     } else {
         at::Tensor new_values = at::zeros(new_values_size, values.options());
         EXEC_NPU_CMD(
