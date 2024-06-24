a6caf34d3c01f46abbdd686eb121b32a738b49bc
刘嘉巍 <843972097@qq.com>
2024-06-24 13:26:21 +0000

    !1643 [Fix] torch.polar to cpu Merge pull request !1643 from 刘嘉巍/6.0.rc2

---
+++ op_plugin/ops/base_ops/opapi/PolarKernelNpuOpApi.cpp
@@ -0,0 +1,46 @@
+// Copyright (c) 2024 Huawei Technologies Co., Ltd
+// Copyright (c) 2019, Facebook CORPORATION.
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License  (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "op_plugin/OpApiInterface.h"
+
+namespace op_api {
+
+at::Tensor& polar_out(const at::Tensor& abs, const at::Tensor& angle, at::Tensor& result)
+{
+    TORCH_WARN_ONCE(
+        "Warning: kernel [polar.out] is not supported by NPU currently. Now this kernel is running on CPU.");
+    auto abs_cpu = abs.cpu();
+    auto angle_cpu = angle.cpu();
+    auto result_cpu = result.cpu();
+    result_cpu = at::polar_out(result_cpu, abs_cpu, angle_cpu);
+
+    result.copy_(result_cpu.to(result.device()));
+    return result;
+}
+
+at::Tensor polar(const at::Tensor& abs, const at::Tensor& angle)
+{
+    TORCH_WARN_ONCE(
+        "Warning: kernel [polar] is not supported by NPU currently. Now this kernel is running on CPU.");
+
+    auto abs_cpu = abs.cpu();
+    auto angle_cpu = angle.cpu();
+    at::Tensor result_cpu = at::polar(abs_cpu, angle_cpu);
+
+    at::Tensor result = result_cpu.to(abs.device());
+    return result;
+}
+} // namespace op_api
