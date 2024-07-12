ca0f70402186676bb01eb47e7776c8a06512b816
流浪者LingLong <linglong2@huawei.com>
2024-07-12 09:54:30 +0000

    !1670 add foreach op lerp and sub, and add op st Merge pull request !1670 from 流浪者LingLong/master

--- op_plugin/ops/base_ops/opapi/ForeachSubScalarInplaceKernelOpApi.cpp
+++ op_plugin/ops/base_ops/opapi/ForeachSubScalarInplaceKernelOpApi.cpp
@@ -1,25 +1,77 @@
 #include <ATen/native/ForeachUtils.h>
 #include "op_plugin/OpApiInterface.h"
 #include "op_plugin/utils/op_api_common.h"
+#include "op_plugin/utils/custom_functions/opapi/scalar_op_api.h"
 
 namespace op_api {
 using npu_preparation = at_npu::native::OpPreparation;
 
-void _foreach_sub_(const at::TensorList self, const at::Scalar &scalar)
+void _split_and_exec_npu_cmd_sub_scalar(at::TensorList tensors1, const at::Scalar &scalar, at::TensorList &result_list, bool is_inplace)
+{
+    size_t tensor_count = tensors1.size();
+    size_t max_tensor_count = is_inplace ? 48 : 24;
+
+    size_t loop_time = tensor_count / max_tensor_count;
+    at::Scalar scalar_ = op_api::adaptToDouble(scalar, tensors1);
+
+    if (tensor_count <= max_tensor_count) {
+        EXEC_NPU_CMD(aclnnForeachSubScalar, tensors1, scalar_, result_list);
+        return;
+    }
+    for (size_t i = 0; i < loop_time; i++) {
+        at::TensorList temp_tensors1(tensors1.data() + i * max_tensor_count, max_tensor_count);
+        at::TensorList temp_result(result_list.data() + i * max_tensor_count, max_tensor_count);
+        EXEC_NPU_CMD(aclnnForeachSubScalar, temp_tensors1, scalar_, temp_result);
+    }
+
+    size_t remaining_count = tensor_count % max_tensor_count;
+    if (remaining_count) {
+        at::TensorList temp_tensors1(tensors1.data() + loop_time * max_tensor_count, remaining_count);
+        at::TensorList temp_result(result_list.data() + loop_time * max_tensor_count, remaining_count);
+        EXEC_NPU_CMD(aclnnForeachSubScalar, temp_tensors1, scalar_, temp_result);
+    }
+}
+
+std::vector<at::Tensor> _foreach_sub(at::TensorList self, const at::Scalar& scalar)
+{
+    // Fallback
+    at::native::check_foreach_api_restrictions(self);
+    if (!at::native::can_use_fast_route(self, scalar, false)) {
+        return at::native::foreach_tensor_sub_scalar_kernel_slow(self, scalar);
+    }
+
+    // Type Check
+    auto scalar_type = self[0].scalar_type();
+    TORCH_CHECK(scalar_type == at::ScalarType::Half || scalar_type == at::ScalarType::Float ||
+                scalar_type == at::ScalarType::Int || scalar_type == at::ScalarType::BFloat16,
+                "input must be half, float, int32 or bfloat16");
+    
+    std::vector<at::Tensor> result(self.size());
+    auto iterRes = result.data();
+    int i = 0;
+    for (const at::Tensor &tensor : self) {
+        auto output_size = op_infer::input_same_output_size(tensor);
+        iterRes[i++] = npu_preparation::apply_tensor_without_format(output_size, tensor.options().dtype(scalar_type));
+    }
+    at::TensorList result_ = at::TensorList(result);
+
+    _split_and_exec_npu_cmd_sub_scalar(self, scalar, result_, false);
+
+    return result;
+}
+
+void _foreach_sub_(at::TensorList self, const at::Scalar& scalar)
 {
     at::native::check_foreach_api_restrictions(self);
-    if (!at_npu::native::env::CheckJitDisable() ||
-        !at::native::can_use_fast_route(self, scalar, false)) {
+    if (!at::native::can_use_fast_route(self, scalar, false)) {
         return at::native::foreach_tensor_sub_scalar_kernel_slow_(self, scalar);
     }
 
     auto scalar_type = self[0].scalar_type();
-    if (scalar_type != at::ScalarType::Half && scalar_type != at::ScalarType::Float &&
-        scalar_type != at::ScalarType::Int) {
-        TORCH_CHECK(false, "input must be half, float or int32", OPS_ERROR(ErrCode::TYPE));
-    }
-    at::Tensor scalar_tensor = npu_preparation::copy_scalar_to_device(scalar, scalar_type, self[0].device());
-    EXEC_NPU_CMD(aclnnForeachSubScalar, self, scalar_tensor, self);
-}
 
-} // namespace op_api
+    TORCH_CHECK(scalar_type == at::ScalarType::Half || scalar_type == at::ScalarType::Float ||
+                scalar_type == at::ScalarType::Int || scalar_type == at::ScalarType::BFloat16,
+                "input must be half, float, int32 or bfloat16");
+    _split_and_exec_npu_cmd_sub_scalar(self, scalar, self, true);
+}
+}
---
+++ test/test_v2r1_ops/test_foreach_lerp_scalar.py
@@ -0,0 +1,115 @@
+import unittest
+import random
+import torch
+import torch_npu
+import hypothesis
+from torch_npu.testing.testcase import TestCase, run_tests
+from torch_npu.testing.common_utils import SupportedDevices
+
+
+class TestForeachLerpScalar(TestCase):
+
+    torch_dtypes = {
+        "float16" : torch.float16,
+        "float32" : torch.float32,
+        "bfloat16" : torch.bfloat16
+    }
+
+    def create_tensors(self, dtype, shapes):
+        cpu_tensors = []
+        npu_tensors = []
+        if dtype == "int32":
+            for shape in shapes:
+                t = torch.randint(low=-10, high=100, size=(shape[0], shape[1]), dtype=self.torch_dtypes.get(dtype))
+                cpu_tensors.append(t)
+                npu_tensors.append(t.npu())
+        else:
+            for shape in shapes:
+                t = torch.randn((shape[0], shape[1]), dtype=self.torch_dtypes.get(dtype))
+                cpu_tensors.append(t)
+                npu_tensors.append(t.npu())
+        return tuple(cpu_tensors), tuple(npu_tensors)
+    
+    def create_input_tensors(self, tensor_num, dtype):
+        input_nums = 2
+        cpu_inputs = []
+        npu_inputs = []
+        shapes = []
+        for i in range(tensor_num):
+            m = random.randint(1, 100)
+            n = random.randint(1, 100)
+            shapes.append([m, n])
+        for i in range(input_nums) :
+            cpu_tensors, npu_tensors = self.create_tensors(dtype, shapes)
+            cpu_inputs.append(cpu_tensors)
+            npu_inputs.append(npu_tensors)
+        return cpu_inputs, npu_inputs
+
+    @SupportedDevices(['Ascend910B'])
+    def test_foreach_lerp_scalar_out_float32_shpae_tensor_num(self):
+        tensor_num_list = [20, 50]
+        for tensor_num in tensor_num_list :
+            cpu_tensors, npu_tensors = self.create_input_tensors(tensor_num, "float32")
+            scalar = 5.0
+            cpu_output = torch._foreach_lerp(cpu_tensors[0], cpu_tensors[1], scalar)
+            npu_output = torch._foreach_lerp(npu_tensors[0], npu_tensors[1], scalar)
+
+            self.assertRtolEqual(cpu_output, npu_output)
+    
+    @SupportedDevices(['Ascend910B'])
+    def test_foreach_lerp_scalar_out_float16_shpae_tensor_num(self):
+        tensor_num_list = [20, 50]
+        for tensor_num in tensor_num_list :
+            cpu_tensors, npu_tensors = self.create_input_tensors(tensor_num, "float16")
+            scalar = 5.0
+            cpu_output = torch._foreach_lerp(cpu_tensors[0], cpu_tensors[1], scalar)
+            npu_output = torch._foreach_lerp(npu_tensors[0], npu_tensors[1], scalar)
+
+            self.assertRtolEqual(cpu_output, npu_output)
+
+    @SupportedDevices(['Ascend910B'])
+    def test_foreach_lerp_scalar_out_bfloat16_shpae_tensor_num(self):
+        tensor_num_list = [20, 50]
+        for tensor_num in tensor_num_list :
+            cpu_tensors, npu_tensors = self.create_input_tensors(tensor_num, "bfloat16")
+            scalar = 5.0
+            cpu_output = torch._foreach_lerp(cpu_tensors[0], cpu_tensors[1], scalar)
+            npu_output = torch._foreach_lerp(npu_tensors[0], npu_tensors[1], scalar)
+
+            self.assertRtolEqual(cpu_output, npu_output)
+
+    @SupportedDevices(['Ascend910B'])
+    def test_foreach_lerp_scalar_inplace_float32_shpae_tensor_num(self):
+        tensor_num_list = [20, 50]
+        for tensor_num in tensor_num_list :
+            cpu_tensors, npu_tensors = self.create_input_tensors(tensor_num, "float32")
+            scalar = 5.0
+            torch._foreach_lerp_(cpu_tensors[0], cpu_tensors[1], scalar)
+            torch._foreach_lerp_(npu_tensors[0], npu_tensors[1], scalar)
+
+            self.assertRtolEqual(cpu_tensors[0], npu_tensors[0])
+    
+    @SupportedDevices(['Ascend910B'])
+    def test_foreach_lerp_scalar_inplace_float16_shpae_tensor_num(self):
+        tensor_num_list = [20, 50]
+        for tensor_num in tensor_num_list :
+            cpu_tensors, npu_tensors = self.create_input_tensors(tensor_num, "float16")
+            scalar = 5.0
+            torch._foreach_lerp_(cpu_tensors[0], cpu_tensors[1], scalar)
+            torch._foreach_lerp_(npu_tensors[0], npu_tensors[1], scalar)
+
+            self.assertRtolEqual(cpu_tensors[0], npu_tensors[0])
+            
+    @SupportedDevices(['Ascend910B'])
+    def test_foreach_lerp_scalar_inplace_bfloat16_shpae_tensor_num(self):
+        tensor_num_list = [20, 50]
+        for tensor_num in tensor_num_list :
+            cpu_tensors, npu_tensors = self.create_input_tensors(tensor_num, "bfloat16")
+            scalar = 5.0
+            torch._foreach_lerp_(cpu_tensors[0], cpu_tensors[1], scalar)
+            torch._foreach_lerp_(npu_tensors[0], npu_tensors[1], scalar)
+
+            self.assertRtolEqual(cpu_tensors[0], npu_tensors[0])
+
+if __name__ == "__main__":
+    run_tests()
