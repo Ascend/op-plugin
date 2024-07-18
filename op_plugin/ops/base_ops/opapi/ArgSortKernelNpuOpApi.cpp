25505533bb3dc5717dc6ab49a64aaf2d8f3f5fa4
zhangqiongwen <zhangqiongwen@huawei.com>
2024-07-18 11:40:22 +0000

    !1675 [adpt]add argsort.stable in opapi Merge pull request !1675 from zhangqiongwen/master_argsort_stable_adpt

--- op_plugin/ops/base_ops/opapi/ArgSortKernelNpuOpApi.cpp
+++ op_plugin/ops/base_ops/opapi/ArgSortKernelNpuOpApi.cpp
@@ -22,26 +22,26 @@ using npu_preparation = at_npu::native::OpPreparation;
 
 at::Tensor argsort(const at::Tensor &self, int64_t dim, bool descending)
 {
-    DO_COMPATIBILITY(aclnnArgsort, acl_op::argsort(self, dim, descending));
     if (self.dtype() == at::kInt || self.dtype() == at::kLong) {
         TORCH_NPU_WARN_ONCE("Warning: kernel [ArgSort] can not support dtype int32 or int64 on AiCore, Now this kernel "
                             "is running on AiCpu."
                             "If you are more concerned about high-performance execution,please cast dtype to float32.");
     }
-    // construct the output tensor of the NPU
-    at::Tensor indices = npu_preparation::apply_tensor_without_format(self.sizes(), self.options().dtype(at::kLong));
-    EXEC_NPU_CMD(aclnnArgsort, self, dim, descending, indices);
-    return indices;
+    return std::get<1>(at::sort(self, dim, descending));
 }
 
 at::Tensor argsort(const at::Tensor &self, at::Dimname dim, bool descending)
 {
-    DO_COMPATIBILITY(aclnnArgsort, acl_op::argsort(self, dim, descending));
+    return op_api::argsort(self, dimname_to_position(self, dim), descending);
+}
+
+at::Tensor argsort(const at::Tensor &self, bool stable, int64_t dim, bool descending)
+{
     if (self.dtype() == at::kInt || self.dtype() == at::kLong) {
         TORCH_NPU_WARN_ONCE("Warning: kernel [ArgSort] can not support dtype int32 or int64 on AiCore, Now this kernel "
                             "is running on AiCpu."
                             "If you are more concerned about high-performance execution,please cast dtype to float32.");
     }
-    return op_api::argsort(self, dimname_to_position(self, dim), descending);
+    return std::get<1>(at::sort(self, stable, dim, descending));
 }
 } // namespace op_api
--- test/test_base_ops/test_argsort.py
+++ test/test_base_ops/test_argsort.py
@@ -1,11 +1,14 @@
+import unittest
 import torch
 import numpy as np
 
 import torch_npu
 from torch_npu.testing.testcase import TestCase, run_tests
 from torch_npu.testing.common_utils import create_common_tensor
+from torch_npu.testing.common_utils import SupportedDevices
 
 
+@SupportedDevices(['Ascend910B'])
 class TestArgSort(TestCase):
     def cpu_op_exec(self, input1, dim, descending):
         output = torch.argsort(input1, dim=dim, descending=descending)
@@ -13,7 +16,6 @@ class TestArgSort(TestCase):
 
     def npu_op_exec(self, input1, dim, descending):
         output = torch.argsort(input1, dim=dim, descending=descending)
-
         return output.cpu().numpy()
 
     def cpu_default_op_exec(self, input1):
@@ -22,6 +24,14 @@ class TestArgSort(TestCase):
 
     def npu_default_op_exec(self, input1):
         output = torch.argsort(input1)
+        return output.cpu().numpy()
+
+    def cpu_op_exec_stable(self, input1, stable, dim, descending):
+        output = torch.argsort(input1, stable=stable, dim=dim, descending=descending)
+        return output.numpy()
+
+    def npu_op_exec_stable(self, input1, stable, dim, descending):
+        output = torch.argsort(input1, stable=stable, dim=dim, descending=descending)
         return output.cpu().numpy()
 
     def test_sort_shape_format_fp32(self):
@@ -53,13 +63,41 @@ class TestArgSort(TestCase):
         for item in shape_format:
             cpu_input1, npu_input1 = create_common_tensor(item[0], -100, 100)
             if len(item) > 1:
-                cpu_output = self.cpu_op_exec(
-                    cpu_input1.to(torch.float32), item[1], item[2]
-                )
+                cpu_output = self.cpu_op_exec(cpu_input1, item[1], item[2])
                 npu_output = self.npu_op_exec(npu_input1, item[1], item[2])
             else:
-                cpu_output = self.cpu_default_op_exec(cpu_input1.to(torch.float32))
+                cpu_output = self.cpu_default_op_exec(cpu_input1)
                 npu_output = self.npu_default_op_exec(npu_input1)
+            self.assertRtolEqual(cpu_output, npu_output)
+
+    @unittest.skipIf("1.11.0" in torch.__version__,
+                "OP `argsort.stable` is not supported on torch v1.11.0, skip this ut for this torch version")
+    def test_sort_stable_shape_format_fp32(self):
+        shape_format = [
+            [[np.float32, 0, (8, 4, 3, 9)], True, 2, False],
+            [[np.float32, 0, (1, 7)], False, 0, True],
+            [[np.float32, 0, (1, 5, 6)], True, 1, False],
+        ]
+
+        for item in shape_format:
+            cpu_input1, npu_input1 = create_common_tensor(item[0], -100, 100)
+            cpu_output = self.cpu_op_exec_stable(cpu_input1, item[1], item[2], item[3])
+            npu_output = self.npu_op_exec_stable(npu_input1, item[1], item[2], item[3])
+            self.assertRtolEqual(cpu_output, npu_output)
+
+    @unittest.skipIf("1.11.0" in torch.__version__,
+                "OP `argsort.stable` is not supported on torch v1.11.0, skip this ut for this torch version")
+    def test_sort_stable_shape_format_fp16(self):
+        shape_format = [
+            [[np.float16, 0, (8, 4, 3, 9)], True, 2, False],
+            [[np.float16, 0, (1, 7)], False, 0, True],
+            [[np.float16, 0, (1, 5, 6)], True, 1, False],
+        ]
+
+        for item in shape_format:
+            cpu_input1, npu_input1 = create_common_tensor(item[0], -100, 100)
+            cpu_output = self.cpu_op_exec_stable(cpu_input1, item[1], item[2], item[3])
+            npu_output = self.npu_op_exec_stable(npu_input1, item[1], item[2], item[3])
             self.assertRtolEqual(cpu_output, npu_output)
 
 
