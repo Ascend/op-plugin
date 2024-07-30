a78a2759adc6f6898594d4b6078b97dc066fe6bc
zhanghao <zhanghao418@huawei.com>
2024-07-30 16:28:56 +0000

    !1739 add foreach_sign、foreach_copy Merge pull request !1739 from zhanghao/master

---
+++ op_plugin/ops/v2r2/opapi/ForeachCopyKernelOpApi.cpp
@@ -0,0 +1,75 @@
+// Copyright (c) 2024 Huawei Technologies Co., Ltd
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License  (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "op_plugin/AclOpsInterface.h"
+#include "op_plugin/OpApiInterface.h"
+#include "op_plugin/utils/op_api_common.h"
+#include "torch_npu/csrc/framework/utils/UtilForOpAdapter.h"
+#include <ATen/native/ForeachUtils.h>
+#include "op_plugin/utils/custom_functions/opapi/ForeachConstants.h"
+
+namespace op_api {
+using npu_preparation = at_npu::native::OpPreparation;
+using npu_calcu_util = at_npu::native::CalcuOpUtil;
+
+
+void exec_npu_cmd_copy(const at::TensorList dst, at::TensorList src, bool non_blocking)
+{
+    if (non_blocking) {
+        EXEC_NPU_CMD(aclnnForeachCopy, src, dst);
+    } else {
+        EXEC_NPU_CMD_SYNC(aclnnForeachCopy, src, dst);
+    }
+}
+
+void split_and_exec_npu_cmd_copy(const at::TensorList dst, at::TensorList src, bool non_blocking)
+{
+    size_t tensor_count = src.size();
+    size_t max_tensor_count = SINGLE_FOREACH_OP_TENSOR_COUNT;
+    size_t loop_time = tensor_count / max_tensor_count;
+
+    if (tensor_count <= max_tensor_count) {
+        exec_npu_cmd_copy(dst, src, non_blocking);
+        return;
+    }
+    for (size_t i = 0; i < loop_time; i++) {
+        at::TensorList temp_src(src.data() + i * max_tensor_count, max_tensor_count);
+        at::TensorList temp_dst(dst.data() + i * max_tensor_count, max_tensor_count);
+        exec_npu_cmd_copy(temp_dst, temp_src, non_blocking);
+    }
+
+    size_t remaining_count = tensor_count % max_tensor_count;
+    if (remaining_count) {
+        at::TensorList temp_src(src.data() + loop_time * max_tensor_count, remaining_count);
+        at::TensorList temp_dst(dst.data() + loop_time * max_tensor_count, remaining_count);
+        exec_npu_cmd_copy(temp_dst, temp_src, non_blocking);
+    }
+}
+
+void _foreach_copy_(const at::TensorList self, const at::TensorList src, bool non_blocking)
+{
+    DO_COMPATIBILITY(aclnnForeachCopy, at::native::foreach_tensor_copy_list_kernel_slow_(self, src, non_blocking));
+    at::native::check_foreach_api_restrictions(self, src);
+
+    static const bool is_support_nd_out = c10_npu::GetSocVersion() >= c10_npu::SocVersion::Ascend910B1 &&
+                                          c10_npu::GetSocVersion() < c10_npu::SocVersion::Ascend310B1;
+    if (!is_support_nd_out || !at::native::can_use_fast_route(self, src)) {
+        return at::native::foreach_tensor_copy_list_kernel_slow_(self, src, non_blocking);
+    }
+
+    split_and_exec_npu_cmd_copy(self, src, non_blocking);
+}
+
+} // namespace at_npu
---
+++ test/test_v2r1_ops/test_foreach_copy.py
@@ -0,0 +1,96 @@
+import unittest
+import random
+import torch
+import torch_npu
+import hypothesis
+from torch_npu.testing.testcase import TestCase, run_tests
+from torch_npu.testing.common_utils import SupportedDevices
+
+
+class TestForeachCopy(TestCase):
+
+    torch_dtypes = {
+        "float16" : torch.float16,
+        "float32" : torch.float32,
+        "bfloat16" : torch.bfloat16,
+        "int32" : torch.int32
+    }
+
+    def assert_equal(self, cpu_outs, npu_outs):
+        for cpu_out, npu_out in zip(cpu_outs, npu_outs):
+            if (cpu_out.shape != npu_out.shape):
+                self.fail("shape error")
+            if (cpu_out.dtype != npu_out.dtype):
+                self.fail("dtype error!")
+            result = torch.allclose(cpu_out, npu_out.cpu(), rtol=0.001, atol=0.001)
+            if not result:
+                self.fail("result error!")
+        return True
+
+    def create_tensors(self, tensor_nums, dtype):
+        cpu_tensors = []
+        npu_tensors = []
+        for i in range(tensor_nums):
+            if (dtype == "int32"):
+                t = torch.randint(low=-100, high=100, size=(1, 1), dtype=self.torch_dtypes.get(dtype))
+            else:
+                t = torch.randn((1, 1), dtype=self.torch_dtypes.get(dtype))
+            cpu_tensors.append(t)
+            npu_tensors.append(t.npu())
+        return tuple(cpu_tensors), tuple(npu_tensors)
+
+    @SupportedDevices(['Ascend910B'])
+    def test_foreach_copy_float32_shpae_tensor_num(self):
+        tensor_num_list = [20, 50]
+        for tensor_num in tensor_num_list :
+            cpu_tensors_x, npu_tensors_x = self.create_tensors(tensor_num, "float32")
+            cpu_tensors_y, npu_tensors_y = self.create_tensors(tensor_num, "float32")
+            torch._foreach_copy_(cpu_tensors_y, cpu_tensors_x)
+            torch._foreach_copy_(npu_tensors_y, npu_tensors_x)
+
+            self.assertRtolEqual(cpu_tensors_y, npu_tensors_y)
+    
+    @SupportedDevices(['Ascend910B'])
+    def test_foreach_copy_float16_shpae_tensor_num(self):
+        tensor_num_list = [20, 50]
+        for tensor_num in tensor_num_list :
+            cpu_tensors_x, npu_tensors_x = self.create_tensors(tensor_num, "float16")
+            cpu_tensors_y, npu_tensors_y = self.create_tensors(tensor_num, "float16")
+            torch._foreach_copy_(cpu_tensors_y, cpu_tensors_x)
+            torch._foreach_copy_(npu_tensors_y, npu_tensors_x)
+            
+            self.assertRtolEqual(cpu_tensors_y, npu_tensors_y)
+
+    @SupportedDevices(['Ascend910B'])
+    def test_foreach_copy_bfloat16_shpae_tensor_num(self):
+        tensor_num_list = [20, 50]
+        for tensor_num in tensor_num_list :
+            cpu_tensors_x, npu_tensors_x = self.create_tensors(tensor_num, "bfloat16")
+            cpu_tensors_y, npu_tensors_y = self.create_tensors(tensor_num, "bfloat16")
+            torch._foreach_copy_(cpu_tensors_y, cpu_tensors_x)
+            torch._foreach_copy_(npu_tensors_y, npu_tensors_x)
+            
+            self.assert_equal(cpu_tensors_y, npu_tensors_y)
+    
+    @SupportedDevices(['Ascend910B'])
+    def test_foreach_copy_int32_shpae_tensor_num(self):
+        tensor_num_list = [20, 50]
+        for tensor_num in tensor_num_list :
+            cpu_tensors_x, npu_tensors_x = self.create_tensors(tensor_num, "int32")
+            cpu_tensors_y, npu_tensors_y = self.create_tensors(tensor_num, "int32")
+            torch._foreach_copy_(cpu_tensors_y, cpu_tensors_x)
+            torch._foreach_copy_(npu_tensors_y, npu_tensors_x)
+            
+            self.assertRtolEqual(cpu_tensors_y, npu_tensors_y)
+
+    @SupportedDevices(['Ascend910B'])
+    def test_foreach_copy_int32_different_xpu(self):
+        tensor_num_list = [20, 50]
+        for tensor_num in tensor_num_list :
+            cpu_tensors_x, npu_tensors_x = self.create_tensors(tensor_num, "int32")
+            cpu_tensors_y, npu_tensors_y = self.create_tensors(tensor_num, "int32")
+            torch._foreach_copy_(npu_tensors_y, cpu_tensors_x)
+            self.assertRtolEqual(npu_tensors_y, cpu_tensors_x)
+
+if __name__ == "__main__":
+    run_tests()
