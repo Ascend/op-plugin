b36ed8314bb58b7a927c07135b958b40a3a87cbf
zhanghao <zhanghao418@huawei.com>
2024-08-27 08:16:05 +0000

    !1806 新增_upsample_bicubic2d_aa、_upsample_bicubic2d_aa算子文件 Merge pull request !1806 from zhanghao/master

---
+++ op_plugin/ops/opapi/UpsampleBicubic2dAAKernelNpuOpApi.cpp
@@ -0,0 +1,49 @@
+// Copyright (c) 2024 Huawei Technologies Co., Ltd
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License  (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "op_plugin/OpApiInterface.h"
+#include "op_plugin/utils/op_api_common.h"
+
+namespace op_api {
+using npu_preparation = at_npu::native::OpPreparation;
+
+at::Tensor& upsample_bicubic2d_aa_opapi(const at::Tensor& self, at::IntArrayRef output_size, bool align_corners,
+                                        c10::optional<double> scales_h, c10::optional<double> scales_w, at::Tensor& result)
+{
+    double scales_h_value = scales_h.value_or(0);
+    double scales_w_value = scales_w.value_or(0);
+    EXEC_NPU_CMD(aclnnUpsampleBicubic2dAA, self, output_size, align_corners, scales_h_value, scales_w_value, result);
+    return result;
+}
+
+at::Tensor& _upsample_bicubic2d_aa_out(const at::Tensor& self, at::IntArrayRef output_size, bool align_corners,
+                                       c10::optional<double> scales_h, c10::optional<double> scales_w, at::Tensor& result)
+{
+    auto outputsize = op_infer::upsample_bicubic2d_npu_output_size(self, output_size);
+    npu_preparation::check_tensor({self}, result, self, outputsize);
+    upsample_bicubic2d_aa_opapi(self, output_size, align_corners, scales_h, scales_w, result);
+    return result;
+}
+
+at::Tensor _upsample_bicubic2d_aa(const at::Tensor& self, at::IntArrayRef output_size, bool align_corners,
+                                  c10::optional<double> scales_h, c10::optional<double> scales_w)
+{
+    auto outputsize = op_infer::upsample_bicubic2d_npu_output_size(self, output_size);
+    at::Tensor result = npu_preparation::apply_tensor_without_format(self, outputsize);
+    upsample_bicubic2d_aa_opapi(self, output_size, align_corners, scales_h, scales_w, result);
+    return result;
+}
+
+} // namespace op_api
