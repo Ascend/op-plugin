7c164e1346e807547d730a18bb3d20efc3044268
dilililiwhy <why.wuhuanyu@huawei.com>
2024-09-09 03:26:27 +0000

    !1820 TORCH MAIN SYNC : enable_gpa of sdpa Merge pull request !1820 from dilililiwhy/main_sync_240825

--- op_plugin/ops/opapi/ScaledDotProductAttentionKernelNpuOpApi.cpp
+++ op_plugin/ops/opapi/ScaledDotProductAttentionKernelNpuOpApi.cpp
@@ -108,7 +108,9 @@ inline c10::SymFloat calculate_scale(
         : (c10::SymFloat(1.0) / (c10::SymFloat(query.sym_size(-1)).sqrt()));
     return c10::SymFloat(softmax_scale);
 }
+#endif
 
+#if VERSION_BETWEEN(V2R1, V2R4)
 at::Tensor scaled_dot_product_attention(
     const at::Tensor &query,
     const at::Tensor &key,
@@ -198,6 +200,99 @@ at::Tensor scaled_dot_product_attention(
                                                          c10::nullopt, scale);
     return std::get<0>(output);
 }
-
 #endif
+
+#if VERSION_BETWEEN(V2R5, VERSION_NEWEST)
+at::Tensor scaled_dot_product_attention(
+    const at::Tensor &query,
+    const at::Tensor &key,
+    const at::Tensor &value,
+    const c10::optional<at::Tensor> &attn_mask,
+    double dropout_p,
+    bool is_causal,
+    c10::optional<double> scale,
+    bool enable_gqa)
+{
+    validate_sdpa_input(query, key, value, attn_mask, dropout_p, is_causal, scale);
+    if (query.requires_grad() && key.requires_grad() && value.requires_grad() &&
+        (query.scalar_type() == at::kHalf || query.scalar_type() == at::kBFloat16 || query.scalar_type() == at::kFloat) &&
+        ((attn_mask.has_value() && attn_mask->dtype() == at::kBool) || !attn_mask.has_value()) &&
+        query.dim() == BNSD_DIM && key.dim() == BNSD_DIM && value.dim() == BNSD_DIM &&
+        query.size(1) <= N_LIMIT && query.size(3) <= D_LIMIT && key.size(1) <= N_LIMIT &&
+        query.size(1) % key.size(1) == 0 && query.size(1) / key.size(1) > 0 &&
+        c10_npu::GetSocVersion() >= c10_npu::SocVersion::Ascend910B1) {
+        /* The implementation of the NPU FlashAttention fusion operator constraints:
+           1. The attn_mask supports only the bool data type.
+           2. The shape [B, N1, S1, D] of the query is suppported, where N1 <= N_LIMIT,
+              D <= D_LIMIT and dim == BNSD_DIM.
+           3. For GQA, the key shape is [B, N2, S2, D], where N2 <= N_LIMIT, and N1 is a positive integer
+              multiple of N2.
+           4. It only supports SocVersion after Ascend910B1. */
+        c10::optional<at::Tensor> atten_mask = convert_boolean_attn_mask(query, attn_mask, is_causal);
+        int64_t head_num = query.size(1);
+        c10::string_view input_layout = "BNSD";
+        auto input_scale = calculate_scale(query, scale);
+        double keep_prob = 1 - dropout_p;
+        int64_t next_tockens = is_causal ? 0 : TOKEN_MAX;
+        int64_t sparse_mode = is_causal ? LEFT_UP_CAUSAL : 0;
+        c10::optional<at::Tensor> nulltensor = c10::nullopt;
+        c10::OptionalIntArrayRef nulllen = c10::nullopt;
+        auto output =
+            at_npu::native::custom_ops::npu_fusion_attention(query, key, value, head_num, input_layout, nulltensor,
+                                                             nulltensor, atten_mask, input_scale.as_float_unchecked(),
+                                                             keep_prob, TOKEN_MAX, next_tockens, 0, nulllen,
+                                                             nulllen, nulllen, sparse_mode, true, false);
+        return std::get<0>(output);
+    } else if ((!query.requires_grad() && !key.requires_grad() && !value.requires_grad()) &&
+                (query.dim() == BNSD_DIM && key.dim() == BNSD_DIM && value.dim() == BNSD_DIM) &&
+                (query.size(0) != 0 && query.size(1) != 0 && query.size(2) != 0 && query.size(3) != 0) &&
+                (query.scalar_type() == at::kHalf || query.scalar_type() == at::kBFloat16) && query.size(3) % 16 ==0 &&
+                ((attn_mask.has_value() && attn_mask->dtype() == at::kBool && attn_mask->size(-2) == query.size(2) &&
+                attn_mask->size(-1) == key.size(2)) || !attn_mask.has_value()) &&
+                (query.size(0) == key.size(0) && query.size(3) == key.size(3) && key.size(0) == value.size(0) &&
+                key.size(1) == value.size(1) && key.size(2) == value.size(2) && key.size(3) == value.size(3)) &&
+                (query.size(0) <= B_LIMIT && query.size(1) <= FIA_N_LIMIT && query.size(3) <= D_LIMIT && key.size(1) <= FIA_N_LIMIT) &&
+                (key.size(1) != 0 && query.size(1) % key.size(1) == 0 && query.size(1) / key.size(1) <= 64) &&
+                c10_npu::GetSocVersion() >= c10_npu::SocVersion::Ascend910B1) {
+                /* The implementation of the NPU Fused Infer Attention operator constraints:
+                1. It only supports the type of fp16 or bf16, and dim = BNSD_DIM
+                2. The shape [B, N1, S1, D] of the query is suppported, where B <= B_LIMIT, N1 <= FIA_N_LIMIT,
+                    D <= D_LIMIT and must be a positive integer multiple of 16.
+                3. The shape of the key and value should be the same, and dim = BNSD_DIM. For GQA, the key shape is [B, N2, S2, D],
+                    where 0 < N2 <= FIA_N_LIMIT, N1 is a positive integer multiple of N2 and N1/N2 <= 64.
+                4. The attn_mask supports only the bool data type, and shpae[-2] = S1, shpae[-1] = S2.
+                5. It only supports SocVersion after Ascend910B1. */
+                int64_t inner_precise = 0;
+                if (query.size(2) == 1) {
+                    is_causal = false;
+                } else {
+                    if (!is_causal) {
+                        inner_precise = 2;
+                    }
+                }
+                c10::optional<at::Tensor> atten_mask = convert_boolean_attn_mask(query, attn_mask, is_causal);
+                int64_t head_num = query.size(1);
+                int64_t head_num_kv = key.size(1);
+                c10::string_view input_layout = "BNSD";
+                auto input_scale = calculate_scale(query, scale);
+                int64_t next_tockens = is_causal ? 0 : TOKEN_MAX;
+                int64_t sparse_mode = is_causal ? LEFT_UP_CAUSAL : 0;
+                c10::optional<at::Tensor> nulltensor = c10::nullopt;
+                at::OptionalSymIntArrayRef nulllen = c10::nullopt;
+                auto output =
+                    at_npu::native::custom_ops::npu_fused_infer_attention_score(query, key, value, nulltensor, atten_mask, nulllen, nulllen, nulltensor,
+                                                                                nulltensor, nulltensor, nulltensor, nulltensor, nulltensor, nulltensor,
+                                                                                nulltensor, nulltensor, nulltensor, nulltensor, nulltensor, nulltensor,
+                                                                                nulltensor, nulltensor, nulltensor, nulllen, head_num, input_scale.as_float_unchecked(),
+                                                                                TOKEN_MAX, next_tockens, input_layout, head_num_kv, sparse_mode, inner_precise, 0, 0, 0, 0, false);
+
+                return std::get<0>(output);
+    }
+    c10::optional<at::Tensor> atten_mask_math = convert_boolean_attn_mask_math(attn_mask, query.dtype());
+    auto output = at::_scaled_dot_product_attention_math(query, key, value, atten_mask_math, dropout_p, is_causal,
+                                                         c10::nullopt, scale);
+    return std::get<0>(output);
+}
+#endif
+
 }  // namespace op_api
