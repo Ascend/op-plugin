4872af493dfc334afd1ed3282ecb2efa1e082ac1
詹昊 <zhanhao9@huawei.com>
2024-09-09 13:28:17 +0000

    !1833 add opapi of adaptive_avg_pool3d Merge pull request !1833 from 詹昊/master

--- op_plugin/ops/aclops/AdaptiveAvgPool3dKernelNpu.cpp
+++ op_plugin/ops/aclops/AdaptiveAvgPool3dKernelNpu.cpp
@@ -74,17 +74,13 @@ at::Tensor& adaptive_avg_pool3d_out(
   return result;
 }
 
-at::Tensor adaptive_avg_pool3d(const at::Tensor& self, at::IntArrayRef output_size) {
-  adaptive_avg_pooling3d_check(self);
-  auto op_infer_output_size = op_infer::adaptive_avg_pool3d_npu_output_size(self, output_size);
-  at::Tensor result = npu_preparation::apply_tensor(self, op_infer_output_size);
-
-  TORCH_CHECK(output_size[0] == 1 && output_size[1] == 1 && output_size[2] == 1,
-      "adaptive_avg_pool3d only support D=1 && H=1 && W=1 current!" + OPS_ERROR(ErrCode::PARAM));
-  return at::mean(self, {self.dim() - 3, self.dim() - 2, self.dim() - 1}, true);
-}
-
 at::Tensor _adaptive_avg_pool3d(const at::Tensor& self, at::IntArrayRef output_size) {
-  return acl_op::adaptive_avg_pool3d(self, output_size);
+    adaptive_avg_pooling3d_check(self);
+    auto op_infer_output_size = op_infer::adaptive_avg_pool3d_npu_output_size(self, output_size);
+    at::Tensor result = npu_preparation::apply_tensor(self, op_infer_output_size);
+
+    TORCH_CHECK(output_size[0] == 1 && output_size[1] == 1 && output_size[2] == 1,
+        "adaptive_avg_pool3d only support D=1 && H=1 && W=1 current!" + OPS_ERROR(ErrCode::PARAM));
+    return at::mean(self, {self.dim() - 3, self.dim() - 2, self.dim() - 1}, true);
 }
 } // namespace acl_op
