4fa695e514e1e36977feceb1da121c8edb5f6e9d
kevin_huang <kevin.huangdi@huawei.com>
2024-09-10 02:18:19 +0000

    !1830 [PTA] add the MoeFinalizeRoutingV2 PTA Merge pull request !1830 from kevin_huang/master

---
+++ op_plugin/ops/opapi/MoeFinalizeRoutingV2KernelNpuOpApi.cpp
@@ -0,0 +1,51 @@
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <torch/csrc/autograd/custom_function.h>
+#include "op_plugin/OpApiInterface.h"
+#include "op_plugin/utils/op_api_common.h"
+
+namespace op_api {
+using npu_preparation = at_npu::native::OpPreparation;
+
+at::Tensor npu_moe_finalize_routing(const at::Tensor& expanded_permuted_rows, const c10::optional<at::Tensor>& skip1,
+                                    const c10::optional<at::Tensor>& skip2,
+                                    const c10::optional<at::Tensor>& bias,
+                                    const c10::optional<at::Tensor>& scales,
+                                    const at::Tensor& expanded_src_to_dst_row,
+                                    const c10::optional<at::Tensor>& expert_for_source_row,
+                                    const c10::optional<int64_t> drop_pad_mode)
+{
+    const auto getWorkspaceSizeFuncAddr = GetOpApiFuncAddr("aclnnMoeFinalizeRoutingV2GetWorkspaceSize");
+    const auto opApiFuncAddr = GetOpApiFuncAddr("aclnnMoeFinalizeRoutingV2");
+    if (getWorkspaceSizeFuncAddr == nullptr || opApiFuncAddr == nullptr) {
+        TORCH_CHECK(skip1.has_value(), "skip1 parameter must have value when there is no aclnnMoeFinalizeRoutingV2",
+            OPS_ERROR(ErrCode::PARAM));
+        at::Tensor result = npu_preparation::apply_tensor_without_format(skip1.value());
+        EXEC_NPU_CMD(aclnnMoeFinalizeRouting, expanded_permuted_rows, skip1, skip2, bias, scales,
+            expanded_src_to_dst_row, expert_for_source_row, result);
+        return result;
+    }
+
+    at::Tensor result;
+    if (!scales.has_value()) {
+        result = npu_preparation::apply_tensor_without_format(expanded_permuted_rows);
+    } else {
+        at::SmallVector<int64_t, op_infer::SIZE> output_size;
+        output_size.push_back(scales.value().size(0));
+        output_size.push_back(expanded_permuted_rows.size(1));
+        result = npu_preparation::apply_tensor_without_format(output_size, expanded_permuted_rows.options());
+    }
+    int64_t mode = c10::value_or_else(drop_pad_mode, [] { return 0; });
+    EXEC_NPU_CMD(aclnnMoeFinalizeRoutingV2, expanded_permuted_rows, expanded_src_to_dst_row,
+                 skip1, skip2, bias, scales, expert_for_source_row, mode, result);
+    return result;
+}
+} // namespace op_api
--- test/test_custom_ops/test_npu_moe_finalize_routing_v2.py
+++ test/test_custom_ops/test_npu_moe_finalize_routing_v2.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from copy import deepcopy
 import numpy as np
 import torch
 import torch_npu
@@ -15,22 +16,35 @@ class MoeFinalizeRoutingData:
     scales: np.array
     expanded_src_to_dst_row: np.array
     expert_for_source_row: np.array
+    drop_pad_mode: np.int64
 
 
 class TestMoeFinalizeRouting(TestCase):
    
     def moe_finalize_routing_np(self, data_struct):
-        out = data_struct.skip1
-        if data_struct.skip2_optional is not None:
-            out = data_struct.skip1 + data_struct.skip2_optional
-        num_rows = data_struct.skip1.shape[0]
+        num_rows = data_struct.expert_for_source_row.shape[0]
         K = data_struct.expanded_src_to_dst_row.shape[0] // num_rows
-
+        H = data_struct.expanded_permuted_rows.shape[1]
+        if (data_struct.skip1 is not None) and (data_struct.skip2_optional is not None):
+            out = data_struct.skip1 + data_struct.skip2_optional
+        elif (data_struct.skip2_optional is not None) and (data_struct.skip1 is None):
+            out = deepcopy(data_struct.skip2_optional)
+        elif (data_struct.skip2_optional is None) and (data_struct.skip1 is not None):
+            out = deepcopy(data_struct.skip1)
+        else:
+            out = np.zeros([num_rows, H])
         for i in range(num_rows):
             for k in range(K):
-                dst_row = data_struct.expanded_permuted_rows[data_struct.expanded_src_to_dst_row[k * num_rows + i], :]
+                value = data_struct.expanded_src_to_dst_row[k * num_rows + i]
+                if value == -1:
+                    dst_row = 0
+                else:
+                    dst_row = data_struct.expanded_permuted_rows[value, :]
                 expert_id = data_struct.expert_for_source_row[i, k]
-                out[i, :] += data_struct.scales[i, k] * (dst_row + data_struct.bias[expert_id, :])
+                if data_struct.bias is not None:
+                    out[i, :] += data_struct.scales[i, k] * (dst_row + data_struct.bias[expert_id, :])
+                else:
+                    out[i, :] += data_struct.scales[i, k] * dst_row
         return out
    
     def custom_op_exec(self, data_struct):
@@ -40,7 +54,8 @@ class TestMoeFinalizeRouting(TestCase):
                                                   torch.tensor(data_struct.bias).npu(),
                                                   torch.tensor(data_struct.scales).npu(),
                                                   torch.tensor(data_struct.expanded_src_to_dst_row).npu(),
-                                                  torch.tensor(data_struct.expert_for_source_row).reshape(5, 4).npu())
+                                                  torch.tensor(data_struct.expert_for_source_row).reshape(5, 4).npu(),
+                                                  0)
     
     def generate_input_data(self, expert_num=16, token_len=10, top_k=4, num_rows=50):
         expanded_permuted_rows = np.random.randn(num_rows * top_k, token_len).astype(np.float32)
@@ -52,7 +67,7 @@ class TestMoeFinalizeRouting(TestCase):
         np.random.shuffle(expanded_src_to_dst_row)
         expert_for_source_row = np.random.randint(low=0, high=expert_num, size=(num_rows, top_k)).astype(np.int32)
         data_struct = MoeFinalizeRoutingData(expanded_permuted_rows, skip1, skip2_optional, bias, scales,
-                                             expanded_src_to_dst_row, expert_for_source_row)
+                                             expanded_src_to_dst_row, expert_for_source_row, 0)
         return data_struct
     
     @SupportedDevices(['Ascend910B'])
