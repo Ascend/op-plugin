ceb73d1bc018ffc4f24fbe969f2edef95f2a4c57
zzx <zhaozhaoxiang@huawei.com>
2024-10-11 09:18:12 +0000

    !1909 add exponential ut Merge pull request !1909 from zzx/master

--- test/test_custom_ops/test_npu_exponential.py
+++ test/test_custom_ops/test_npu_exponential.py
@@ -45,6 +45,12 @@ class TestExponential(TestCase):
             return True
         else:
             return False
+    
+    @SupportedDevices(['Ascend910B'])
+    def test_exponential_no_zero(self):
+        for _ in range(100):
+            x = torch.empty(500000000, device="npu", dtype=torch.float32).exponential_()
+            self.assertTrue(x.min() > 0)
 
 
 if __name__ == "__main__":
