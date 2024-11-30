b907b5e5e8564c5cdec8e55f2b9910369aead6f5
Tangmenhao <tangmenhao2@huawei.com>
2024-11-30 03:25:41 +0000

    !2010 动态量化算子添加支持MOE和int4 Merge pull request !2010 from Tangmenhao/6.0.0_1125

--- op_plugin/ops/opapi/DynamicQuantKernelNpuOpApi.cpp
+++ op_plugin/ops/opapi/DynamicQuantKernelNpuOpApi.cpp
@@ -19,8 +19,18 @@
 
 namespace op_api {
 using npu_preparation = at_npu::native::OpPreparation;
-std::tuple<at::Tensor, at::Tensor> npu_dynamic_quant(const at::Tensor &input, const c10::optional<at::Tensor> &smooth_scales)
+const int64_t INT4_IN_INT32_NUM = 8;
+
+std::tuple<at::Tensor, at::Tensor> npu_dynamic_quant_v0(
+    const at::Tensor &input,
+    const c10::optional<at::Tensor> &smooth_scales,
+    const c10::optional<at::Tensor> &group_index,
+    c10::optional<at::ScalarType> dst_dtype)
 {
+    TORCH_CHECK(dst_dtype != at::ScalarType::QUInt4x2,
+                "please update your CANN to support int4 quantization" + OPS_ERROR(ErrCode::NOT_SUPPORT));
+    TORCH_CHECK(!group_index.has_value(),
+                "please update your CANN to support MOE quantization" + OPS_ERROR(ErrCode::NOT_SUPPORT));
     at::SmallVector<int64_t, op_infer::SIZE> scale_size;
     int scale_dim = input.dim() - 1;
     for (int i = 0; i < scale_dim; ++i) {
@@ -34,18 +44,70 @@ std::tuple<at::Tensor, at::Tensor> npu_dynamic_quant(const at::Tensor &input, co
     return std::make_tuple(output, scale);
 }
 
-std::tuple<at::Tensor, at::Tensor, at::Tensor> npu_dynamic_quant_asymmetric(const at::Tensor &input, const c10::optional<at::Tensor> &smooth_scales, const c10::optional<at::Tensor> &group_index, c10::optional<at::ScalarType> dst_dtype)
+std::tuple<at::Tensor, at::Tensor> npu_dynamic_quant(
+    const at::Tensor &input,
+    const c10::optional<at::Tensor> &smooth_scales,
+    const c10::optional<at::Tensor> &group_index,
+    c10::optional<at::ScalarType> dst_dtype)
 {
+    TORCH_CHECK(!dst_dtype.has_value() || dst_dtype == at::ScalarType::Char || dst_dtype == at::ScalarType::QUInt4x2,
+                "dtype must be torch.int8 for int8 or torch.quint4x2 for int4" + OPS_ERROR(ErrCode::TYPE));
+    DO_COMPATIBILITY(aclnnDynamicQuantV2, npu_dynamic_quant_v0(input, smooth_scales, group_index, dst_dtype));
     at::SmallVector<int64_t, op_infer::SIZE> scale_size;
     int scale_dim = input.dim() - 1;
-    for (int i = 0; i < scale_dim; ++i) {
-        scale_size.push_back(input.size(i));
+    int index = 0;
+    for (; index < scale_dim; ++index) {
+        scale_size.push_back(input.size(index));
+    }
+    at::Tensor scale = npu_preparation::apply_tensor_without_format(scale_size, c10::dtype(c10::ScalarType::Float));
+
+    int output_type;
+    at::Tensor output;
+    if (dst_dtype == at::ScalarType::QUInt4x2) { // INT4
+        TORCH_CHECK(input.size(index) % INT4_IN_INT32_NUM == 0,
+                    "input shape last dim must be divded by 8 when int4 quantization" + OPS_ERROR(ErrCode::PARAM));
+        output_type = ge::DataType::DT_INT32;
+        scale_size.push_back(input.size(index) / INT4_IN_INT32_NUM);
+        output = npu_preparation::apply_tensor_without_format(scale_size, c10::dtype(c10::ScalarType::Int));
+    } else { // 默认INT8
+        output_type = ge::DataType::DT_INT8;
+        output = npu_preparation::apply_tensor_without_format(input.sizes(), c10::dtype(c10::ScalarType::Char));
+    }
+    c10::optional<at::Tensor> offset;
+
+    EXEC_NPU_CMD(aclnnDynamicQuantV2, input, smooth_scales, group_index, output_type, output, scale, offset);
+    return std::make_tuple(output, scale);
+}
+
+std::tuple<at::Tensor, at::Tensor, at::Tensor> npu_dynamic_quant_asymmetric(
+    const at::Tensor &input,
+    const c10::optional<at::Tensor> &smooth_scales,
+    const c10::optional<at::Tensor> &group_index,
+    c10::optional<at::ScalarType> dst_dtype)
+{
+    TORCH_CHECK(!dst_dtype.has_value() || dst_dtype == at::ScalarType::Char || dst_dtype == at::ScalarType::QUInt4x2,
+                "dtype must be torch.int8 for int8 or torch.quint4x2 for int4 when int4 quantization" + OPS_ERROR(ErrCode::TYPE));
+    at::SmallVector<int64_t, op_infer::SIZE> scale_size;
+    int scale_dim = input.dim() - 1;
+    int index = 0;
+    for (; index < scale_dim; ++index) {
+        scale_size.push_back(input.size(index));
     }
 
     at::Tensor scale = npu_preparation::apply_tensor_without_format(scale_size, c10::dtype(c10::ScalarType::Float));
     at::Tensor offset = npu_preparation::apply_tensor_without_format(scale_size, c10::dtype(c10::ScalarType::Float));
-    int output_type = ge::DataType::DT_INT8; // 当前仅支持INT8
-    at::Tensor output = npu_preparation::apply_tensor_without_format(input.sizes(), c10::dtype(c10::ScalarType::Char));
+    int output_type;
+    at::Tensor output;
+    if (dst_dtype == at::ScalarType::QUInt4x2) { // INT4
+        TORCH_CHECK(input.size(index) % INT4_IN_INT32_NUM == 0,
+                    "input shape last dim must be divded by 8" + OPS_ERROR(ErrCode::PARAM));
+        output_type = ge::DataType::DT_INT32;
+        scale_size.push_back(input.size(index) / INT4_IN_INT32_NUM);
+        output = npu_preparation::apply_tensor_without_format(scale_size, c10::dtype(c10::ScalarType::Int));
+    } else { // 默认INT8
+        output_type = ge::DataType::DT_INT8;
+        output = npu_preparation::apply_tensor_without_format(input.sizes(), c10::dtype(c10::ScalarType::Char));
+    }
 
     EXEC_NPU_CMD(aclnnDynamicQuantV2, input, smooth_scales, group_index, output_type, output, scale, offset);
     return std::make_tuple(output, scale, offset);
