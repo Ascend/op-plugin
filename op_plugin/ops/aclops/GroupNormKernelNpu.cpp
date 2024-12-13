88036e7dc1798b698df8e0a782026fc078ac095c
汪鑫巍 <wangxinwei21@huawei.com>
2024-12-13 11:11:46 +0000

    !2052 native_group_norm fix coredump Merge pull request !2052 from 汪鑫巍/gns_fix_core

--- op_plugin/ops/aclops/GroupNormKernelNpu.cpp
+++ op_plugin/ops/aclops/GroupNormKernelNpu.cpp
@@ -22,14 +22,9 @@ namespace acl_op {
 using npu_preparation = at_npu::native::OpPreparation;
 
 namespace {
-inline bool is_support_group_norm_swish(const at::Tensor& X)
+inline bool is_support_group_norm_swish()
 {
-    // GroupNorm supports format NC1HWC0
-    if (npu_preparation::get_tensor_npu_format(X) == ACL_FORMAT_NC1HWC0) {
-        return false;
-    }
-
-    if ((c10_npu::GetSocVersion() >= c10_npu::SocVersion::Ascend310P1 &&
+    if ((c10_npu::GetSocVersion() >= c10_npu::SocVersion::Ascend910B1 &&
          c10_npu::GetSocVersion() < c10_npu::SocVersion::Ascend310B1) ||
         (c10_npu::GetSocVersion() > c10_npu::SocVersion::Ascend310B4)) {
         return true;
@@ -44,12 +39,17 @@ std::tuple<at::Tensor, at::Tensor, at::Tensor> native_group_norm_out_npu(
     at::Tensor& variance,
     at::Tensor& rstd,
     const at::Tensor& X,
-    const at::Tensor& gamma,
-    const at::Tensor& beta,
+    const c10::optional<at::Tensor>& gamma_opt,
+    const c10::optional<at::Tensor>& beta_opt,
     int64_t num_groups,
     double eps,
     int64_t C)
 {
+    const at::Tensor& gamma_ = c10::value_or_else(gamma_opt, [] {return at::Tensor();});
+    at::Tensor gamma = gamma_.defined() ? gamma_ : at::ones({C}, X.options());
+    const at::Tensor& beta_ = c10::value_or_else(beta_opt, [] {return at::Tensor();});
+    at::Tensor beta = beta_.defined() ? beta_ : at::zeros({C}, X.options());
+
     at_npu::native::OpCommand cmd;
     cmd.Name("GroupNorm")
         .Input(X)
@@ -73,28 +73,33 @@ std::tuple<at::Tensor, at::Tensor, at::Tensor> native_group_norm_swish_out_npu(
     at::Tensor& variance,
     at::Tensor& rstd,
     const at::Tensor& X,
-    const at::Tensor& gamma,
-    const at::Tensor& beta,
+    const c10::optional<at::Tensor>& gamma_opt,
+    const c10::optional<at::Tensor>& beta_opt,
     int64_t num_groups,
     double eps,
     int64_t C)
 {
-    at_npu::native::OpCommand cmd;
-    if (is_support_group_norm_swish(X)) {
-        cmd.Name("GroupNormSwish")
-            .Input(X)
-            .Input(gamma)
-            .Input(beta)
-            .Output(y)
-            .Output(mean)
-            .Output(rstd)
-            .Attr("num_groups", num_groups)
-            .Attr("eps", static_cast<float>(eps))
-            .Attr("activate_swish", false)
-            .Run();
-        return std::make_tuple(y, mean, rstd);
+    if (!is_support_group_norm_swish()) {
+        return native_group_norm_out_npu(y, mean, variance, rstd, X, gamma_opt, beta_opt, num_groups, eps, C);
     }
-    return native_group_norm_out_npu(y, mean, variance, rstd, X, gamma, beta, num_groups, eps, C);
+    const at::Tensor& gamma_ = c10::value_or_else(gamma_opt, [] {return at::Tensor();});
+    at::Tensor gamma = gamma_.defined() ? gamma_ : at::ones({C}, X.options());
+    const at::Tensor& beta_ = c10::value_or_else(beta_opt, [] {return at::Tensor();});
+    at::Tensor beta = beta_.defined() ? beta_ : at::zeros({C}, X.options());
+
+    at_npu::native::OpCommand cmd;
+    cmd.Name("GroupNormSwish")
+        .Input(X)
+        .Input(gamma)
+        .Input(beta)
+        .Output(y)
+        .Output(mean)
+        .Output(rstd)
+        .Attr("num_groups", num_groups)
+        .Attr("eps", static_cast<float>(eps))
+        .Attr("activate_swish", false)
+        .Run();
+    return std::make_tuple(y, mean, rstd);
 }
 }
 
@@ -112,13 +117,9 @@ std::tuple<at::Tensor, at::Tensor, at::Tensor> native_group_norm(
     at::Tensor mean = npu_preparation::apply_tensor_with_format({N, group}, X.options(), ACL_FORMAT_ND);
     at::Tensor variance = npu_preparation::apply_tensor_with_format({N, group}, X.options(), ACL_FORMAT_ND);
     at::Tensor rstd = npu_preparation::apply_tensor_with_format({N, group}, X.options(), ACL_FORMAT_ND);
-    const at::Tensor& gamma_ = c10::value_or_else(gamma_opt, [] {return at::Tensor();});
-    at::Tensor gamma = gamma_.defined() ? gamma_ : at::ones({C}, X.options());
-    const at::Tensor& beta_ = c10::value_or_else(beta_opt, [] {return at::Tensor();});
-    at::Tensor beta = beta_.defined() ? beta_ : at::zeros({C}, X.options());
     // DO_COMPATIBILITY is used to determine whether GroupNormSwish exists
-    DO_COMPATIBILITY(aclnnGroupNormSwish, acl_op::native_group_norm_out_npu(result, mean, variance, rstd, X, gamma,
-                                                                            beta, group, eps, C));
-    return native_group_norm_swish_out_npu(result, mean, variance, rstd, X, gamma, beta, group, eps, C);
+    DO_COMPATIBILITY(aclnnGroupNormSwish, acl_op::native_group_norm_out_npu(result, mean, variance, rstd, X, gamma_opt,
+                                                                            beta_opt, group, eps, C));
+    return native_group_norm_swish_out_npu(result, mean, variance, rstd, X, gamma_opt, beta_opt, group, eps, C);
 }
 } // namespace acl_op
