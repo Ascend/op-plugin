872222a5b5722a4575e309cce5b5d80088f9271f
zhanghao <zhanghao418@huawei.com>
2025-02-18 08:26:58 +0000

    !2143 upsampleLinear1dBackward算子修改scale传参 Merge pull request !2143 from zhanghao/master

--- op_plugin/ops/opapi/UpsampleLinear1dBackwardKernelNpuOpApi.cpp
+++ op_plugin/ops/opapi/UpsampleLinear1dBackwardKernelNpuOpApi.cpp
@@ -26,15 +26,15 @@ at::Tensor upsample_linear1d_backward(
     at::IntArrayRef input_size,
     bool align_corners,
     c10::optional<double> scales) {
-  DO_COMPATIBILITY(aclnnUpsampleLinear1dBackward,
-                   acl_op::upsample_linear1d_backward(grad_output, output_size, input_size, align_corners, scales));
-  double scales_attr = scales.value_or(1);
+    DO_COMPATIBILITY(aclnnUpsampleLinear1dBackward,
+                     acl_op::upsample_linear1d_backward(grad_output, output_size, input_size, align_corners, scales));
+    double scales_attr = scales.value_or(0);
 
-  at::Tensor grad_input = npu_preparation::apply_tensor(grad_output, input_size);
+    at::Tensor grad_input = npu_preparation::apply_tensor(grad_output, input_size);
 
-  EXEC_NPU_CMD(aclnnUpsampleLinear1dBackward, grad_output, output_size, input_size, align_corners,
-               scales_attr, grad_input);
-  return grad_input;
+    EXEC_NPU_CMD(aclnnUpsampleLinear1dBackward, grad_output, output_size, input_size, align_corners,
+                 scales_attr, grad_input);
+    return grad_input;
 }
 
 #if VERSION_BETWEEN(V1R11, V1R11)
@@ -44,19 +44,19 @@ at::Tensor upsample_linear1d_backward(
     at::IntArrayRef input_size,
     bool align_corners,
     c10::optional<at::ArrayRef<double>> scale_factors) {
-  DO_COMPATIBILITY(aclnnUpsampleLinear1dBackward,
-                   acl_op::upsample_linear1d_backward(grad_output, output_size, input_size,
-                                                      align_corners, scale_factors));
-  auto osize = op_infer::upsample_infershape_with_scale(input_size, output_size, scale_factors);
-  auto outputsize = at::IntArrayRef(osize);
-  auto scales_l = op_plugin::utils::get_scale_value(scale_factors, 0);
-  double scales_l_attr = scales_l.value_or(1);
+    DO_COMPATIBILITY(aclnnUpsampleLinear1dBackward,
+                     acl_op::upsample_linear1d_backward(grad_output, output_size, input_size,
+                                                        align_corners, scale_factors));
+    auto osize = op_infer::upsample_infershape_with_scale(input_size, output_size, scale_factors);
+    auto outputsize = at::IntArrayRef(osize);
+    auto scales_l = op_plugin::utils::get_scale_value(scale_factors, 0);
+    double scales_l_attr = scales_l.value_or(0);
 
-  at::Tensor grad_input = npu_preparation::apply_tensor(grad_output, input_size);
+    at::Tensor grad_input = npu_preparation::apply_tensor(grad_output, input_size);
 
-  EXEC_NPU_CMD(aclnnUpsampleLinear1dBackward, grad_output, outputsize, input_size, align_corners,
-               scales_l_attr, grad_input);
-  return grad_input;
+    EXEC_NPU_CMD(aclnnUpsampleLinear1dBackward, grad_output, outputsize, input_size, align_corners,
+                 scales_l_attr, grad_input);
+    return grad_input;
 }
 #endif
 }
