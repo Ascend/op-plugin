50b5372416beb16d0018cab571101479df68b926
dilililiwhy <why.wuhuanyu@huawei.com>
2025-02-18 07:50:56 +0000

    !2149 TORCH MAIN SYNC : remove use_const_ref_for_mutable_tensors Merge pull request !2149 from dilililiwhy/main_sync_20250125

--- op_plugin/ops/aclops/ConvDepthWise2dKernelNpu.cpp
+++ op_plugin/ops/aclops/ConvDepthWise2dKernelNpu.cpp
@@ -19,6 +19,7 @@
 namespace acl_op {
 using npu_preparation = at_npu::native::OpPreparation;
 
+#if VERSION_BETWEEN(V2R1, V2R6)
 const at::Tensor &_conv_depthwise2d_out(const at::Tensor &self, const at::Tensor &weight, c10::IntArrayRef kernel_size,
                                         const c10::optional<at::Tensor> &bias_opt, c10::IntArrayRef stride,
                                         c10::IntArrayRef padding, c10::IntArrayRef dilation, const at::Tensor &result)
@@ -53,6 +54,44 @@ const at::Tensor &_conv_depthwise2d_out(const at::Tensor &self, const at::Tensor
         .Run();
     return result;
 }
+#endif
+
+#if VERSION_BETWEEN(V2R7, VERSION_NEWEST)
+at::Tensor &_conv_depthwise2d_out(const at::Tensor &self, const at::Tensor &weight, c10::IntArrayRef kernel_size,
+                                  const c10::optional<at::Tensor> &bias_opt, c10::IntArrayRef stride,
+                                  c10::IntArrayRef padding, c10::IntArrayRef dilation, at::Tensor &result)
+{
+    TORCH_CHECK(weight.dim() >= 4, "weight has to be more than 4D, but got Tensor of dimension ", weight.dim(),
+        OPS_ERROR(ErrCode::PARAM));
+    TORCH_CHECK(stride.size() >= 2, "stride has to contain more than 2 elements, but got ", stride.size(),
+        OPS_ERROR(ErrCode::PARAM));
+    TORCH_CHECK(padding.size() >= 2, "padding has to contain more than 2 elements, but got ", padding.size(),
+        OPS_ERROR(ErrCode::PARAM));
+    TORCH_CHECK(dilation.size() >= 2, "dilation has to contain more than 2 elements, but got ", dilation.size(),
+        OPS_ERROR(ErrCode::PARAM));
+
+    const at::Tensor &bias = c10::value_or_else(bias_opt, [] { return at::Tensor(); });
+    const at::Tensor &weight_modify = weight.permute({1, 0, 2, 3});
+
+    c10::SmallVector<int64_t, N> strides_size = {1, 1, stride[0], stride[1]};
+    c10::SmallVector<int64_t, N> paddings = {padding[0], padding[0], padding[1], padding[1]};
+    c10::SmallVector<int64_t, N> dilations = {1, 1, dilation[0], dilation[1]};
+    at::Tensor temp_out = result;
+
+    at_npu::native::OpCommand cmd;
+    cmd.Name("DepthwiseConv2D").Input(self, "x").Input(weight_modify, "filter");
+    if (bias.defined()) {
+        cmd.Input(bias);
+    }
+    cmd.Output(temp_out, "y")
+        .Attr("strides", strides_size)
+        .Attr("pads", paddings)
+        .Attr("dilations", dilations)
+        .Attr("data_format", (string) "NCHW")
+        .Run();
+    return result;
+}
+#endif
 
 at::Tensor _conv_depthwise2d(const at::Tensor &self, const at::Tensor &weight, c10::IntArrayRef kernel_size,
                              const c10::optional<at::Tensor> &bias_opt, c10::IntArrayRef stride,
--- op_plugin/ops/opapi/ConvDepthwise2dKernelNpuOpApi.cpp
+++ op_plugin/ops/opapi/ConvDepthwise2dKernelNpuOpApi.cpp
@@ -20,6 +20,7 @@
 namespace op_api {
 using npu_preparation = at_npu::native::OpPreparation;
 
+#if VERSION_BETWEEN(V2R1, V2R6)
 const at::Tensor& _conv_depthwise2d_out(
     const at::Tensor& self,
     const at::Tensor& weight,
@@ -43,6 +44,34 @@ const at::Tensor& _conv_depthwise2d_out(
     EXEC_NPU_CMD(aclnnConvDepthwise2d, self, weight, kernel_size, bias, stride, padding, dilation, out, cube_math_type);
     return out;
 }
+#endif
+
+#if VERSION_BETWEEN(V2R7, VERSION_NEWEST)
+at::Tensor& _conv_depthwise2d_out(
+    const at::Tensor& self,
+    const at::Tensor& weight,
+    at::IntArrayRef kernel_size,
+    const c10::optional<at::Tensor>& bias_opt,
+    at::IntArrayRef stride,
+    at::IntArrayRef padding,
+    at::IntArrayRef dilation,
+    at::Tensor& out)
+{
+    DO_COMPATIBILITY(aclnnConvDepthwise2d, acl_op::_conv_depthwise2d_out(self, weight, kernel_size, bias_opt,
+                                                                         stride, padding, dilation, out));
+    bool is_jit_enable = !at_npu::native::env::CheckJitDisable();
+    bool is_allow_internel_format = !at_npu::native::env::CheckForbidInternalFormat();
+    ASCEND_LOGI("_conv_depthwise2d_out exec with jit compile: %d, allow internal format: %d",
+                is_jit_enable, is_allow_internel_format);
+    if (is_allow_internel_format || is_jit_enable) {
+        return acl_op::_conv_depthwise2d_out(self, weight, kernel_size, bias_opt, stride, padding, dilation, out);
+    }
+    const at::Tensor& bias = c10::value_or_else(bias_opt, [] {return at::Tensor();});
+    int8_t cube_math_type = npu_preparation::get_cube_math_type(at_npu::native::env::IsAllowConvHF32());
+    EXEC_NPU_CMD(aclnnConvDepthwise2d, self, weight, kernel_size, bias, stride, padding, dilation, out, cube_math_type);
+    return out;
+}
+#endif
 
 at::Tensor _conv_depthwise2d(
     const at::Tensor& self,
