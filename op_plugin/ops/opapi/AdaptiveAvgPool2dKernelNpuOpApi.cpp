85faf0d142c3a6a44672bad8d5c4dc0a9fed0ffa
刘嘉巍 <843972097@qq.com>
2025-03-04 11:34:20 +0000

    !2210 [Fix] Fix special case and add check for adaptive_avg_pool2d. Merge pull request !2210 from 刘嘉巍/dev

--- op_plugin/ops/opapi/AdaptiveAvgPool2dKernelNpuOpApi.cpp
+++ op_plugin/ops/opapi/AdaptiveAvgPool2dKernelNpuOpApi.cpp
@@ -20,24 +20,41 @@
 namespace op_api {
 using npu_preparation = at_npu::native::OpPreparation;
 
-at::Tensor& adaptive_avg_pool2d_out(const at::Tensor& self, at::IntArrayRef output_size, at::Tensor& result) {
-  DO_COMPATIBILITY(aclnnAdaptiveAvgPool2d, acl_op::adaptive_avg_pool2d_out(self, output_size, result));
-  EXEC_NPU_CMD(aclnnAdaptiveAvgPool2d, self, output_size, result);
-  return result;
+at::Tensor& adaptive_avg_pool2d_out(const at::Tensor& self, at::IntArrayRef output_size, at::Tensor& result)
+{
+    DO_COMPATIBILITY(aclnnAdaptiveAvgPool2d, acl_op::adaptive_avg_pool2d_out(self, output_size, result));
+    TORCH_CHECK((output_size.size() == 2), "adaptive_avg_pool2d: output size must be 2." + OPS_ERROR(ErrCode::PARAM));
+    TORCH_CHECK(self.dtype() == result.dtype(),
+                "expected dtype ", self.dtype(),
+                " for `output` but got dtype ", result.dtype(), OPS_ERROR(ErrCode::TYPE));
+    EXEC_NPU_CMD(aclnnAdaptiveAvgPool2d, self, output_size, result);
+    return result;
 }
 
-at::Tensor adaptive_avg_pool2d(const at::Tensor& self, at::IntArrayRef output_size) {
-  DO_COMPATIBILITY(aclnnAdaptiveAvgPool2d, acl_op::adaptive_avg_pool2d(self, output_size));
-  // The logic is a little different from CPU_impl
-  // can't use "NPUNativeOpApiFunctions::_adaptive_avg_pool2d(self, output_size)", this will resnet50 accuracy error
-  return at::_adaptive_avg_pool2d(self, output_size);
+at::Tensor adaptive_avg_pool2d(const at::Tensor& self, at::IntArrayRef output_size)
+{
+    DO_COMPATIBILITY(aclnnAdaptiveAvgPool2d, acl_op::adaptive_avg_pool2d(self, output_size));
+    // The logic is a little different from CPU_impl
+    // can't use "NPUNativeOpApiFunctions::_adaptive_avg_pool2d(self, output_size)", this will resnet50 accuracy error
+    TORCH_CHECK((output_size.size() == 2), "adaptive_avg_pool2d: output size must be 2." + OPS_ERROR(ErrCode::PARAM));
+    TORCH_CHECK(
+        (output_size[0] >= 0 && output_size[1] >= 0),
+        "adaptive_avg_pool2d: elements of output_size must be greater than or equal to 0 ",
+        "but received {", output_size[0], ", ", output_size[1], "}" + OPS_ERROR(ErrCode::PARAM));
+    if (!self.is_quantized() && output_size[0] == 1 && output_size[1] == 1) {
+        // in this case, adaptive pooling is just computing mean over hw
+        // dimensions, which can be done more efficiently
+        at::Tensor result = self.mean({-1, -2}, true);
+        return result;
+    }
+    return at::_adaptive_avg_pool2d(self, output_size);
 }
 
-at::Tensor _adaptive_avg_pool2d(const at::Tensor& self, at::IntArrayRef output_size) {
+at::Tensor _adaptive_avg_pool2d(const at::Tensor& self, at::IntArrayRef output_size)
+{
     DO_COMPATIBILITY(aclnnAdaptiveAvgPool2d, acl_op::_adaptive_avg_pool2d(self, output_size));
     TORCH_CHECK((self.dim() == 3 || self.dim() == 4), "non-empty 3D or 4D (batch mode) tensor expected for input"
         + OPS_ERROR(ErrCode::PARAM));
-    TORCH_CHECK((output_size.size() >= 2), "Output size must be at least 2." + OPS_ERROR(ErrCode::PARAM));
     auto outputSize = op_infer::array_to_small_vector(self.sizes());
     outputSize[self.dim() - 1] = output_size[1];
     outputSize[self.dim() - 2] = output_size[0];
--- test/test_base_ops/test_adaptive_avg_pool2d_backward.py
+++ test/test_base_ops/test_adaptive_avg_pool2d_backward.py
@@ -4,6 +4,7 @@ from torch.nn import functional as F
 import torch_npu
 
 from torch_npu.testing.testcase import TestCase, run_tests
+from torch_npu.testing.common_utils import SupportedDevices
 
 
 class TestAdaptiveAvgPool2dBackward(TestCase):
@@ -22,6 +23,24 @@ class TestAdaptiveAvgPool2dBackward(TestCase):
         input_x.requires_grad_(True)
         m = torch.nn.AdaptiveAvgPool2d(input_grad)
         output = m(input_x)
+        output.backward(output)
+        out = output.detach().cpu(), input_x.grad.cpu()
+        return out
+
+    def cpu_op_exec_func(self, input_x, output_size):
+        input_x.requires_grad_(True)
+        output = F.adaptive_avg_pool2d(input_x, output_size)
+        if input_x.dtype == torch.half:
+            output = F.adaptive_avg_pool2d(input_x.float(), output_size).half()
+        else:
+            output = F.adaptive_avg_pool2d(input_x, output_size)
+        output.backward(output)
+        out = output.detach(), input_x.grad
+        return out
+
+    def npu_op_exec_func(self, input_x, output_size):
+        input_x.requires_grad_(True)
+        output = F.adaptive_avg_pool2d(input_x, output_size)
         output.backward(output)
         out = output.detach().cpu(), input_x.grad.cpu()
         return out
@@ -55,6 +74,32 @@ class TestAdaptiveAvgPool2dBackward(TestCase):
         npu_output = self.npu_op_exec(npu_input, output_size)
         self.assertRtolEqual(cpu_output[0], npu_output[0])
         self.assertRtolEqual(cpu_output[1], npu_output[1])
+
+    @SupportedDevices(['Ascend910B'])
+    def test_adaptiveAvgPool2d_backward_special_case(self):
+        torch.manual_seed(123)
+        cpu_input = torch.randn((1, 1, 1, 3, 3), dtype=torch.float32)
+        npu_input = cpu_input.npu()
+        output_size = 1
+        cpu_output = self.cpu_op_exec_func(cpu_input, output_size)
+        npu_output = self.npu_op_exec_func(npu_input, output_size)
+        self.assertRtolEqual(cpu_output[0], npu_output[0], prec=1e-3)
+        self.assertRtolEqual(cpu_output[1], npu_output[1], prec=1e-3)
+
+    @SupportedDevices(['Ascend910B'])
+    def test_adaptiveAvgPool2d_backward_check(self):
+        torch.manual_seed(123)
+        npu_input = torch.randn((1, 3, 3, 3), dtype=torch.float32).npu()
+        output_size = -1
+        msg = "adaptive_avg_pool2d: elements of output_size must be greater than or equal to 0"
+        with self.assertRaisesRegex(RuntimeError, msg):
+            F.adaptive_avg_pool2d(npu_input, output_size)
+        
+        npu_input = torch.randn((1, 3, 3, 3), dtype=torch.float32).npu()
+        output_size = np.array((2, 2, 2))
+        msg = "adaptive_avg_pool2d: output size must be 2"
+        with self.assertRaisesRegex(RuntimeError, msg):
+            F.adaptive_avg_pool2d(npu_input, output_size)
 
 
 if __name__ == "__main__":
