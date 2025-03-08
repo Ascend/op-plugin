5debaaaf2a93ebeb690f157f9a28c2e2fda1119b
zhanghao <zhanghao418@huawei.com>
2025-03-08 09:09:35 +0000

    !2238 Upsamplebilinear2dbackward适配V2接口，兼容性修改 Merge pull request !2238 from zhanghao/master

--- op_plugin/ops/opapi/UpsampleBilinear2dBackwardKernelNpuOpApi.cpp
+++ op_plugin/ops/opapi/UpsampleBilinear2dBackwardKernelNpuOpApi.cpp
@@ -20,7 +20,7 @@
 namespace op_api {
 using npu_preparation = at_npu::native::OpPreparation;
 
-at::Tensor& upsample_bilinear2d_backward_out(
+at::Tensor& upsample_bilinear2d_backward_old_out(
     const at::Tensor& grad_output,
     at::IntArrayRef output_size,
     at::IntArrayRef input_size,
@@ -40,7 +40,7 @@ at::Tensor& upsample_bilinear2d_backward_out(
     return grad_input;
 }
 
-at::Tensor upsample_bilinear2d_backward(
+at::Tensor upsample_bilinear2d_backward_old(
     const at::Tensor& grad_output,
     at::IntArrayRef output_size,
     at::IntArrayRef input_size,
@@ -61,8 +61,74 @@ at::Tensor upsample_bilinear2d_backward(
     return grad_input;
 }
 
+at::Tensor upsample_bilinear2d_backward(
+    const at::Tensor& grad_output,
+    at::IntArrayRef output_size,
+    at::IntArrayRef input_size,
+    bool align_corners,
+    c10::optional<double> scales_h,
+    c10::optional<double> scales_w)
+{
+    DO_COMPATIBILITY(aclnnUpsampleBilinear2dBackwardV2,
+                     op_api::upsample_bilinear2d_backward_old(grad_output, output_size, input_size,
+                                                              align_corners, scales_h, scales_w));
+    auto outputSize = input_size;
+    at::Tensor grad_input = npu_preparation::apply_tensor(grad_output, outputSize);
+    double scales_h_attr = scales_h.value_or(0);
+    double scales_w_attr = scales_w.value_or(0);
+
+    EXEC_NPU_CMD(aclnnUpsampleBilinear2dBackwardV2, grad_output, output_size, input_size, align_corners,
+                 scales_h_attr, scales_w_attr, grad_input);
+    return grad_input;
+}
+
+at::Tensor& upsample_bilinear2d_backward_out(
+    const at::Tensor& grad_output,
+    at::IntArrayRef output_size,
+    at::IntArrayRef input_size,
+    bool align_corners,
+    c10::optional<double> scales_h,
+    c10::optional<double> scales_w,
+    at::Tensor& grad_input)
+{
+    DO_COMPATIBILITY(aclnnUpsampleBilinear2dBackwardV2,
+                     op_api::upsample_bilinear2d_backward_old_out(grad_output, output_size, input_size,
+                                                                  align_corners, scales_h, scales_w, grad_input));
+    npu_preparation::check_tensor({grad_output}, grad_input, grad_output, input_size);
+    double scales_h_attr = scales_h.value_or(0);
+    double scales_w_attr = scales_w.value_or(0);
+    EXEC_NPU_CMD(aclnnUpsampleBilinear2dBackwardV2, grad_output, output_size, input_size, align_corners,
+                 scales_h_attr, scales_w_attr, grad_input);
+    return grad_input;
+}
+
 #if VERSION_BETWEEN(V1R11, V1R11)
 at::Tensor upsample_bilinear2d_backward(
+    const at::Tensor& grad_output,
+    c10::optional<at::IntArrayRef> output_size,
+    at::IntArrayRef input_size,
+    bool align_corners,
+    c10::optional<at::ArrayRef<double>> scale_factors)
+{
+    DO_COMPATIBILITY(aclnnUpsampleBilinear2dBackwardV2,
+                     op_api::upsample_bilinear2d_backward_old(grad_output, output_size, input_size,
+                                                              align_corners, scale_factors));
+    auto osize = op_infer::upsample_infershape_with_scale(input_size, output_size, scale_factors);
+    auto scales_h = op_plugin::utils::get_scale_value(scale_factors, 0);
+    auto scales_w = op_plugin::utils::get_scale_value(scale_factors, 1);
+    double scales_h_attr = scales_h.value_or(0);
+    double scales_w_attr = scales_w.value_or(0);
+
+    auto outputsize = at::IntArrayRef(osize);
+    auto outputSize = input_size;
+    at::Tensor grad_input = npu_preparation::apply_tensor(grad_output, outputSize);
+
+    EXEC_NPU_CMD(aclnnUpsampleBilinear2dBackwardV2, grad_output, outputsize, input_size, align_corners,
+                 scales_h_attr, scales_w_attr, grad_input);
+    return grad_input;
+}
+
+at::Tensor upsample_bilinear2d_backward_old(
     const at::Tensor& grad_output,
     c10::optional<at::IntArrayRef> output_size,
     at::IntArrayRef input_size,
