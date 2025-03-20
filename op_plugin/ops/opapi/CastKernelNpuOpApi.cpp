7d4032b731ef21df50289b26fc78cae85b1d2060
刘嘉巍 <843972097@qq.com>
2025-03-20 01:12:40 +0000

    !2268 [Codecheck] Fix codecheck. Merge pull request !2268 from 刘嘉巍/deformable

--- op_plugin/ops/opapi/CastKernelNpuOpApi.cpp
+++ op_plugin/ops/opapi/CastKernelNpuOpApi.cpp
@@ -19,31 +19,35 @@
 #include "op_plugin/utils/op_api_common.h"
 
 namespace op_api {
+using npu_preparation = at_npu::native::OpPreparation;
 
-namespace{
-at::Tensor npu_dtype_cast_impl_op_api(const at::Tensor& self, at::ScalarType dtype) {
-  if (self.dtype() == dtype) {
-    return self.clone();
-  }
-  // construct the output tensor of the NPU
-  at::Tensor result = at_npu::native::OpPreparation::apply_tensor_without_format(self.sizes(),
-                                                                                 self.options().dtype(dtype));
+namespace {
+at::Tensor npu_dtype_cast_impl_op_api(const at::Tensor& self, at::ScalarType dtype)
+{
+    if (self.dtype() == dtype) {
+        return self.clone();
+    }
+    // construct the output tensor of the NPU
+    at::Tensor result = npu_preparation::apply_tensor_without_format(self.sizes(),
+                                                                     self.options().dtype(dtype));
 
-  // calculate the output result of the NPU
-  EXEC_NPU_CMD(aclnnCast, self, dtype, result);
+    // calculate the output result of the NPU
+    EXEC_NPU_CMD(aclnnCast, self, dtype, result);
 
-  return result;
+    return result;
 }
 } // namespace
 
-at::Tensor npu_dtype_cast(const at::Tensor& self, at::ScalarType dtype) {
-  DO_COMPATIBILITY(aclnnCast, acl_op::npu_dtype_cast(self, dtype));
-  return npu_dtype_cast_impl_op_api(self, dtype);
+at::Tensor npu_dtype_cast(const at::Tensor& self, at::ScalarType dtype)
+{
+    DO_COMPATIBILITY(aclnnCast, acl_op::npu_dtype_cast(self, dtype));
+    return npu_dtype_cast_impl_op_api(self, dtype);
 }
 
-at::Tensor npu_dtype_cast_backward(const at::Tensor& grad, at::ScalarType dtype) {
-  grad.requires_grad_();
-  return at_npu::native::custom_ops::npu_dtype_cast(grad, dtype);
+at::Tensor npu_dtype_cast_backward(const at::Tensor& grad, at::ScalarType dtype)
+{
+    grad.requires_grad_();
+    return at_npu::native::custom_ops::npu_dtype_cast(grad, dtype);
 }
 
 }
