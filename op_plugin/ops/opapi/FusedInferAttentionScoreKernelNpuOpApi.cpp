c8fecc6e518aa4e9a6a7a5e6b070dd6289386e25
minmengdie <minmengdie@huawei.com>
2025-03-28 02:47:59 +0000

    !2352 fused_infer_attention_score interface add param : key_rope_antiquant_scale Merge pull request !2352 from minmengdie/kras

--- op_plugin/ops/opapi/FusedInferAttentionScoreKernelNpuOpApi.cpp
+++ op_plugin/ops/opapi/FusedInferAttentionScoreKernelNpuOpApi.cpp
@@ -51,6 +51,7 @@ std::tuple<at::Tensor, at::Tensor> npu_fused_infer_attention_score_symint(
     c10::OptionalArrayRef<c10::SymInt> actual_shared_prefix_len,
     const c10::optional<at::Tensor> &query_rope,
     const c10::optional<at::Tensor> &key_rope,
+    const c10::optional<at::Tensor> &key_rope_antiquant_scale,
     int64_t num_heads, double scale,
     int64_t pre_tokens, int64_t next_tokens,
     c10::string_view input_layout, int64_t num_key_value_heads,
@@ -113,7 +114,7 @@ std::tuple<at::Tensor, at::Tensor> npu_fused_infer_attention_score_symint(
     // dispatch hostAPI
     EXEC_NPU_NO_FORMAT_CHECK_CMD(aclnnFusedInferAttentionScoreV3, query, keyTensors, valueTensors, pse_shift, atten_mask, actSeqLen, actSeqLenKv, dequant_scale1, quant_scale1, dequant_scale2,
         quant_scale2, quant_offset2, antiquant_scale, antiquant_offset, block_table, query_padding_size, kv_padding_size, key_antiquant_scale, key_antiquant_offset, value_antiquant_scale,
-        value_antiquant_offset, key_shared_prefix, value_shared_prefix, actSeqLenPrefix, query_rope, key_rope, num_heads, scale, pre_tokens, next_tokens, input_layout_ptr,
+        value_antiquant_offset, key_shared_prefix, value_shared_prefix, actSeqLenPrefix, query_rope, key_rope, key_rope_antiquant_scale, num_heads, scale, pre_tokens, next_tokens, input_layout_ptr,
         num_key_value_heads, sparse_mode, inner_precise, block_size, antiquant_mode, softmax_lse_flag, key_antiquant_mode, value_antiquant_mode, output, softmax_lse);
     return std::tuple<at::Tensor, at::Tensor>(output, softmax_lse);
 }
