f30848c21a2eede9e8238d084e1282bd98b53b4e
shaoyf <shaoyifan1@huawei.com>
2025-03-28 00:00:07 +0000

    !2364 回退 'Pull Request !2336 : combine原型变更' Merge pull request !2364 from shaoyf/revert-merge-2336-master

--- op_plugin/python/meta/_meta_registrations.py
+++ op_plugin/python/meta/_meta_registrations.py
@@ -525,7 +525,7 @@ def npu_moe_distribute_dispatch_meta(x, expert_ids, group_ep, ep_world_size, ep_
 
 @impl(m, "npu_moe_distribute_combine")
 def npu_moe_distribute_combine_meta(expand_x, expert_ids, expand_idx, ep_send_counts, expert_scales, group_ep, ep_world_size, ep_rank_id, moe_expert_num,
-                                    tp_send_counts=None, x_active_mask=None, activation_scale=None, weight_scale=None, group_list=None, expand_scales=None, group_tp="", tp_world_size=0,
+                                    tp_send_counts=None, x_active_mask=None, activation_scale=None, weight_scale=None, group_list=None, group_tp="", tp_world_size=0,
                                     tp_rank_id=0, expert_shard_type=0, shared_expert_num=1, shared_expert_rank_num=0, global_bs=0, out_dtype=0, comm_quant_mode=0, group_list_type=0):
     dim_list = []
     dim_list.append(expert_ids.size(0))
