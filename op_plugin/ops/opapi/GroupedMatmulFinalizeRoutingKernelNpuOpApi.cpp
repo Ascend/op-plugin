73650948756798baf08bb7ca03dcfb24e1f53bf8
PapayaMilkZ <zhangdengke9@h-partners.com>
2025-04-03 14:39:10 +0000

    !2381 add npu_grouped_matmul_finalize_routing Merge pull request !2381 from PapayaMilkZ/GMM_FinalizeRouting

---
+++ op_plugin/ops/opapi/GroupedMatmulFinalizeRoutingKernelNpuOpApi.cpp
@@ -0,0 +1,127 @@
+// Copyright (c) 2025 Huawei Technologies Co., Ltd
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "op_plugin/OpApiInterface.h"
+#include "op_plugin/utils/op_api_common.h"
+
+namespace op_api {
+constexpr size_t LAST_SECOND_DIM_INDEX = 2;
+constexpr int64_t INT4_NUMS_IN_INT32 = 8;
+using npu_preparation = at_npu::native::OpPreparation;
+
+static bool is_nz_format(const at::Tensor& w)
+{
+    const torch_npu::NPUStorageDesc &tensor_desc =
+        torch_npu::NPUBridge::GetNpuStorageImpl(w)->npu_desc_;
+    return tensor_desc.npu_format_ == ACL_FORMAT_FRACTAL_NZ;
+}
+
+at::Tensor npu_grouped_matmul_finalize_routing(
+    const at::Tensor &x,
+    const at::Tensor &w,
+    const at::Tensor &group_list,
+    const c10::optional<at::Tensor>& scale,
+    const c10::optional<at::Tensor>& bias,
+    const c10::optional<at::Tensor>& pertoken_scale,
+    const c10::optional<at::Tensor>& shared_input,
+    const c10::optional<at::Tensor>& logit,
+    const c10::optional<at::Tensor>& row_index,
+    c10::optional<at::ScalarType> dtype,
+    c10::optional<double> shared_input_weight,
+    c10::optional<int64_t> shared_input_offset,
+    c10::optional<int64_t> output_bs,
+    c10::optional<int64_t> group_list_type
+    )
+{
+    TORCH_CHECK(is_nz_format(w),
+                "only support w's format is nz, please upgrade CANN.",
+                OPS_ERROR(ErrCode::PARAM));
+    TORCH_CHECK(group_list_type == 1,
+                "only support group_list_type's value is 1.",
+                OPS_ERROR(ErrCode::PARAM));
+
+    auto x_dim_num = x.dim();
+    auto w_dim_num = w.dim();
+
+    constexpr int EXPECTED_X_DIM = 2;
+    constexpr int EXPECTED_W_DIM = 3;
+
+    TORCH_CHECK(x_dim_num == EXPECTED_X_DIM && w_dim_num == EXPECTED_W_DIM,
+                "x dim is ", EXPECTED_X_DIM, " weight dim is ", EXPECTED_W_DIM,
+                OPS_ERROR(ErrCode::PARAM));
+
+    auto x_m_dim = x.size(x_dim_num - LAST_SECOND_DIM_INDEX);
+    auto x_k_dim = x.size(x_dim_num - 1);
+    auto w_n_dim = w.size(w_dim_num - 1);
+    auto w_k_dim = w.size(w_dim_num - LAST_SECOND_DIM_INDEX);
+
+    auto output_size = op_infer::array_to_small_vector(x.sizes());
+    int32_t output_bs_real = static_cast<int32_t>(output_bs.value_or(0));
+    if (output_bs_real == 0) {
+        output_bs_real = x_m_dim;
+    }
+    output_size[x_dim_num - LAST_SECOND_DIM_INDEX] = output_bs_real;
+    output_size[x_dim_num - 1] = w_n_dim;
+
+    const at::Tensor &scale_real = scale.value_or(at::Tensor());
+    const at::Tensor &bias_real = bias.value_or(at::Tensor());
+    const at::Tensor &pertoken_scale_real = pertoken_scale.value_or(at::Tensor());
+    const at::Tensor &shared_input_real = shared_input.value_or(at::Tensor());
+    const at::Tensor &logit_real = logit.value_or(at::Tensor());
+    const at::Tensor &row_index_real = row_index.value_or(at::Tensor());
+    float shared_input_weight_real = static_cast<float>(shared_input_weight.value_or(1.0));
+    int64_t shared_input_offset_real = shared_input_offset.value_or(0);
+    int64_t group_list_type_real = group_list_type.value_or(1);
+
+    auto scene_has_share = false;
+    auto scene_no_share = false;
+    if (scale.has_value() &&  pertoken_scale.has_value() && shared_input.has_value()
+        && logit.has_value() && row_index.has_value()) {
+        scene_has_share = true;
+    }
+
+    if (scale.has_value() &&  pertoken_scale.has_value() && !shared_input.has_value()
+        && !logit.has_value() && row_index.has_value()) {
+        scene_no_share = true;
+    }
+
+    TORCH_CHECK(scene_has_share || scene_no_share,
+                "input tensor only support shared_input and logit empty tensor",
+                OPS_ERROR(ErrCode::PARAM));
+    
+    at::ScalarType dst_type = c10::value_or_else(dtype, [] {return at::ScalarType::Float;});
+    TORCH_CHECK(dst_type == at::ScalarType::Float,
+        "The dtype should be float", OPS_ERROR(ErrCode::PARAM));
+
+    if (shared_input.has_value() && logit.has_value()) {
+        TORCH_CHECK(dst_type == at::ScalarType::Float,
+                    "When shared_input and logit is not None, the dtype must be float32",
+                    OPS_ERROR(ErrCode::PARAM));
+    }
+    c10::TensorOptions options = x.options().dtype(dst_type);
+
+    at::Tensor result = npu_preparation::apply_tensor_without_format(output_size, options);
+
+    bool transposeX = false;
+    bool transposeW = false;
+
+    int64_t dtype_real = 0;
+
+    EXEC_NPU_CMD(aclnnGroupedMatmulFinalizeRoutingWeightNz, x, w, scale_real, bias_real, pertoken_scale_real, group_list, shared_input_real,
+                 logit_real, row_index_real, dtype_real, shared_input_weight_real, shared_input_offset_real, transposeX, transposeW, group_list_type_real, result);
+    return result;
+}
+
+}
---
+++ test/test_custom_ops/test_npu_grouped_matmul_finalize_routing.py
@@ -0,0 +1,99 @@
+import torch
+import torch_npu
+import unittest
+import torch.nn.functional as F
+
+from torch_npu.testing.testcase import TestCase, run_tests
+from torch_npu.testing.common_utils import SupportedDevices
+
+
+class TestGroupedMatmulFinalizeRouting(TestCase):
+
+    def supported_op_exec(self,
+                          topK, x, weight, group_list, scale, pertoken_scale,
+                          shared_input=None, logit=None, row_index=None,
+                          shared_input_scale=1, shared_input_offset=0):
+        x_split = torch.split(x, group_list.tolist(), dim=0)
+        pertoken_scale_split = torch.split(pertoken_scale, group_list.tolist(), dim=0)
+        mm_outs = []
+        for i in range(len(group_list)):
+            mm = torch.matmul(x_split[i].to(torch.int32), weight[i].to(torch.int32))
+            mm = mm.to(torch.float32) * scale[i].to(torch.float32) * pertoken_scale_split[i]
+            mm_outs.append(mm)
+        mm_out = torch.cat(mm_outs, dim=0)
+        if shared_input is not None:
+            out = mm_out * logit.reshape(-1, 1)
+            index = torch.argsort(row_index, dim=0)
+            out = out[index].reshape(-1, topK, mm_out.shape[-1]).sum(dim=1)
+            out[shared_input_offset:shared_input_offset + shared_input.shape[0], :] += \
+                shared_input_scale * shared_input.to(torch.float32)
+        else:
+            index = torch.argsort(row_index, dim=0)
+            out = mm_out[index].reshape(-1, topK, mm_out.shape[-1]).sum(dim=1)
+        return out
+    
+    @unittest.skip("skip test_npu_grouped_matmul_finalize_routing_1")
+    @SupportedDevices(["Ascend910B"])
+    def test_npu_grouped_matmul_finalize_routing_1(self, device="npu"):
+        m, k, n, batch, topK, group_num, shared_input_scale = 576, 2048, 7168, 72, 8, 8, 1
+        x = torch.randint(-10, 10, (m, k), dtype=torch.int8)
+        weight = torch.randint(-10, 10, (group_num, k, n), dtype=torch.int8)
+        scale = torch.normal(0, 0.01, (group_num, n), dtype=torch.float32)
+        pertoken_scale = torch.normal(0, 0.01, (m, 1), dtype=torch.float32)
+        group_list = torch.tensor([batch] * group_num, dtype=torch.int64)
+
+        logit_ori = torch.normal(0, 0.1, (batch, group_num), dtype=torch.float32)
+        routing = torch.argsort(logit_ori, 1)[:, -topK:]
+
+        shared_input = torch.normal(0, 0.1, (batch // 4, n), dtype=torch.bfloat16)
+        logit = F.softmax(
+            logit_ori[torch.arange(batch).reshape(-1, 1).repeat(1, topK), routing],
+            dim=1,
+            dtype=torch.float32
+        ).reshape(m)
+        row_index = (torch.argsort(routing.reshape(-1)) // topK).to(torch.int64)
+        shared_input_offset = batch // 2
+        output_bs = batch
+
+        supported_output = self.supported_op_exec(topK, x, weight, group_list, scale, 
+                                                  pertoken_scale, shared_input, logit, row_index,
+                                                  shared_input_scale, shared_input_offset)
+        weightNz = torch_npu.npu_format_cast(weight.npu(), 29)
+        pertoken_scale = pertoken_scale.reshape(m)
+        custome_output = torch_npu.npu_grouped_matmul_finalize_routing(
+            x.npu(), weightNz, group_list.npu(), scale=scale.npu(),
+            pertoken_scale=pertoken_scale.npu(), shared_input=shared_input.npu(),
+            logit=logit.npu(), row_index=row_index.npu(),
+            shared_input_offset=shared_input_offset, output_bs=output_bs
+        ).to("cpu")
+        self.assertRtolEqual(supported_output, custome_output, 0.001)
+
+    @unittest.skip("skip test_npu_grouped_matmul_finalize_routing_2")
+    @SupportedDevices(["Ascend910B"])
+    def test_npu_grouped_matmul_finalize_routing_2(self, device="npu"):
+        m, k, n, batch, topK, group_num = 72, 2048, 7168, 72, 1, 1
+        x = torch.randint(-10, 10, (m, k), dtype=torch.int8)
+        weight = torch.randint(-10, 10, (group_num, k, n), dtype=torch.int8)
+        scale = torch.normal(0, 0.01, (group_num, n), dtype=torch.float32)
+        pertoken_scale = torch.normal(0, 0.01, (m, 1), dtype=torch.float32)
+        group_list = torch.tensor([batch] * group_num, dtype=torch.int64)
+
+        logit_ori = torch.normal(0, 0.1, (batch, group_num), dtype=torch.float32)
+        routing = torch.argsort(logit_ori, 1)[:, -topK:]
+        row_index = (torch.argsort(routing.reshape(-1)) // topK).to(torch.int64)
+        output_bs = m
+
+        supported_output = self.supported_op_exec(topK, x, weight, group_list,
+                                                  scale, pertoken_scale, row_index=row_index)
+        weightNz = torch_npu.npu_format_cast(weight.npu(), 29)
+        pertoken_scale = pertoken_scale.reshape(m)
+        custom_output = torch_npu.npu_grouped_matmul_finalize_routing(
+            x.npu(), weightNz, group_list.npu(), scale=scale.npu(),
+            pertoken_scale=pertoken_scale.npu(), row_index=row_index.npu(),
+            output_bs=output_bs
+        ).to("cpu")
+        self.assertRtolEqual(supported_output, custom_output, 0.001)
+
+
+if __name__ == "__main__":
+    run_tests()
