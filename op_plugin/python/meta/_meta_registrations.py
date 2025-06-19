64886f4420e39aff5013e79a78ea4dad9f8ba06a
刘嘉巍 <843972097@qq.com>
2025-06-19 12:06:17 +0000

    !2652 [Feature] Add npu_gather_sparse_index meta Merge pull request !2652 from 刘嘉巍/master-dev

--- op_plugin/python/meta/_meta_registrations.py
+++ op_plugin/python/meta/_meta_registrations.py
@@ -9,6 +9,7 @@ Registering Meta implementations for custom ops
 '''
 BIT_NUMBER = 128
 UINT8_BIT_NUMBER = 8
+NPU_TENSOR_DIM_LIMIT = 8
 INPUTS_DIM_LIMIT_QUANTCONV2D = 4
 ATTR_DIM_LIMIT_QUANTCONV2D = 2
 #meta register implementation
@@ -2049,3 +2050,30 @@ def npu_mrope_meta(positions, query, key, cos_sin_cache, head_size, *, mrope_sec
 @impl(m, "npu_format_cast")
 def npu_format_cast_meta(self, dst):
     return torch.empty_like(self)
+
+
+@impl(m, "npu_gather_sparse_index")
+def npu_gather_sparse_index(inputs, index):
+    output_dim = inputs.dim() + index.dim() - 1
+    torch._check(
+        output_dim <= NPU_TENSOR_DIM_LIMIT,
+        lambda: f"input.dim() + index.dim() - 1 must not greater than 8, but got {output_dim}.",
+        )
+
+    output_size = []
+    input_dim = inputs.dim()
+    input_size = inputs.size()
+    if input_dim == 0:
+        output_size = input_size
+
+        return torch.empty(output_size, dtype=inputs.dtype, device=inputs.device)
+
+    index_dim = index.dim()
+    index_size = index.size()
+    for i in range(index_dim):
+        output_size.append(index_size[i])
+
+    for i in range(1, input_dim):
+        output_size.append(input_size[i])
+    
+    return torch.empty(output_size, dtype=inputs.dtype, device=inputs.device)
