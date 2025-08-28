da020c752a28984b2b71692311e97ab94fe1bb2e
操仁垒 <caorenlei@huawei.com>
2025-08-28 01:35:54 +0000

    !3053 add torch_npu npu_kv_rmsnorm_rope_cache_v2 api Merge pull request !3053 from 操仁垒/qzh_master

--- op_plugin/ops/opapi/KvRmsNormRopeCacheNpuOpApi.cpp
+++ op_plugin/ops/opapi/KvRmsNormRopeCacheNpuOpApi.cpp
@@ -18,6 +18,7 @@
 
 namespace op_api {
 using npu_preparation = at_npu::native::OpPreparation;
+const int64_t MAX_DIM = 4;
 std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor> npu_kv_rmsnorm_rope_cache(
     const at::Tensor &kv,
     const at::Tensor &gamma,
@@ -47,6 +48,72 @@ std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor> npu_kv_rmsnorm_rope_c
                                  k_rope_scale, c_kv_scale, k_rope_offset, c_kv_offset, epsilon, cache_mode_ptr,
                                  is_output_kv, k_rope, c_kv);
     return std::tie(k_cache, ckv_cache, k_rope, c_kv);
+}
+
+std::tuple<at::Tensor, at::Tensor> npu_kv_rmsnorm_rope_cache_v2(
+    const at::Tensor &kv,
+    const at::Tensor &gamma,
+    const at::Tensor &cos,
+    const at::Tensor &sin,
+    const at::Tensor &index,
+    at::Tensor &k_cache,
+    at::Tensor &ckv_cache,
+    const c10::optional<at::Tensor> &k_rope_scale,
+    const c10::optional<at::Tensor> &c_kv_scale,
+    const c10::optional<at::Tensor> &k_rope_offset,
+    const c10::optional<at::Tensor> &c_kv_offset,
+    double epsilon,
+    c10::string_view cache_mode,
+    bool is_output_kv)
+{
+    TORCH_CHECK((kv.dim() == MAX_DIM), "4D tensor expected for input kv" + OPS_ERROR(ErrCode::PARAM));
+    TORCH_CHECK((gamma.dim() == 1), "1D tensor expected for input gamma" + OPS_ERROR(ErrCode::PARAM));
+    TORCH_CHECK((cos.dim() == MAX_DIM), "4D tensor expected for input cos" + OPS_ERROR(ErrCode::PARAM));
+    c10::SmallVector<int64_t, SIZE> k_rope_shape = {kv.size(0), kv.size(1), kv.size(2), cos.size(3)};
+    c10::SmallVector<int64_t, SIZE> c_kv_shape = {kv.size(0), kv.size(1), kv.size(2), gamma.size(0)};
+
+    char *cache_mode_ptr = const_cast<char *>(cache_mode.data());
+    at::Tensor k_rope = npu_preparation::apply_tensor_without_format(k_rope_shape, kv.options());
+    at::Tensor c_kv = npu_preparation::apply_tensor_without_format(c_kv_shape, kv.options());
+    EXEC_NPU_NO_FORMAT_CHECK_CMD(aclnnKvRmsNormRopeCache, kv, gamma, cos, sin, index, k_cache, ckv_cache,
+                                 k_rope_scale, c_kv_scale, k_rope_offset, c_kv_offset, epsilon, cache_mode_ptr,
+                                 is_output_kv, k_rope, c_kv);
+    return std::tie(k_rope, c_kv);
+}
+
+std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor> npu_kv_rmsnorm_rope_cache_v2_functional(
+    const at::Tensor &kv,
+    const at::Tensor &gamma,
+    const at::Tensor &cos,
+    const at::Tensor &sin,
+    const at::Tensor &index,
+    const at::Tensor &k_cache,
+    const at::Tensor &ckv_cache,
+    const c10::optional<at::Tensor> &k_rope_scale,
+    const c10::optional<at::Tensor> &c_kv_scale,
+    const c10::optional<at::Tensor> &k_rope_offset,
+    const c10::optional<at::Tensor> &c_kv_offset,
+    double epsilon,
+    c10::string_view cache_mode,
+    bool is_output_kv)
+{
+    TORCH_CHECK((kv.dim() == MAX_DIM), "4D tensor expected for input kv" + OPS_ERROR(ErrCode::PARAM));
+    TORCH_CHECK((gamma.dim() == 1), "1D tensor expected for input gamma" + OPS_ERROR(ErrCode::PARAM));
+    TORCH_CHECK((cos.dim() == MAX_DIM), "4D tensor expected for input cos" + OPS_ERROR(ErrCode::PARAM));
+    c10::SmallVector<int64_t, SIZE> k_rope_shape = {kv.size(0), kv.size(1), kv.size(2), cos.size(3)};
+    c10::SmallVector<int64_t, SIZE> c_kv_shape = {kv.size(0), kv.size(1), kv.size(2), gamma.size(0)};
+
+    char *cache_mode_ptr = const_cast<char *>(cache_mode.data());
+    at::Tensor k_rope = npu_preparation::apply_tensor_without_format(k_rope_shape, kv.options());
+    at::Tensor c_kv = npu_preparation::apply_tensor_without_format(c_kv_shape, kv.options());
+    // 保证接口对外为functional
+    at::Tensor k_cache_inplace = k_cache.clone();
+    at::Tensor ckv_cache_inplace = ckv_cache.clone();
+
+    EXEC_NPU_NO_FORMAT_CHECK_CMD(aclnnKvRmsNormRopeCache, kv, gamma, cos, sin, index, k_cache_inplace,
+                                 ckv_cache_inplace, k_rope_scale, c_kv_scale, k_rope_offset, c_kv_offset, epsilon,
+                                 cache_mode_ptr, is_output_kv, k_rope, c_kv);
+    return std::tie(k_rope, c_kv, k_cache_inplace, ckv_cache_inplace);
 }
 
 }
--- op_plugin/python/meta/_meta_registrations.py
+++ op_plugin/python/meta/_meta_registrations.py
@@ -2191,6 +2191,50 @@ def npu_kv_rmsnorm_rope_cache_meta(kv, gamma, cos, sin, index, k_cache, ckv_cach
             torch.empty(c_kv_size, dtype=kv.dtype, device=kv.device))
 
 
+@impl(m, "npu_kv_rmsnorm_rope_cache_v2")
+def npu_kv_rmsnorm_rope_cache_v2_meta(kv, gamma, cos, sin, index, k_cache, ckv_cache, *, k_rope_scale=None,
+                                           c_kv_scale=None, k_rope_offset=None, c_kv_offset=None, epsilon=1e-5,
+                                           cache_mode='Norm', is_output_kv=False):
+    if kv.dim() != 4:
+        raise RuntimeError("4D tensor expected for input kv" + ops_error(ErrCode.PARAM))
+    if gamma.dim() != 1:
+        raise RuntimeError("1D tensor expected for input gamma" + ops_error(ErrCode.PARAM))
+    if cos.dim() != 4:
+        raise RuntimeError("4D tensor expected for input cos" + ops_error(ErrCode.PARAM))
+    k_rope_size = []
+    c_kv_size = []
+    for i in range(kv.dim() - 1):
+        k_rope_size.append(kv.size(i))
+        c_kv_size.append(kv.size(i))
+    k_rope_size.append(cos.size(3))
+    c_kv_size.append(gamma.size(0))
+    return (torch.empty(k_rope_size, dtype=kv.dtype, device=kv.device),
+            torch.empty(c_kv_size, dtype=kv.dtype, device=kv.device))
+
+
+@impl(m, "npu_kv_rmsnorm_rope_cache_v2_functional")
+def npu_kv_rmsnorm_rope_cache_v2_functional_meta(kv, gamma, cos, sin, index, k_cache, ckv_cache, *,
+                                                      k_rope_scale=None, c_kv_scale=None, k_rope_offset=None,
+                                                      c_kv_offset=None, epsilon=1e-5, cache_mode='Norm',
+                                                      is_output_kv=False):
+    if kv.dim() != 4:
+        raise RuntimeError("4D tensor expected for input kv" + ops_error(ErrCode.PARAM))
+    if gamma.dim() != 1:
+        raise RuntimeError("1D tensor expected for input gamma" + ops_error(ErrCode.PARAM))
+    if cos.dim() != 4:
+        raise RuntimeError("4D tensor expected for input cos" + ops_error(ErrCode.PARAM))
+    k_rope_size = []
+    c_kv_size = []
+    for i in range(kv.dim() - 1):
+        k_rope_size.append(kv.size(i))
+        c_kv_size.append(kv.size(i))
+    k_rope_size.append(cos.size(3))
+    c_kv_size.append(gamma.size(0))
+    return (torch.empty(k_rope_size, dtype=kv.dtype, device=kv.device),
+            torch.empty(c_kv_size, dtype=kv.dtype, device=kv.device),
+            torch.empty_like(k_cache), torch.empty_like(ckv_cache))
+
+
 @impl(m, "npu_apply_rotary_pos_emb")
 def npu_apply_rotary_pos_emb_meta(query, key, cos, sin, layout=1, rotary_mode='half'):
     return (torch.empty_like(query, dtype=query.dtype), torch.empty_like(key, dtype=key.dtype))
