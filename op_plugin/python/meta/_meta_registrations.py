fix softmax lse empty tensor
Co-authored-by: hshi25<shihaoyu3@huawei.com>



# message auto-generated for no-merge-commit merge:
merge master into master

fix softmax lse empty tensor

Created-by: hshi25
Commit-by: hshi25
Merged-by: ascend-robot
Description: <!--  Thanks for sending a pull request! 
-->

**What type of PR is this?**
> Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
>
> /kind bug
> /kind task
> /kind feature


**What does this PR do / why do we need it**:


**Special notes for your reviewers**:




See merge request: Ascend/op-plugin!3174

op_plugin/python/meta/_meta_registrations.py
@@ -661,7 +661,7 @@ def npu_fused_infer_attention_score_forward(query, key, value, *, pse_shift=None
             else:
                 return (torch.empty_like(tmp_out, dtype=torch.int8), torch.empty([B, N, S1, 1], dtype=torch.float32, device='meta'))
         else:
-            return (torch.empty_like(tmp_out, dtype=torch.int8), torch.empty([1], dtype=torch.float32, device='meta'))
+            return (torch.empty_like(tmp_out, dtype=torch.int8), torch.empty([0], dtype=torch.float32, device='meta'))
     elif query.dtype == torch.int8:
         out_dtype = torch.half
         if query_rope is not None:
@@ -674,7 +674,7 @@ def npu_fused_infer_attention_score_forward(query, key, value, *, pse_shift=None
                 return (torch.empty_like(tmp_out, dtype=out_dtype), torch.empty([B, N, S1, 1], dtype=torch.float32, \
                     device='meta'))
         else:
-            return (torch.empty_like(tmp_out, dtype=out_dtype), torch.empty([1], dtype=torch.float32, device='meta'))
+            return (torch.empty_like(tmp_out, dtype=out_dtype), torch.empty([0], dtype=torch.float32, device='meta'))
     else:
         if (softmax_lse_flag == True):
             if input_layout == "TND" or input_layout == "TND_NTD":
@@ -687,7 +687,7 @@ def npu_fused_infer_attention_score_forward(query, key, value, *, pse_shift=None
             else:
                 return (torch.empty_like(tmp_out), torch.empty([B, N, S1, 1], dtype=torch.float32, device='meta'))
         else:
-            return (torch.empty_like(tmp_out), torch.empty([1], dtype=torch.float32, device='meta'))
+            return (torch.empty_like(tmp_out), torch.empty([0], dtype=torch.float32, device='meta'))
 
 
 @impl(m, "npu_fused_infer_attention_score_v2")
@@ -851,7 +851,7 @@ def npu_fused_infer_attention_score_v2_forward(query, key, value, *, query_rope=
             else:
                 return (torch.empty_like(tmp_out, dtype=torch.int8), torch.empty([B, N, S1, 1], dtype=torch.float32, device='meta'))
         else:
-            return (torch.empty_like(tmp_out, dtype=torch.int8), torch.empty([1], dtype=torch.float32, device='meta'))
+            return (torch.empty_like(tmp_out, dtype=torch.int8), torch.empty([0], dtype=torch.float32, device='meta'))
     elif query.dtype == torch.int8:
         out_dtype = torch.half
         if query_rope is not None:
@@ -864,7 +864,7 @@ def npu_fused_infer_attention_score_v2_forward(query, key, value, *, query_rope=
                 return (torch.empty_like(tmp_out, dtype=out_dtype), torch.empty([B, N, S1, 1], dtype=torch.float32, \
                     device='meta'))
         else:
-            return (torch.empty_like(tmp_out, dtype=out_dtype), torch.empty([1], dtype=torch.float32, device='meta'))
+            return (torch.empty_like(tmp_out, dtype=out_dtype), torch.empty([0], dtype=torch.float32, device='meta'))
     else:
         if return_softmax_lse:
             if input_layout == "TND" or input_layout == "TND_NTD":
@@ -885,7 +885,7 @@ def npu_fused_infer_attention_score_v2_forward(query, key, value, *, query_rope=
             else:
                 return (torch.empty_like(tmp_out), torch.empty([B, N, S1, 1], dtype=torch.float32, device='meta'))
         else:
-            return (torch.empty_like(tmp_out), torch.empty([1], dtype=torch.float32, device='meta'))
+            return (torch.empty_like(tmp_out), torch.empty([0], dtype=torch.float32, device='meta'))
 
 
 @impl(m, "npu_fusion_attention")