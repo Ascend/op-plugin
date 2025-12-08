3ce6abf2e3de4e326572600d479cc74529d5cb27
qq_43150381 <zhugelu1@h-partners.com>
2025-12-08 15:54:13 +0800

    Change the error message to match aclnnRound
    Co-authored-by: qq_43150381<zhugelu1@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3688 merge 7.3.0 into 7.3.0
    
    Change the error message to match aclnnRound
    
    Created-by: qq_43150381
    Commit-by: qq_43150381
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/op-plugin!3688

--- op_plugin/ops/opapi/RoundDecimalsKernelNpuOpApi.cpp
+++ op_plugin/ops/opapi/RoundDecimalsKernelNpuOpApi.cpp
@@ -22,7 +22,9 @@ using npu_preparation = at_npu::native::OpPreparation;
 
 static void round_decimals_check(const at::Tensor& self, int64_t decimals)
 {
-    TORCH_CHECK(!isIntegralType(self.scalar_type(), true),
+    TORCH_CHECK(isFloatingType(self.scalar_type()) ||
+                self.scalar_type() == at::ScalarType::Int ||
+                self.scalar_type() == at::ScalarType::Long,
                 "\"round_npu\" not implemented for '", toString(self.scalar_type()), "'",
                 OPS_ERROR(ErrCode::TYPE));
 }
