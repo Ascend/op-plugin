9344e658f544266e07b93623b02872ce5cecbe21
culechan <chenshuai81@huawei.com>
2025-12-24 18:11:33 +0800

    using op_api replace acl_op in embedding_bag_per_sample_weights_backward
    Co-authored-by: chenshuai<chenshuai81@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3806 merge SR1211 into master
    
    using op_api replace acl_op in embedding_bag_per_sample_weights_backward
    
    Created-by: culechan
    Commit-by: chenshuai
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/op-plugin!3806

---
+++ op_plugin/ops/opapi/EmbeddingBagPerSampleWeightsBackwardKernelNpuOpApi.cpp
@@ -0,0 +1,32 @@
+// Copyright (c) 2025 Huawei Technologies Co., Ltd
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License  (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "op_plugin/AclOpsInterface.h"
+
+namespace op_api {
+at::Tensor _embedding_bag_per_sample_weights_backward(const at::Tensor& grad, const at::Tensor& weight,
+                                                      const at::Tensor& indices, const at::Tensor& offsets,
+                                                      const at::Tensor& offset2bag, int64_t mode,
+                                                      int64_t padding_idx)
+{
+    at::Tensor grad_cpu = grad.to("cpu");
+    at::Tensor weight_cpu = weight.to("cpu");
+    at::Tensor indices_cpu = indices.to("cpu");
+    at::Tensor offsets_cpu = offsets.to("cpu");
+    at::Tensor offset2bag_cpu = offset2bag.to("cpu");
+    at::Tensor result = at::_embedding_bag_per_sample_weights_backward(grad_cpu, weight_cpu, indices_cpu,
+                                                                       offsets_cpu, offset2bag_cpu, mode, padding_idx);
+    result = at::native::sparse_to_dense(result);
+    result = result.to(indices.device());
+    return result;
+}
+} // namespace op_api
