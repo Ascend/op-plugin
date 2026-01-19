142429bf0bcfe7cd933b4e570bf8c457e3c432fd
xftxyz <pangxiaoyu@h-partners.com>
2026-01-19 22:12:28 +0800

    add op_api npu_reshape
    Co-authored-by: xftxyz<pangxiaoyu@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4064 merge master-2601170 into master
    
    add op_api npu_reshape
    
    Created-by: xftxyz
    Commit-by: xftxyz
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/op-plugin!4064

---
+++ op_plugin/ops/opapi/ReshapeKernelNpuOpApi.cpp
@@ -0,0 +1,52 @@
+// Copyright (c) 2023 Huawei Technologies Co., Ltd
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License  (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "op_plugin/OpApiInterface.h"
+#include "op_plugin/utils/OpAdapter.h"
+
+#include "torch_npu/csrc/framework/utils/InternalFormatOpAdapter.h"
+
+namespace op_api {
+using npu_preparation = at_npu::native::OpPreparation;
+using calcu_op_util = at_npu::native::CalcuOpUtil;
+
+at::Tensor& npu_reshape_out(
+    const at::Tensor& self,
+    at::IntArrayRef shape,
+    bool can_refresh,
+    at::Tensor& out)
+{
+    if (can_refresh) {
+        at_npu::native::StorageDescHelper::SetDesc(
+            out,
+            op_infer::array_to_small_vector(out.sizes()),
+            op_infer::array_to_small_vector(out.strides()));
+    } else {
+        at_npu::native::copy_d2d_by_memcpy(
+            out,
+            self,
+            at_npu::native::NPUNativeFunctions::get_storage_size(out));
+    }
+    return out;
+}
+
+at::Tensor npu_reshape(const at::Tensor& self, at::IntArrayRef shape, bool can_refresh)
+{
+    at::Tensor result = npu_preparation::apply_tensor(self, shape);
+    op_api::npu_reshape_out(self, shape, can_refresh, result);
+
+    return result;
+}
+} // namespace op_api
