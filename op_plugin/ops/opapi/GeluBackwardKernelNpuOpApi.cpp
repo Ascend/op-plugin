80eee28465ac824f8ac0e3ac816d6cb5ebba5fa9
shi-jiaxin9 <shijiaxin10@h-partners.com>
2026-01-31 19:54:42 +0800

    torch.nn.functional.gelu preserves api consistency1.
    Co-authored-by: shi-jiaxin9<shijiaxin10@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4152 merge 7.3.0 into 7.3.0
    
    torch.nn.functional.gelu preserves api consistency1.
    
    Created-by: shi-jiaxin9
    Commit-by: shi-jiaxin9
    Merged-by: ascend-robot
    Description: <!--  Thanks for sending a pull request! 
    -->
    
    **What type of PR is this?**
    > Uncomment only one ` /kind <>` line, hit enter to put that in a new line, and remove leading whitespaces from that line:
    >
    > /kind bug
    > /kind task
    > /kind feature
    
    
    **What does this PR do / why do we need it**:
    
    
    **Special notes for your reviewers**:
    
    
    
    
    See merge request: Ascend/op-plugin!4152

---
+++ op_plugin/ops/opapi/GeluBackwardKernelNpuOpApi.cpp
@@ -0,0 +1,39 @@
+// Copyright (c) 2026 Huawei Technologies Co., Ltd
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License  (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "op_plugin/OpApiInterface.h"
+#include "op_plugin/AclOpsInterface.h"
+#include "op_plugin/utils/op_api_common.h"
+#include "torch_npu/csrc/framework/utils/InternalFormatOpAdapter.h"
+
+namespace op_api {
+using npu_preparation = at_npu::native::OpPreparation;
+
+at::Tensor gelu_backward(const at::Tensor & grad_output, const at::Tensor & self, c10::string_view approximate)
+{
+    static auto gelu_sc = at_npu::native::env::CheckCompatibleImpl();
+    auto output_size_0 = op_infer::broadcast_ops_npu_output_size(grad_output, self);
+    auto output_dtype_0 = at::native::result_type(grad_output, self);
+    at::Tensor grad_input = npu_preparation::apply_tensor_without_format(output_size_0,
+                                                                         grad_output.options().dtype(output_dtype_0));
+    auto approximate_str = op_infer::npu_gelu_approximate_str(approximate);
+    auto approximate_ptr = const_cast<char *>(approximate_str.c_str());
+    if (!gelu_sc) {
+        DO_COMPATIBILITY(aclnnGeluBackward, acl_op::gelu_backward(grad_output, self, approximate));
+        EXEC_NPU_CMD(aclnnGeluBackward, grad_output, self, grad_input);
+    } else {
+        EXEC_NPU_CMD(aclnnGeluBackwardV2, grad_output, self, approximate_ptr, grad_input);
+    }
+    return grad_input;
+}
+}
--- test/test_base_ops/test_gelu_backward.py
+++ test/test_base_ops/test_gelu_backward.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch_npu
 
 from torch_npu.testing.testcase import TestCase, run_tests
+from torch_npu.testing.common_utils import SkipIfNotGteCANNVersion
 
 
 class TestGeluBackward(TestCase):
@@ -29,6 +30,7 @@ class TestGeluBackward(TestCase):
         res = input1.grad.to("cpu")
         return res.detach().numpy()
 
+    @SkipIfNotGteCANNVersion("9.0.0")
     def test_gelu_backward_float32_1(self):
         input1 = self.generate_single_data(0, 100, (4, 3, 1, 1), np.float32)
         cpu_input1 = copy.deepcopy(input1)
@@ -36,6 +38,7 @@ class TestGeluBackward(TestCase):
         npu_output = self.npu_op_exec(input1)
         self.assertRtolEqual(cpu_output, npu_output)
 
+    @SkipIfNotGteCANNVersion("9.0.0")
     def test_gelu_backward_float32_2(self):
         input1 = self.generate_single_data(0, 100, (15, 3, 1), np.float32)
         cpu_input1 = copy.deepcopy(input1)
@@ -43,6 +46,7 @@ class TestGeluBackward(TestCase):
         npu_output = self.npu_op_exec(input1)
         self.assertRtolEqual(cpu_output, npu_output)
 
+    @SkipIfNotGteCANNVersion("9.0.0")
     def test_gelu_backward_float32_3(self):
         input1 = self.generate_single_data(0, 100, (4, 4), np.float32)
         cpu_input1 = copy.deepcopy(input1)
@@ -50,6 +54,7 @@ class TestGeluBackward(TestCase):
         npu_output = self.npu_op_exec(input1)
         self.assertRtolEqual(cpu_output, npu_output)
 
+    @SkipIfNotGteCANNVersion("9.0.0")
     def test_gelu_backward_float16(self):
         input1 = self.generate_single_data(0, 100, (5, 10, 100), np.float16)
         cpu_input1 = input1.to(torch.float32)
@@ -58,7 +63,52 @@ class TestGeluBackward(TestCase):
         npu_output = self.npu_op_exec(input1)
         self.assertRtolEqual(cpu_output, npu_output)
 
+    def get_golden(self, grad_tensor, input_self_tensor, approximate="none"):
+        output = torch.ops.aten.gelu_backward(grad_tensor, input_self_tensor, approximate=approximate)
+        return output
+
+    @SkipIfNotGteCANNVersion("9.0.0")
+    def test_gelu_backward_float32_none(self):
+        shape = [100, 400]
+        mode = "none"
+        input_self_tensor = torch.rand(shape, dtype=torch.float32).npu()
+        grad_tensor = torch.rand(shape, dtype=torch.float32).npu()
+        output = torch.ops.aten.gelu_backward(grad_tensor, input_self_tensor, approximate=mode)
+        golden = self.get_golden(grad_tensor.cpu(), input_self_tensor.cpu(), mode)
+        self.assertRtolEqual(output, golden)
+
+    @SkipIfNotGteCANNVersion("9.0.0")
+    def test_gelu_backward_float32_tanh(self):
+        shape = [5, 10, 100]
+        mode = "none"
+        input_self_tensor = torch.rand(shape, dtype=torch.float32).npu()
+        grad_tensor = torch.rand(shape, dtype=torch.float32).npu()
+        output = torch.ops.aten.gelu_backward(grad_tensor, input_self_tensor, approximate=mode)
+        golden = self.get_golden(grad_tensor.cpu(), input_self_tensor.cpu(), mode)
+        self.assertRtolEqual(output, golden)
+
+    @SkipIfNotGteCANNVersion("9.0.0")
+    def test_gelu_backward_float16_none(self):
+        shape = [4, 4]
+        mode = "none"
+        input_self_tensor = torch.rand(shape, dtype=torch.float16).npu()
+        grad_tensor = torch.rand(shape, dtype=torch.float16).npu()
+        output = torch.ops.aten.gelu_backward(grad_tensor, input_self_tensor, approximate=mode)
+        golden = self.get_golden(grad_tensor.cpu(), input_self_tensor.cpu(), mode)
+        self.assertRtolEqual(output, golden)
+
+    @SkipIfNotGteCANNVersion("9.0.0")
+    def test_gelu_backward_float16_tanh(self):
+        shape = [4, 3, 1, 1]
+        mode = "tanh"
+        input_self_tensor = torch.rand(shape, dtype=torch.float16).npu()
+        grad_tensor = torch.rand(shape, dtype=torch.float16).npu()
+        output = torch.ops.aten.gelu_backward(grad_tensor, input_self_tensor, approximate=mode)
+        golden = self.get_golden(grad_tensor.cpu(), input_self_tensor.cpu(), mode)
+        self.assertRtolEqual(output, golden)
+
 
 if __name__ == "__main__":
     np.random.seed(20)
+    torch_npu.npu.use_compatible_impl(True)
     run_tests()
