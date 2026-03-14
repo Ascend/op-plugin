d2ae6566df44b1a5d67167d6fc372640c90ca449
libohao6 <libohao3@huawei.com>
2026-03-14 19:18:04 +0800

    修复npu_alltoallv_quant_gmm问题
    Co-authored-by: libohao6<libohao3@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4395 merge master into master
    
    修复npu_alltoallv_quant_gmm问题
    
    Created-by: libohao6
    Commit-by: libohao6
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [ ] 需求
    - [X] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    修复npu_alltoallv_quant_gmm问题
    
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    资料已变更，同步修改代码。
    
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    不涉及。
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    功能ST测试已验证通过。
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [X] 代码注释完备，正确记录错误日志
    - [X] 代码实现进行了返回值、空指针等校验
    - [X] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [X] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/op-plugin!4395

--- op_plugin/ops/opapi/AllToAllvQuantGmmNpuOpApi.cpp
+++ op_plugin/ops/opapi/AllToAllvQuantGmmNpuOpApi.cpp
@@ -24,16 +24,13 @@ static const int N1_DIM = 2;
 static const int ONE_DIM = 1;
 static const int TWO_DIM = 2;
 static const int THREE_DIM = 3;
+static const int PERTENSOR_QUANT_MODE = 1;
 // world_size
 const std::set<int> SUPPORT_WORLD_SIZE_LIST{2, 4, 8, 16, 32, 64, 128, 256};
 // input valid dtype
-const std::set<int64_t> SUPPORT_INPUT_DTYPE_LIST{
-    static_cast<int64_t>(c10_npu::DType::HIFLOAT8)
-};
+const std::set<int64_t> SUPPORT_INPUT_DTYPE_LIST{static_cast<int64_t>(c10_npu::DType::HIFLOAT8)};
 // scale valid dtype
-const std::set<int64_t> SUPPORT_SCALE_DTYPE_LIST{
-    static_cast<int64_t>(c10_npu::DType::FLOAT)
-};
+const std::set<int64_t> SUPPORT_SCALE_DTYPE_LIST{static_cast<int64_t>(c10_npu::DType::FLOAT)};
 
 std::tuple<at::Tensor, at::Tensor, at::Tensor> npu_alltoallv_quant_gmm(const at::Tensor &gmm_x,
     const at::Tensor &gmm_weight, const at::Tensor &gmm_x_scale, const at::Tensor &gmm_weight_scale,
@@ -41,9 +38,7 @@ std::tuple<at::Tensor, at::Tensor, at::Tensor> npu_alltoallv_quant_gmm(const at:
     int64_t gmm_y_dtype, const c10::optional<at::Tensor> &send_counts_tensor,
     const c10::optional<at::Tensor> &recv_counts_tensor, const c10::optional<at::Tensor> &mm_x,
     const c10::optional<at::Tensor> &mm_weight, const c10::optional<at::Tensor> &mm_x_scale,
-    const c10::optional<at::Tensor> &mm_weight_scale, const c10::optional<at::Tensor> &gmm_x_offset,
-    const c10::optional<at::Tensor> &gmm_weight_offset, const c10::optional<at::Tensor> &mm_x_offset,
-    const c10::optional<at::Tensor> &mm_weight_offset, c10::optional<int64_t> gmm_x_quant_mode,
+    const c10::optional<at::Tensor> &mm_weight_scale, c10::optional<int64_t> gmm_x_quant_mode,
     c10::optional<int64_t> gmm_weight_quant_mode, c10::optional<int64_t> mm_x_quant_mode,
     c10::optional<int64_t> mm_weight_quant_mode, bool permute_out_flag, c10::OptionalIntArrayRef group_size,
     c10::optional<int64_t> gmm_x_dtype, c10::optional<int64_t> gmm_weight_dtype,
@@ -55,56 +50,74 @@ std::tuple<at::Tensor, at::Tensor, at::Tensor> npu_alltoallv_quant_gmm(const at:
     TORCH_CHECK(gmm_x.defined(), "The input tensor gmm_x can not be None.", OPS_ERROR(ErrCode::PARAM));
     TORCH_CHECK(gmm_weight.defined(), "The input tensor gmm_weight can not be None.", OPS_ERROR(ErrCode::PARAM));
     TORCH_CHECK(gmm_x_scale.defined(), "The input tensor gmm_x_scale can not be None.", OPS_ERROR(ErrCode::PARAM));
-    TORCH_CHECK(gmm_weight_scale.defined(), "The input tensor gmm_weight_scale can not be None.", OPS_ERROR(ErrCode::PARAM));
+    TORCH_CHECK(
+        gmm_weight_scale.defined(), "The input tensor gmm_weight_scale can not be None.", OPS_ERROR(ErrCode::PARAM));
     // 校验空gmm_x，gmm_weight的shape
-    TORCH_CHECK(gmm_x.dim() == TWO_DIM,
-        "The gmm_x input of gmm is required to be 2D, but the actual gmm_x input is ",
-        gmm_x.dim(),
-        OPS_ERROR(ErrCode::PARAM));
+    TORCH_CHECK(
+        gmm_x.dim() == TWO_DIM, "The dim of gmm_x should be 2, but got ", gmm_x.dim(), OPS_ERROR(ErrCode::PARAM));
     TORCH_CHECK(gmm_weight.dim() == THREE_DIM,
-        "The gmm_weight input of gmm is required to be 3D, but the actual gmm_weight input is ",
+        "The dim of gmm_weight should be 3, but got ",
         gmm_weight.dim(),
         OPS_ERROR(ErrCode::PARAM));
-    TORCH_CHECK(gmm_x.size(1) == gmm_weight.size(1), "The K-axis in the two inputs of gmm must be equal, but in reality, the K-axis of gmm_x is ",
-        gmm_x.size(1), " and the K-axis of gmm_weight is ", gmm_weight.size(1), "." + OPS_ERROR(ErrCode::PARAM));
+    TORCH_CHECK(gmm_x.size(1) == gmm_weight.size(1),
+        "The K-axis in the two inputs of gmm must be equal, but in reality, the K-axis of gmm_x is ",
+        gmm_x.size(1),
+        " and the K-axis of gmm_weight is ",
+        gmm_weight.size(1),
+        "." + OPS_ERROR(ErrCode::PARAM));
     // 校验ep_world_size
     TORCH_CHECK(SUPPORT_WORLD_SIZE_LIST.find(ep_world_size) != SUPPORT_WORLD_SIZE_LIST.end(),
-        "The world_size should be in [2, 4, 8, 16, 32, 64, 128, 256], but the actual value is ", ep_world_size, OPS_ERROR(ErrCode::VALUE));
+        "The world_size should be in [2, 4, 8, 16, 32, 64, 128, 256], but the actual value is ",
+        ep_world_size,
+        OPS_ERROR(ErrCode::VALUE));
     // 校验scale的shape
-    TORCH_CHECK(gmm_x_scale.dim() == ONE_DIM && gmm_weight_scale.dim() == ONE_DIM,
-        "The input gmm_x_scale and gmm_weight_scale tensor shape are required to be 1D, but the actual gmm_x_scale input is, ", gmm_x_scale.dim(),
-        "the actual gmm_weight_scale input is", gmm_weight_scale.dim(), OPS_ERROR(ErrCode::PARAM));
+    TORCH_CHECK(gmm_x_scale.dim() == ONE_DIM,
+        "The dim of gmm_x_scale should be 1, but got ",
+        gmm_x_scale.dim(),
+        OPS_ERROR(ErrCode::PARAM));
+    TORCH_CHECK(gmm_weight_scale.dim() == ONE_DIM,
+        "The dim of gmm_weight_scale should be 1, but got ",
+        gmm_weight_scale.dim(),
+        OPS_ERROR(ErrCode::PARAM));
     // 校验scale是否为空tensor
     if (gmm_x_scale.dim() == ONE_DIM) {
-        TORCH_CHECK(gmm_x_scale.size(0) != 0, "The input tensor gmm_x_scale can not be empty tensor",
-                    OPS_ERROR(ErrCode::PARAM));
+        TORCH_CHECK(gmm_x_scale.size(0) != 0,
+            "The input tensor gmm_x_scale can not be empty tensor",
+            OPS_ERROR(ErrCode::PARAM));
     }
     if (gmm_weight_scale.dim() == ONE_DIM) {
-        TORCH_CHECK(gmm_weight_scale.size(0) != 0, "The input tensor gmm_weight_scale can not be empty tensor",
-                    OPS_ERROR(ErrCode::PARAM));
+        TORCH_CHECK(gmm_weight_scale.size(0) != 0,
+            "The input tensor gmm_weight_scale can not be empty tensor",
+            OPS_ERROR(ErrCode::PARAM));
     }
     // 校验input的dtype
     if (gmm_x_dtype.has_value()) {
         TORCH_CHECK(SUPPORT_INPUT_DTYPE_LIST.find(gmm_x_dtype.value()) != SUPPORT_INPUT_DTYPE_LIST.end(),
-                    "The optional parameter gmm_x_dtype only supports hifloat8, but now is ",
-                    op_plugin::utils::DTypeToString(gmm_x_dtype.value()), "." + OPS_ERROR(ErrCode::VALUE));
+            "The optional parameter gmm_x_dtype only supports hifloat8, but got ",
+            op_plugin::utils::DTypeToString(gmm_x_dtype.value()),
+            "." + OPS_ERROR(ErrCode::VALUE));
     }
     if (gmm_weight_dtype.has_value()) {
         TORCH_CHECK(SUPPORT_INPUT_DTYPE_LIST.find(gmm_weight_dtype.value()) != SUPPORT_INPUT_DTYPE_LIST.end(),
-                    "The optional parameter gmm_weight_dtype only supports hifloat8, but now is ",
-                    op_plugin::utils::DTypeToString(gmm_weight_dtype.value()), "." + OPS_ERROR(ErrCode::VALUE));
+            "The optional parameter gmm_weight_dtype only supports hifloat8, but got ",
+            op_plugin::utils::DTypeToString(gmm_weight_dtype.value()),
+            "." + OPS_ERROR(ErrCode::VALUE));
     }
     // 校验scale的dtype
     if (gmm_x_scale_dtype.has_value()) {
         TORCH_CHECK(SUPPORT_SCALE_DTYPE_LIST.find(gmm_x_scale_dtype.value()) != SUPPORT_SCALE_DTYPE_LIST.end(),
-                    "The optional parameter gmm_x_scale_dtype only supports float, but now is ",
-                    op_plugin::utils::DTypeToString(gmm_x_scale_dtype.value()), "." + OPS_ERROR(ErrCode::VALUE));
+            "The optional parameter gmm_x_scale_dtype only supports float, but got ",
+            op_plugin::utils::DTypeToString(gmm_x_scale_dtype.value()),
+            "." + OPS_ERROR(ErrCode::VALUE));
     }
     if (gmm_weight_scale_dtype.has_value()) {
         TORCH_CHECK(SUPPORT_SCALE_DTYPE_LIST.find(gmm_weight_scale_dtype.value()) != SUPPORT_SCALE_DTYPE_LIST.end(),
-                    "The optional parameter gmm_weight_scale_dtype only supports float, but now is ",
-                    op_plugin::utils::DTypeToString(gmm_weight_scale_dtype.value()), "." + OPS_ERROR(ErrCode::VALUE));
+            "The optional parameter gmm_weight_scale_dtype only supports float, but got ",
+            op_plugin::utils::DTypeToString(gmm_weight_scale_dtype.value()),
+            "." + OPS_ERROR(ErrCode::VALUE));
     }
+    // 校验groupSize传None
+    TORCH_CHECK(!group_size.has_value(), "The input group_size should be None.", OPS_ERROR(ErrCode::PARAM));
 
     at::Tensor mm_y{nullptr};
     at::Tensor permute_out{nullptr};
@@ -128,27 +141,30 @@ std::tuple<at::Tensor, at::Tensor, at::Tensor> npu_alltoallv_quant_gmm(const at:
             OPS_ERROR(ErrCode::PARAM));
         // 校验input的dtype
         if (mm_x_dtype.has_value()) {
+            TORCH_CHECK(mm_y_dtype.has_value(),
+                "mm_y_dtype must be provided when mm_x and mm_weight are present",
+                OPS_ERROR(ErrCode::PARAM));
             TORCH_CHECK(SUPPORT_INPUT_DTYPE_LIST.find(mm_x_dtype.value()) != SUPPORT_INPUT_DTYPE_LIST.end(),
-                "The optional parameter mm_x_dtype only supports hifloat8, but now is ",
+                "The optional parameter mm_x_dtype only supports hifloat8, but got ",
                 op_plugin::utils::DTypeToString(mm_x_dtype.value()),
                 "." + OPS_ERROR(ErrCode::VALUE));
         }
         if (mm_weight_dtype.has_value()) {
             TORCH_CHECK(SUPPORT_INPUT_DTYPE_LIST.find(mm_weight_dtype.value()) != SUPPORT_INPUT_DTYPE_LIST.end(),
-                "The optional parameter mm_weight_dtype only supports hifloat8, but now is ",
+                "The optional parameter mm_weight_dtype only supports hifloat8, but got ",
                 op_plugin::utils::DTypeToString(mm_weight_dtype.value()),
                 "." + OPS_ERROR(ErrCode::VALUE));
         }
         // 校验scale的dtype
         if (mm_x_scale_dtype.has_value()) {
             TORCH_CHECK(SUPPORT_SCALE_DTYPE_LIST.find(mm_x_scale_dtype.value()) != SUPPORT_SCALE_DTYPE_LIST.end(),
-                "The optional parameter mm_x_scale_dtype only supports float, but now is ",
+                "The optional parameter mm_x_scale_dtype only supports float, but got ",
                 op_plugin::utils::DTypeToString(mm_x_scale_dtype.value()),
                 "." + OPS_ERROR(ErrCode::VALUE));
         }
         if (mm_weight_scale_dtype.has_value()) {
             TORCH_CHECK(SUPPORT_SCALE_DTYPE_LIST.find(mm_weight_scale_dtype.value()) != SUPPORT_SCALE_DTYPE_LIST.end(),
-                "The optional parameter mm_weight_scale_dtype only supports float, but now is ",
+                "The optional parameter mm_weight_scale_dtype only supports float, but got ",
                 op_plugin::utils::DTypeToString(mm_weight_scale_dtype.value()),
                 "." + OPS_ERROR(ErrCode::VALUE));
         }
@@ -161,7 +177,12 @@ std::tuple<at::Tensor, at::Tensor, at::Tensor> npu_alltoallv_quant_gmm(const at:
     if (permute_out_flag) {
         int64_t h1 = gmm_x.size(1);
         // 推导输出permute_out的shape
-        aclDataType permute_out_acltype = c10_npu::GetAclDataType(gmm_x_dtype.value());
+        aclDataType permute_out_acltype;
+        if (gmm_x_dtype.has_value()) {
+            permute_out_acltype = c10_npu::GetAclDataType(gmm_x_dtype.value());
+        } else {
+            permute_out_acltype = at_npu::native::OpPreparation::convert_to_acl_data_type(gmm_x.scalar_type());
+        }
         at::ScalarType permute_out_scalar_dtype =
             at_npu::native::OpPreparation::convert_to_scalar_type(permute_out_acltype);
         permute_out =
@@ -185,15 +206,11 @@ std::tuple<at::Tensor, at::Tensor, at::Tensor> npu_alltoallv_quant_gmm(const at:
 
     const at::Tensor &mm_x_scale_real = mm_x_scale.value_or(at::Tensor());
     const at::Tensor &mm_weight_scale_real = mm_weight_scale.value_or(at::Tensor());
-    const at::Tensor &gmm_x_offset_real = gmm_x_offset.value_or(at::Tensor());
-    const at::Tensor &gmm_weight_offset_real = gmm_weight_offset.value_or(at::Tensor());
-    const at::Tensor &mm_x_offset_real = mm_x_offset.value_or(at::Tensor());
-    const at::Tensor &mm_weight_offset_real = mm_weight_offset.value_or(at::Tensor());
 
-    int64_t gmm_x_quant_mode_real = gmm_x_quant_mode.value();
-    int64_t gmm_weight_quant_mode_real = gmm_weight_quant_mode.value();
-    int64_t mm_x_quant_mode_real = mm_x_quant_mode.value();
-    int64_t mm_weight_quant_mode_real = mm_weight_quant_mode.value();
+    int64_t gmm_x_quant_mode_real = gmm_x_quant_mode.value_or(PERTENSOR_QUANT_MODE);
+    int64_t gmm_weight_quant_mode_real = gmm_weight_quant_mode.value_or(PERTENSOR_QUANT_MODE);
+    int64_t mm_x_quant_mode_real = mm_x_quant_mode.value_or(PERTENSOR_QUANT_MODE);
+    int64_t mm_weight_quant_mode_real = mm_weight_quant_mode.value_or(PERTENSOR_QUANT_MODE);
     int64_t group_sizes = 0;
     bool transposeGmmWeight = false;
     bool transposeMmWeight = false;
@@ -208,16 +225,12 @@ std::tuple<at::Tensor, at::Tensor, at::Tensor> npu_alltoallv_quant_gmm(const at:
         gmm_weight_wrapper,
         gmm_x_scale,
         gmm_weight_scale,
-        gmm_x_offset_real,
-        gmm_weight_offset_real,
         send_count_tensor_real,
         recv_count_tensor_real,
         mm_x_wrapper,
         mm_weight_wrapper,
         mm_x_scale_real,
         mm_weight_scale_real,
-        mm_x_offset_real,
-        mm_weight_offset_real,
         gmm_x_quant_mode_real,
         gmm_weight_quant_mode_real,
         mm_x_quant_mode_real,
