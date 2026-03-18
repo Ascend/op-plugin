b747b2a3b1a2ba0bf56b2f6ea5c53ad2385029a2
SCh_zx <1325467101@qq.com>
2026-03-18 18:41:14 +0800

    revert [fix] add ops isclose check
    Co-authored-by: SCh_zx<1325467101@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4479 merge master into master
    
    revert [fix] add ops isclose check
    
    Created-by: SCh_zx
    Commit-by: SCh_zx
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [ ] 需求
    - [ ] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    CI不过
    
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [ ] 代码注释完备，正确记录错误日志
    - [ ] 代码实现进行了返回值、空指针等校验
    - [ ] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [ ] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/op-plugin!4479

--- op_plugin/utils/KernelNpuOutputSize.cpp
+++ op_plugin/utils/KernelNpuOutputSize.cpp
@@ -781,16 +781,6 @@ c10::SmallVector<int64_t, SIZE> index_select_npu_output_size(const at::Tensor &s
     return outputSize;
 }
 
-c10::IntArrayRef isclose_output_size(const at::Tensor& self, const at::Tensor& other, float rtol,
-                                     float atol, bool equal_nan)
-{
-    TORCH_CHECK(
-        rtol >= 0, "rtol must be greater than or equal to zero, but got ", rtol);
-    TORCH_CHECK(
-        atol >= 0, "atol must be greater than or equal to zero, but got ", atol);
-    return at::isFloatingType(self.scalar_type()) && equal_nan ? self.sizes() : broadcast_ops_npu_output_size(self, other);
-}
-
 c10::SmallVector<int64_t, SIZE> nnpack_spatial_convolution_npu_output_size(const at::Tensor &input,
                                                                            const at::Tensor &weight,
                                                                            c10::IntArrayRef padding,
