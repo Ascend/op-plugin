79c2f454889691bc620e8bfc94d69c997f9b53e0
chujinjin <chujinjin52@huawei.com>
2026-04-16 17:32:48 +0800

    Revert "EXEC_NPU_CMD_EXT解耦，保持ABI兼容"
    Co-authored-by: chujinjin<chujinjin52@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4734 merge revert_npu_cpp_extention_for_26 into 26.0.0
    
    Revert "EXEC_NPU_CMD_EXT解耦，保持ABI兼容"
    
    Created-by: chujinjin
    Commit-by: chujinjin
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [ ] 需求
    - [x] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    
    原有头文件将内部组件的头文件暴露给外部，EXEC_NPU_CMD_EXT解耦导致部分将无关头文件去除，导致外部需要的头文件缺失。
    
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    
    不涉及
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    
    不涉及
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    
    编译examples里面的aclnn_extention，编译执行通过
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/op-plugin!4734

--- op_plugin/include/npu_cpp_extension.h
+++ op_plugin/include/npu_cpp_extension.h
@@ -16,6 +16,6 @@
 #ifndef OP_PULGIN_INCLUDE_NPU_CPU_EXTENSION
 #define OP_PULGIN_INCLUDE_NPU_CPU_EXTENSION
 
-#include "op_plugin/utils/npu_op_command.h"
+#include "op_plugin/utils/op_api_common_base.h"
 
 #endif  // OP_PULGIN_INCLUDE_NPU_CPU_EXTENSION
