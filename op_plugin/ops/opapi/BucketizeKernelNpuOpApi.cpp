2a39284c6df6785e542b993e91e37a3a6d2dd454
ascend-robot <zhongyuanke@huawei.com>
2026-04-29 19:50:24 +0800

    [feat]Bucketize新增Aicore通路支持
    Co-authored-by: 何金鹏<hejinpeng5@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4711 merge master into master
    
    [feat]Bucketize新增Aicore通路支持
    
    Created-by: jinpenghe
    Commit-by: 何金鹏
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [x] 需求
    - [ ] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    芯片版本大于等于950， 输入x和boundaries不为double情况下走aclnnBucketize
    # 【资料变更】
    不涉及
    # 【接口变更】
    不涉及
    # 【功能验证】
    - 测试用例：
    values = torch.tensor(  [
            [-5, 10000, 1],
            [150, 10, 2],
            [5, 100, 3]
        ], dtype=torch.float32, device=device)
    bins = torch.tensor([0.0, 10.0, 100.0], dtype=torch.float32, device=device)
    - 结果：
    function operator())
    输入值 : tensor([[-5.0000e+00,  1.0000e+04,  1.0000e+00],
            [ 1.5000e+02,  1.0000e+01,  2.0000e+00],
            [ 5.0000e+00,  1.0000e+02,  3.0000e+00]], device='npu:0')
    分桶边界 : tensor([  0.,  10., 100.], device='npu:0')
    bucketize 结果 : tensor([[0, 3, 1],
            [3, 1, 1],
            [1, 2, 1]], device='npu:0')
    - 通路结果：
    ![image.png](https://raw.gitcode.com/user-images/assets/7403085/6d7b35ee-85eb-49c3-8b0a-d3486e1353f2/image.png 'image.png')
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/op-plugin!4711

--- op_plugin/ops/opapi/BucketizeKernelNpuOpApi.cpp
+++ op_plugin/ops/opapi/BucketizeKernelNpuOpApi.cpp
@@ -23,7 +23,19 @@ at::Tensor bucketize(const at::Tensor& self, const at::Tensor& boundaries, bool 
 {
     TORCH_CHECK(boundaries.dim() == 1, "boundaries tensor must be 1 dimension, but got dim(",
         boundaries.dim(), ")" + OPS_ERROR(ErrCode::PARAM));
-    return op_api::searchsorted(boundaries, self, out_int32, right, c10::nullopt, c10::nullopt);
+
+    static bool isRegBaseSoc = c10_npu::GetSocVersion() >= c10_npu::SocVersion::Ascend950;
+    bool typeSupport = self.dtype() != at::kDouble && boundaries.dtype() != at::kDouble;
+    static const bool is_bucketize_available = check_aclnn_kernel_available("aclnnBucketize");
+    if (isRegBaseSoc && typeSupport && is_bucketize_available) {
+        at::ScalarType expectedType = out_int32 ? at::kInt : at::kLong;
+        at::TensorOptions opts = self.options().dtype(expectedType);
+        at::Tensor result = at_npu::native::OpPreparation::apply_tensor_without_format(self.sizes(), opts);
+        EXEC_NPU_CMD(aclnnBucketize, self, boundaries, out_int32, right, result);
+        return result;
+    } else {
+        return op_api::searchsorted(boundaries, self, out_int32, right, c10::nullopt, c10::nullopt);
+    }
 }
 
 at::Tensor bucketize(const at::Scalar& self, const at::Tensor& boundaries, bool out_int32, bool right)
@@ -42,6 +54,19 @@ at::Tensor &bucketize_out(
 {
     TORCH_CHECK(boundaries.dim() == 1, "boundaries tensor must be 1 dimension, but got dim(", boundaries.dim(),
         ")" + OPS_ERROR(ErrCode::PARAM));
-    return op_api::searchsorted_out(boundaries, self, out_int32, right, c10::nullopt, c10::nullopt, out);
+
+    static bool isRegBaseSoc = c10_npu::GetSocVersion() >= c10_npu::SocVersion::Ascend950;
+    bool typeSupport = self.dtype() != at::kDouble && boundaries.dtype() != at::kDouble;
+    static const bool is_bucketize_available = check_aclnn_kernel_available("aclnnBucketize");
+    if (isRegBaseSoc && typeSupport && is_bucketize_available) {
+        at_npu::native::OpPreparation::check_tensor({self, boundaries},
+                                                    out,
+                                                    out.scalar_type(),
+                                                    self.sizes());
+        EXEC_NPU_CMD(aclnnBucketize, self, boundaries, out_int32, right, out);
+        return out;
+    } else {
+        return op_api::searchsorted_out(boundaries, self, out_int32, right, c10::nullopt, c10::nullopt, out);
+    }
 }
 }
--- test/test_base_ops/test_bucketize.py
+++ test/test_base_ops/test_bucketize.py
@@ -8,9 +8,18 @@ from torch_npu.testing.testcase import TestCase, run_tests
 
 class TestBucketize(TestCase):
     
-    def test_bucketize(self):
-        v = torch.tensor([[3, 6, 9], [3, 6, 9]])
-        boundaries = torch.tensor([1, 3, 5, 7, 9])
+    def test_bucketize_aclnn_search(self):
+        v = torch.tensor([[3, 6, 9], [3, 6, 9]], dtype=torch.float64)
+        boundaries = torch.tensor([1, 3, 5, 7, 9], dtype=torch.float64)
+
+        cpu_output = torch.bucketize(v, boundaries)
+        npu_output = torch.bucketize(v.npu(), boundaries.npu())
+
+        self.assertRtolEqual(cpu_output, npu_output)
+    
+    def test_bucketize_aclnn_bucketize(self):
+        v = torch.tensor([[3, 6, 9], [3, 6, 9]], dtype=torch.int32)
+        boundaries = torch.tensor([1, 3, 5, 7, 9], dtype=torch.int32)
 
         cpu_output = torch.bucketize(v, boundaries)
         npu_output = torch.bucketize(v.npu(), boundaries.npu())
