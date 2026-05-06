fb5a024193abd6f597e2778e0916bd61c7faa0da
l1919_snow <lixinyu68@huawei.com>
2026-05-06 15:54:26 +0800

    删除test_add_custom.py里求反向梯度相关
    Co-authored-by: l00881990<lixinyu68@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4879 merge master-lxy into master
    
    删除test_add_custom.py里求反向梯度相关
    
    Created-by: l1919_snow
    Commit-by: l00881990
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [ ] 需求
    - [x] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    删除 examples\cpp_extension_base\test\test_add_custom.py test_add_custom.py文件中的所有反向求梯度
    
    # 【资料变更】
    不涉及
    
    # 【接口变更】
    不涉及
    
    # 【功能验证】
    
    ![image.png](https://raw.gitcode.com/user-images/assets/7403085/ff3f5620-6428-47c2-bddb-c5a058fb8cea/image.png 'image.png')
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x]代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/op-plugin!4879

--- examples/cpp_extension_base/test/test_add_custom.py
+++ examples/cpp_extension_base/test/test_add_custom.py
@@ -13,45 +13,29 @@ class TestCustomAdd(TestCase):
         x_cpu = torch.randn([8, 2048], dtype=torch.float16)
         y_cpu = torch.randn([8, 2048], dtype=torch.float16)
         x_npu, y_npu = copy.deepcopy(x_cpu).npu(), copy.deepcopy(y_cpu).npu()
-        x_cpu.requires_grad = True
-        y_cpu.requires_grad = True
-        x_npu.requires_grad = True
-        y_npu.requires_grad = True
-
+       
         # calculate on npu
         output = torch.ops.cpp_extension_base.add_custom(x_npu, y_npu)
-        output.backward(output)
 
         # calculate on cpu
         cpuout = torch.add(x_cpu, y_cpu)
-        cpuout.backward(cpuout)
 
         # compare result
         self.assertRtolEqual(output, cpuout)
-        self.assertRtolEqual(x_npu.grad, x_cpu.grad)
-        self.assertRtolEqual(y_npu.grad, y_cpu.grad)
-
+  
     def test_add_custom_ops(self):
         x_cpu = torch.randn([8, 2048], dtype=torch.float16)
         y_cpu = torch.randn([8, 2048], dtype=torch.float16)
         x_npu, y_npu = copy.deepcopy(x_cpu).npu(), copy.deepcopy(y_cpu).npu()
-        x_cpu.requires_grad = True
-        y_cpu.requires_grad = True
-        x_npu.requires_grad = True
-        y_npu.requires_grad = True
 
         # calculate on npu
         output = cpp_extension_base.ops.add_custom(x_npu, y_npu)
-        output.backward(output)
-
+   
         # calculate on cpu
         cpuout = torch.add(x_cpu, y_cpu)
-        cpuout.backward(cpuout)
 
         # compare result
         self.assertRtolEqual(output, cpuout)
-        self.assertRtolEqual(x_npu.grad, x_cpu.grad)
-        self.assertRtolEqual(y_npu.grad, y_cpu.grad)
 
 if __name__ == "__main__":
     run_tests()
