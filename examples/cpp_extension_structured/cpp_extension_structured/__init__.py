f0302065083bbb1b31ce0cffea2522f59ebfcc7f
l1919_snow <lixinyu68@huawei.com>
2026-05-11 09:49:42 +0800

    修改cpp_extension_structured的setup.py文件
    Co-authored-by: l00881990<lixinyu68@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4900 merge op-master-cpp-structure into master
    
    修改cpp_extension_structured的setup.py文件
    
    Created-by: l1919_snow
    Commit-by: l00881990
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [ ] 需求
    - [x] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    修改cpp_extension_structured的setup.py文件，
    
    # 【资料变更】
    不涉及
    
    # 【接口变更】
    不涉及
    
    # 【功能验证】
    1、cpp_extension 的whl 包成功编出
    op-plugin\examples\cpp_extension_structured\test\test_npu_fast_gelu_custom.py  测试脚本成功执行
    ![image.png](https://raw.gitcode.com/user-images/assets/7403085/c38bbadf-9d21-44a6-b036-5de40c4b3bd3/image.png 'image.png')
    
    
    
    
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    -  [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
      
    
    See merge request: Ascend/op-plugin!4900

--- examples/cpp_extension_structured/cpp_extension_structured/__init__.py
+++ examples/cpp_extension_structured/cpp_extension_structured/__init__.py
@@ -1,6 +1,3 @@
-from ._load import _load_opextension_so
-
-_load_opextension_so()
 import pathlib
 import torch
 
--- examples/cpp_extension_structured/setup.py
+++ examples/cpp_extension_structured/setup.py
@@ -1,34 +1,94 @@
 import os
-import glob
+import sys
 import sysconfig
-from distutils.errors import CompileError
-from distutils.spawn import find_executable
+import subprocess
+from setuptools import setup, Extension, find_packages
 import torch
 import torch_npu
-import torch.utils.cpp_extension as cpp_extension
-from setuptools import setup, Extension, find_packages
-from setuptools.command.build_ext import build_ext
+from torch.utils.cpp_extension import BuildExtension, CppExtension
 
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-source_files = glob.glob(os.path.join(BASE_DIR, "csrc", "*.asc"), recursive=True)
+# Get PyTorch version
+PYTORCH_VERSION = subprocess.check_output([sys.executable, '-c', 'import torch; print(torch.__version__.split("+")[0])']).decode('utf-8').strip()
+version_parts = PYTORCH_VERSION.split('.')
+PYTORCH_VERSION_DIR = f"v{version_parts[0]}r{version_parts[1]}"
+
+# Set os env
+os.environ["PYTORCH_VERSION"] = PYTORCH_VERSION
+os.environ["PYTORCH_CUSTOM_DERIVATIVES_PATH"] = os.path.join(os.path.dirname(__file__), f"op-plugin/config/{PYTORCH_VERSION_DIR}/derivatives.yaml")
+os.environ["ACNN_EXTENSION_PATH"] = os.path.dirname(__file__)
+os.environ["ACNN_EXTENSION_SWITCH"] = "TRUE"
+
+
+# Get all source files that need to be compiled
+def get_sources():
+    sources = []
+    # 添加csrc/aten目录下的源文件
+    aten_dir = os.path.join(os.path.dirname(__file__), "torch_npu/csrc/aten")
+    if os.path.exists(aten_dir):
+        for root, _, files in os.walk(aten_dir):
+            for file in files:
+                if file.endswith(".cpp") or file.endswith(".cc"):
+                    sources.append(os.path.join(root, file))
+    # 添加op-plugin/ops目录下的源文件
+    ops_dir = os.path.join(os.path.dirname(__file__), "op_plugin")
+    if os.path.exists(ops_dir):
+        for root, _, files in os.walk(ops_dir):
+            for file in files:
+                if file.endswith(".cpp") or file.endswith(".cc"):
+                    sources.append(os.path.join(root, file))
+
+    BUILD_EXCLUDE_LIST = [f"{aten_dir}/VariableTypeEverything.cpp",
+        f"{aten_dir}/ADInplaceOrViewTypeEverything.cpp",
+        f"{aten_dir}/python_functionsEverything.cpp",
+        f"{aten_dir}/RegisterFunctionalizationEverything.cpp"]
+
+    sources_new = [cur_file for cur_file in sources if cur_file not in BUILD_EXCLUDE_LIST]
+
+    return sources_new
+
+
+# Get all needed head files
+def get_include_dirs():
+    PYTORCH_NPU_INSTALL_PATH = os.path.dirname(os.path.realpath(torch_npu.__file__))
+
+    include_dirs = []
+    # Add csrc/aten path
+    aten_dir = os.path.join(os.path.dirname(__file__), "torch_npu/csrc/aten")
+    if os.path.exists(aten_dir):
+        include_dirs.append(aten_dir)
+    # Add op-plugin path
+    ops_dir = os.path.join(os.path.dirname(__file__), "op_plugin")
+    if os.path.exists(ops_dir):
+        include_dirs.append(ops_dir)
+
+    base_dir = os.path.dirname(__file__)
+    if os.path.exists(base_dir):
+        include_dirs.append(base_dir)
+
+    torch_npu_dir = PYTORCH_NPU_INSTALL_PATH
+    include_dirs.append(os.path.join(torch_npu_dir, 'include'))
+    include_dirs.append(os.path.join(torch_npu_dir, 'include', 'third_party', 'acl', 'inc'))
+    include_dirs.append(os.path.join(torch_npu_dir, 'include', 'third_party', 'hccl', 'inc'))
+    include_dirs.append(os.path.join(torch_npu_dir, 'include', 'third_party', 'op-plugin'))
+    return include_dirs
+
+
+def get_compile_args():
+    compile_args = ["-std=c++17"]
+    # for Windows
+    if sys.platform == "win32":
+        compile_args.append("/MD")
+    # for Linux
+    elif sys.platform == "linux":
+        compile_args.append("-fPIC")
+    return compile_args
 
 
 def get_dependency_paths():
-    python_include = sysconfig.get_config_var("INCLUDEPY")
     python_lib = sysconfig.get_config_var("LIBDIR")
-
-    torch_include_paths = cpp_extension.include_paths()
     torch_lib = os.path.join(os.path.dirname(torch.__file__), "lib")
-
     torch_npu_path = os.path.dirname(torch_npu.__file__)
-    torch_npu_include = os.path.join(torch_npu_path, "include")
     torch_npu_lib = os.path.join(torch_npu_path, "lib")
-
-    all_include_paths = list([
-        *torch_include_paths,
-        python_include,
-        torch_npu_include,
-    ])
 
     all_libs = list([
         python_lib,
@@ -37,65 +97,45 @@ def get_dependency_paths():
     ])
 
     return {
-        "all_includes": all_include_paths,
         "all_libs": all_libs
     }
 
 
-class AscendBuildExtension(build_ext):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+def get_link_args():
+    link_args = []
 
-    def _check_bisheng_compiler(self):
-        bisheng_compiler = find_executable('bisheng')
-        if not bisheng_compiler:
-            raise RuntimeError("bisheng command not found!")
+    link_args.append("-ltorch_npu")
+    link_args.append("-ltorch")
+    link_args.append("-lc10")
 
-    def build_extension(self, ext):
-        self._check_bisheng_compiler()
-        dep_paths = get_dependency_paths()
+    dep_paths = get_dependency_paths()
+    for lib_dir in dep_paths["all_libs"]:
+        link_args.append(f"-L{lib_dir}")
+    return link_args
 
-        ext_fullpath = self.get_ext_fullpath(ext.name)
-        os.makedirs(os.path.dirname(ext_fullpath), exist_ok=True)
-
-        use_cxx11_abi = torch._C._GLIBCXX_USE_CXX11_ABI
-        abi_value = "1" if use_cxx11_abi else "0"
-
-        compile_cmd = [
-            "bisheng",
-            "-x", "asc",
-            "--npu-arch=dav-2201",
-            "-shared",
-            "-fPIC",
-            "-std=c++17",
-            f"-D_GLIBCXX_USE_CXX11_ABI={abi_value}",
-            "-ltorch_npu", "-ltorch", "-lc10",
-            *ext.sources,
-            "-o", ext_fullpath,
-        ]
-
-        for include_dir in dep_paths["all_includes"]:
-            compile_cmd.append(f"-I{include_dir}")
-
-        for lib_dir in dep_paths["all_libs"]:
-            compile_cmd.append(f"-L{lib_dir}")
-
-        try:
-            self.spawn(compile_cmd)
-        except Exception as e:
-            raise CompileError(f"{str(e)}") from e
-
-
-your_ext = Extension(
-    name="cpp_extension_acs.custom_ops_lib",
-    sources=source_files,
-    language="asc",
-)
+# Set extension configuration
+# Use CppExtension in PyTorch instead of the standard Extension for better PyTorch adaption
+extensions = [
+    CppExtension(
+        "cpp_extension_structured.custom_cpp_extension_structured_lib",
+        sources=get_sources(),
+        include_dirs=get_include_dirs(),
+        extra_compile_args=get_compile_args(),
+        extra_link_args=get_link_args(),
+    )
+]
 
 setup(
-    name="cpp_extension_acs",
-    version="0.1",
-    ext_modules=[your_ext],
-    packages=find_packages(),
-    cmdclass={"build_ext": AscendBuildExtension},
+    name="cpp_extension_structured",
+    version="1.0.0",
+    description="ACLNN extension for PyTorch",
+    ext_modules=extensions,
+    cmdclass={
+        'build_ext': BuildExtension,
+    },
+    zip_safe=False,
+    install_requires=[
+        f"torch=={PYTORCH_VERSION}"
+    ],
+    packages=find_packages()
 )
