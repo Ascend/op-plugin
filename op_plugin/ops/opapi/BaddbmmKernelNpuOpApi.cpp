d9c8fb04f4dfed684940182add0634bd5cbef7f7
HuangKun8682 <huangkun61@huawei.com>
2026-06-09 18:15:06 +0800

    baddbmm接口支持out_dtype参数
    Co-authored-by: HuangKun8682<huangkun61@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !5090 merge baddbmm_16in32out into master
    
    baddbmm接口支持out_dtype参数
    
    Created-by: HuangKun8682
    Commit-by: HuangKun8682
    Merged-by: ascend-robot
    Description: # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    保持接口能力与torch官方能力一致https://docs.pytorch.org/docs/2.12/generated/torch.baddbmm.html
    
    [#142](https://gitcode.com/Ascend/op-plugin/issues/142) 
    
    - [x] 需求
    - [ ] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    对于baddbmm算子新增可选的outdtype参数支持16进32出功能
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    不涉及
    
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    为 baddbmm 算子新增一个可选的 output_dtype 参数，修改out属性参数
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    接口自验证截图:
    ![image.png](https://raw.gitcode.com/user-images/assets/7403085/b041f3d7-2861-4a3b-abd8-6a49a5c858fd/image.png 'image.png')
    ![image.png](https://raw.gitcode.com/user-images/assets/7403085/9a7a428f-25ff-45a9-b1e2-6d8d8a93fd57/image.png 'image.png')
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/op-plugin!5090

--- op_plugin/ops/opapi/BaddbmmKernelNpuOpApi.cpp
+++ op_plugin/ops/opapi/BaddbmmKernelNpuOpApi.cpp
@@ -32,12 +32,36 @@ at::Tensor &baddbmm_out(const at::Tensor &self, const at::Tensor &batch1, const 
     return out;
 }
 
+at::Tensor &baddbmm_out(const at::Tensor &self, const at::Tensor &batch1, const at::Tensor &batch2,
+                        const at::ScalarType output_dtype, const at::Scalar &beta, const at::Scalar &alpha,
+                        at::Tensor &out)
+{
+    DO_COMPATIBILITY(aclnnBaddbmm, acl_op::baddbmm_out(self, batch1, batch2, beta, alpha, out));
+    auto output_size = op_infer::baddbmm_npu_output_size(batch1, batch2);
+    npu_preparation::check_tensor({self, batch1, batch2}, out, output_dtype, output_size);
+    int8_t cube_math_type = op_plugin::utils::get_cube_math_type_with_passthrough();
+    EXEC_NPU_CMD(aclnnBaddbmm, self, batch1, batch2, beta, alpha, out, cube_math_type);
+    return out;
+}
+
 at::Tensor baddbmm(const at::Tensor &self, const at::Tensor &batch1, const at::Tensor &batch2, const at::Scalar &beta,
                    const at::Scalar &alpha)
 {
     DO_COMPATIBILITY(aclnnBaddbmm, acl_op::baddbmm(self, batch1, batch2, beta, alpha));
     auto output_size = op_infer::baddbmm_npu_output_size(batch1, batch2);
     at::Tensor result = npu_preparation::apply_tensor_without_format(output_size, self.options());
+    int8_t cube_math_type = op_plugin::utils::get_cube_math_type_with_passthrough();
+    EXEC_NPU_CMD(aclnnBaddbmm, self, batch1, batch2, beta, alpha, result, cube_math_type);
+    FLOP_COUNT(FlopCounter::baddbmm_flop, batch1, batch2);
+    return result;
+}
+
+at::Tensor baddbmm(const at::Tensor &self, const at::Tensor &batch1, const at::Tensor &batch2,
+                   const at::ScalarType output_dtype, const at::Scalar &beta, const at::Scalar &alpha)
+{
+    DO_COMPATIBILITY(aclnnBaddbmm, acl_op::baddbmm(self, batch1, batch2, beta, alpha));
+    auto output_size = op_infer::baddbmm_npu_output_size(batch1, batch2);
+    at::Tensor result = npu_preparation::apply_tensor_without_format(output_size, self.options().dtype(output_dtype));
     int8_t cube_math_type = op_plugin::utils::get_cube_math_type_with_passthrough();
     EXEC_NPU_CMD(aclnnBaddbmm, self, batch1, batch2, beta, alpha, result, cube_math_type);
     FLOP_COUNT(FlopCounter::baddbmm_flop, batch1, batch2);
