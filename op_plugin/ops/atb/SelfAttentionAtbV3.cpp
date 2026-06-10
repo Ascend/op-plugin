8dfc35b204162d564b6272c7cf8b30f8d5b82622
gcw_zvndY5nI <chenli222@huawei.com>
2026-06-10 20:11:57 +0800

    flashattention支持masktype
    Co-authored-by: 陈立<2825128415@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !5136 merge 26.1.0-flash into 26.1.0
    
    flashattention支持masktype
    
    Created-by: gcw_zvndY5nI
    Commit-by: 陈立
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    https://gitcode.com/Ascend/pytorch/issues/2172
    - [x] 需求
    - [ ] 问题单
    - [x] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    新增op_plugin/ops/atb/SelfAttentionAtbV3.cpp并将接口注册到torch_npu
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    仅内部使用，不涉及
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    仅内部使用，不涉及
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    ![image.png](https://raw.gitcode.com/user-images/assets/7403085/7f22df16-4a14-46b2-bc95-556dee2a5dbe/image.png 'image.png')
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/op-plugin!5136

---
+++ op_plugin/ops/atb/SelfAttentionAtbV3.cpp
@@ -0,0 +1,78 @@
+// Copyright (c) 2026 Huawei Technologies Co., Ltd
+// All rights reserved.
+//
+// Licensed under the BSD 3-Clause License  (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include "op_plugin/OpApiInterface.h"
+#include "op_plugin/utils/custom_functions/atb/AtbCommon.h"
+#include <acl/acl.h>
+using namespace std;
+namespace atb {
+using SelfAttentionParam = atb::infer::SelfAttentionParam;
+void _npu_flash_attention_v3(const at::Tensor &query, const at::Tensor &key, const at::Tensor &value, const at::Tensor &mask, const at::Tensor &seq_len, const double scale_value, const int64_t num_heads, const int64_t num_kv_heads, const c10::optional<int64_t> mask_type, at::Tensor &out)
+{
+    const c10::OptionalDeviceGuard device_guard(device_of(query));
+    int64_t mtype = mask_type.has_value() ? mask_type.value() : SelfAttentionParam::MASK_TYPE_NORM_COMPRESS;
+    TORCH_CHECK(mtype == SelfAttentionParam::MASK_TYPE_NORM_COMPRESS,
+                "mask_type only supports MASK_TYPE_NORM_COMPRESS, but got: ", mtype);
+    OpParamCache<SelfAttentionParam>& selfAttentionParamCache = OpParamCache<SelfAttentionParam>::getInstance();
+    SelfAttentionParam selfattentionparam;
+
+    selfattentionparam.calcType = SelfAttentionParam::PA_ENCODER;
+    selfattentionparam.kernelType = SelfAttentionParam::KERNELTYPE_DEFAULT;
+    selfattentionparam.clampType = SelfAttentionParam::CLAMP_TYPE_UNDEFINED;
+    selfattentionparam.maskType = static_cast<SelfAttentionParam::MaskType>(mtype);
+    selfattentionparam.kvcacheCfg = SelfAttentionParam::K_CACHE_V_CACHE;
+    selfattentionparam.scaleType = SelfAttentionParam::SCALE_TYPE_TOR;
+    selfattentionparam.quantType = SelfAttentionParam::TYPE_QUANT_UNDEFINED;
+    selfattentionparam.cacheType = SelfAttentionParam::CACHE_TYPE_NORM;
+    selfattentionparam.outDataType = ACL_DT_UNDEFINED;
+    selfattentionparam.headNum = num_heads;
+    selfattentionparam.kvHeadNum = num_kv_heads;
+    selfattentionparam.qScale = 1;
+    selfattentionparam.qkScale = scale_value;
+    selfattentionparam.batchRunStatusEnable = false;
+    selfattentionparam.isTriuMask = 1;
+    selfattentionparam.clampMin = 0;
+    selfattentionparam.clampMax = 0;
+    selfattentionparam.inputLayout = atb::infer::TYPE_BSND;
+    selfattentionparam.mlaVHeadSize = 0;
+    selfattentionparam.windowSize = 0;
+
+    ParamSetter parametter;
+    parametter.Input(query, true)
+        .Input(key, true)
+        .Input(value, true)
+        .Input(mask)
+        .Input(seq_len, true)
+        .Output(out);
+
+    auto opSelfattention = selfAttentionParamCache.getOperation(selfattentionparam, "SelfAttentionOperation");
+    RunAtbCmd(opSelfattention, parametter, "SelfAttentionOperation");
+
+    return;
+}
+
+namespace {
+TORCH_LIBRARY_FRAGMENT(atb, m)
+{
+    m.def("_npu_flash_attention_v3(Tensor query, Tensor key, Tensor value, Tensor mask, Tensor seq_len, float scale_value, int num_heads, int num_kv_heads, *, int ? mask_type = 3, Tensor(a!) out) -> ()");
+}
+}
+
+namespace {
+TORCH_LIBRARY_IMPL(atb, PrivateUse1, m)
+{
+    m.impl("_npu_flash_attention_v3", TORCH_FN(atb::_npu_flash_attention_v3));
+}
+}
+} // namespace
