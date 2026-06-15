781c1de9ce3cf72a84d1be08bf5dea4dffa06a1b
huangyunlong2022 <huangyunlong4@h-partners.com>
2026-06-15 16:24:04 +0800

    test update ut
    Co-authored-by: huangyunlong2022<huangyunlong4@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !5181 merge cherry-pick-mr-5125-1781485906258-auto into 26.1.0
    
    test update ut
    
    Created-by: huangyunlong2022
    Commit-by: huangyunlong2022
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    
    - [ ] 需求
    - [x] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    > 请描述修改内容的具体实现，涉及哪些组件之间进行交互，可以用1、2、3、...进行罗列\
    > 如果是需求或者重构类的PR，需要补充详细设计文档（说明上下游组件关系、时序图、类图、DFX能力等内容）
    
    im2col_backward fp16使用双标杆
    addcmul fp16使用bf16的标准
    
    # 【资料变更】
    > 请确认是否涉及资料变更。如涉及，需要在PR中体现，并简要说明修改内容。如不涉及，需填写“不涉及”
    
    # 【接口变更】
    > 请确认是否涉及跨代码仓或者客户面可见的接口变更。如涉及，需要详细说明接口以及对应的变更内容，同时需要在资料中体现。如不涉及，需填写“不涉及”
    
    # 【功能验证】
    > 说明测试场景，测试方法。如果本次测试方式与常规单元测试不同，请详细说明您的测试步骤\
    > 新增/变更内容是否已新增/适配UT测试用例看护，并补充测试自验证截图
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [x] 代码注释完备，正确记录错误日志
    - [x] 代码实现进行了返回值、空指针等校验
    - [x] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [x] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/op-plugin!5181

--- test/test_base_ops/test_im2col_backward.py
+++ test/test_base_ops/test_im2col_backward.py
@@ -10,7 +10,16 @@ from torch_npu.testing.common_utils import create_common_tensor
 
 class TestIm2colBackward(TestCase):
 
-    @unittest.skip("Temporarily skipping")
+    def compute_re(self, output, golden):
+        diff_value = torch.abs(torch.subtract(output.to(golden.dtype), golden))
+        diff_value_rel = diff_value / (torch.abs(golden) + 1e-7)
+        max_re = torch.max(diff_value_rel).item()
+        avg_re = torch.mean(diff_value_rel).item()
+
+        diff = torch.subtract(output.to(golden.dtype), golden)
+        rmse = torch.sqrt(torch.sum(diff*diff)/diff.numel()).item()
+        return max_re, avg_re, rmse
+
     def test_im2col_backward(self):
         dtype_list = [np.float16, np.float32]
         shape_list = [[1, 144, 256], [144, 256]]
@@ -20,7 +29,16 @@ class TestIm2colBackward(TestCase):
             cpu_input, npu_input = create_common_tensor((dtype, 0, shape), -100, 100)
             cpu_output = fold_cpu(cpu_input)
             npu_output = fold_npu(npu_input)
-            self.assertRtolEqual(cpu_output, npu_output.cpu())
+            if dtype == np.float16:
+                golden = fold_cpu(cpu_input.float())
+                cpu_max_re, cpu_avg_re, cpu_rmse = self.compute_re(cpu_output, golden)
+                npu_max_re, npu_avg_re, npu_rmse = self.compute_re(npu_output.cpu(), golden)
+                self.assertLessEqual(npu_max_re/cpu_max_re, 2)
+                self.assertLessEqual(npu_avg_re/cpu_avg_re, 1.2)
+                self.assertLessEqual(npu_rmse/cpu_rmse, 1.2)
+
+            else:
+                self.assertRtolEqual(cpu_output, npu_output.cpu())
 
 
 if __name__ == '__main__':
