c179d60b3888f66251cbad448abe9f272c68cf38
jy_514wand <1520348265@qq.com>
2026-07-12 16:10:50 +0800

    _npu_paged_attention_splitfuse_v2的context_lens输入添加host数据绑定
    Co-authored-by: jy_514wand<1520348265@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !5455 merge 26.1.0 into 26.1.0
    
    _npu_paged_attention_splitfuse_v2的context_lens输入添加host数据绑定
    
    Created-by: jy_514wand
    Commit-by: jy_514wand
    Merged-by: ascend-robot
    Description: <!--
    PR描述模板更新日期：20260203
    -->
    
    # 【合入来源】
    > <font color="red">**如有社区issue，请关联issue链接**</font>\
    > <font color="red">**请勿携带内部流程信息（需求链接、问题单、内部issue等）**</font>
    https://gitcode.com/Ascend/op-plugin/issues/275
    - [ ] 需求
    - [x] 问题单
    - [ ] issue/工单
    - [ ] 重构优化
    - [ ] 资料更新
    
    # 【修改方案】
    _npu_paged_attention_splitfuse_v2的context_lens输入添加host数据绑定
    
    # 【资料变更】
    不涉及
    
    # 【接口变更】
    不涉及
    
    # 【功能验证】
    产品环境vllm验证通过
    
    # 【CheckList】
    > PR提交人对以下CheckList自检项进行全量自检，自检通过或不涉及，均修改 [ ] 为 [x]
    
    - [ ] 代码注释完备，正确记录错误日志
    - [ ] 代码实现进行了返回值、空指针等校验
    - [ ] PR标题正确使用类型标签，如：feat、fix、refactor、docs、test等
    - [ ] PR持续集成流水线（CI）执行通过，代码检查无异常
    
    
    See merge request: Ascend/op-plugin!5455

--- op_plugin/ops/atb/PagedAttentionSplitfuseV2.cpp
+++ op_plugin/ops/atb/PagedAttentionSplitfuseV2.cpp
@@ -18,6 +18,9 @@
 
 using namespace std;
 
+namespace {
+    constexpr int64_t CONTEXT_LENS_INDEX = 4;
+}
 namespace atb {
 using PagedAttentionParam = atb::infer::PagedAttentionParam;
 
@@ -66,6 +69,9 @@ void _npu_paged_attention_splitfuse_v2(const at::Tensor &query, const at::Tensor
         .Input(mask)
         .Input(seq_len, true)
         .Output(out);
+    at::Tensor context_lens_cpu = context_lens.cpu();
+    paramsetter.variant_pack_.inTensors[CONTEXT_LENS_INDEX].hostData = context_lens_cpu.data_ptr();
+    paramsetter.tensor_maintainer_.cpu_tensors.emplace_back(std::move(context_lens_cpu));
     auto opPaged = pagedAttentionParamCache.getOperation(pagedparam, "PagedAttentionOperation");
     RunAtbCmd(opPaged, paramsetter, "PagedAttentionOperation");
 
